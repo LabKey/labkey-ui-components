5800f39b984defb370edf1b614c7e881cd025d72
cnathe <cnathe@labkey.com>
2019-02-20 13:12:38 -0600

    Missed including fetchDomain function in exported items for @glass/domainproperties

--- packages/domainproperties/src/index.ts
+++ packages/domainproperties/src/index.ts
@@ -2,12 +2,16 @@
  * Copyright (c) 2019 LabKey Corporation. All rights reserved. No portion of this work may be reproduced in
  * any form or by any electronic or mechanical means without written permission from LabKey Corporation.
  */
+import {fetchDomain} from "./actions";
 import {DomainDesign} from "./models";
 import {DomainFieldsDisplay} from "./components/DomainFieldsDisplay"
 
 export {
     // components
     DomainFieldsDisplay,
+
+    // functions
+    fetchDomain,
 
     // models
     DomainDesign

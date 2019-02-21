19fff6c5aa03626417386290cdd244497316b305
labkey-susanh <susanh@labkey.com>
2019-02-21 12:48:13 -0800

    Add minimal tests for querygrid with some TODOs

---
+++ packages/querygrid/src/components/QueryGrid.spec.tsx
@@ -0,0 +1,62 @@
+import React from 'reactn';
+import renderer from 'react-test-renderer';
+
+import { QueryGrid } from "./QueryGrid";
+import { QueryGridModel, SchemaQuery } from "@glass/models/src";
+import { initQueryGridState, updateQueryGridModel } from "../global";
+
+beforeAll(() => {
+    initQueryGridState()
+});
+
+// Mock all the actions to test just the rendering parts for QueryGrid itself
+jest.mock('../actions');
+
+describe("render", () => {
+    test("loading", () => {
+
+        const schemaQuery = new SchemaQuery({
+            schemaName: "schema",
+            queryName: "q-snapshot"
+        });
+       const tree = renderer.create(<QueryGrid schemaQuery={schemaQuery}/>).toJSON();
+       expect(tree).toMatchSnapshot();
+    });
+
+    test("query grid error", () => {
+        const modelId = "queryGridError";
+        const schemaQuery = new SchemaQuery({
+            schemaName: "schema",
+            queryName: "q-snapshot"
+        });
+        const model = new QueryGridModel({
+            id: modelId,
+            isLoaded: true,
+            isLoading: false,
+            isError: true,
+            schema: schemaQuery.schemaName,
+            query: schemaQuery.queryName,
+        });
+        updateQueryGridModel(model, {}, false);
+        const tree = renderer.create(<QueryGrid model={model} schemaQuery={schemaQuery}/>);
+        expect(tree).toMatchSnapshot();
+    });
+
+    test("allow selection", () => {
+        const modelId = "gridWithSelection";
+        const schemaQuery = new SchemaQuery({
+            schemaName: "schema",
+            queryName: "withSelection"
+        });
+        const model = new QueryGridModel({
+            id: modelId,
+            isLoaded: true,
+            schema: schemaQuery.schemaName,
+            query: schemaQuery.queryName,
+
+        });
+        // TODO make sure selection column shows up
+        // TODO ??? click and verify state
+    });
+
+});

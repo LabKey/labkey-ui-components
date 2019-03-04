6709c4cad0736a2afdaa8ff123165453c2b81664
labkey-danield <danield@labkey.com>
2019-03-04 12:00:50 -0800

    Trying to get the test to show up in the TeamCity log. Also checking in to see if TC triggers work as expected.

--- packages/domainproperties/rollup.config.js
+++ packages/domainproperties/rollup.config.js
@@ -2,6 +2,7 @@
  * Copyright (c) 2018 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ * Yet another try.
  */
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';

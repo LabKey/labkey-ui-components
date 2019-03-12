06735a76b92d90865643c03204c5882e9110b468
labkey-susanh <susanh@labkey.com>
2019-03-11 19:44:21 -0700

    Add package template

---
+++ packages/template/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation. All rights reserved. No portion of this work may be reproduced in
+ * any form or by any electronic or mechanical means without written permission from LabKey Corporation.
+ */
+// Add explicit export block for the classes and functions to be exported from this package
+export {
+    // classes
+    // TODO
+
+    // functions
+    // TODO
+}

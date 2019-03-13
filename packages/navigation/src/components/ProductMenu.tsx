cb92024c0ba81b62ef1ea210ce510bc7ae9ed185
labkey-susanh <susanh@labkey.com>
2019-03-12 20:01:33 -0700

    Remove components currently residing in sampleManagement repo.  They will come back once they are a bit more stable.

--- packages/navigation/src/components/ProductMenu.tsx
+++
@@ -1,8 +0,0 @@
-import React from "reactn";
-
-interface ProductMenuProps {
-    productId: string
-}
-
-export class ProductMenu extends React.Component<ProductMenuProps, any> {
-}
--- packages/navigation/src/components/SearchBox.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'reactn'
-
-export class SearchBox extends React.Component<any, any> {
-    render() {
-        return (
-            <input placeholder="Enter search terms"/>
-        )
-    }
-}

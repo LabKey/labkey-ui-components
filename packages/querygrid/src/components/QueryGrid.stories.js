7336614e4725af1bf049fe29bffb6bee9f7bdec2
labkey-susanh <susanh@labkey.com>
2019-04-15 16:12:41 -0700

    Initial storybook setup for querygrid

--- packages/querygrid/src/components/QueryGrid.stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/react'
-import { withMarkdownNotes } from '@storybook/addon-notes'
-
-import * as constants from "@glass/storybook/src/stories/constants";
-import {QueryGrid} from "../../dist";
-
-storiesOf('QueryGrid', module)
-    .add('With basic data', withMarkdownNotes(constants.gridWithBasicDataMD)(() => <QueryGrid/>));

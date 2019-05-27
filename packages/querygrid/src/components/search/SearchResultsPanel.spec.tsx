2e5935409472320d25e66e042b4e16769ac2df00
cnathe <cnathe@labkey.com>
2019-05-27 12:14:06 -0500

    Epic 5215: jest tests for NavigationBar search box, SearchResultCard, and SearchResultsPanel

---
+++ packages/querygrid/src/components/search/SearchResultsPanel.spec.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react'
+import { fromJS } from 'immutable';
+import { mount } from "enzyme";
+import { Alert, LoadingSpinner } from "@glass/base";
+
+import { SearchResultCard } from "./SearchResultCard";
+import { SearchResultsPanel } from "./SearchResultsPanel";
+import { SearchResultsModel } from "../../model";
+import entitiesJSON from "../../test/data/searchResults.json";
+
+describe("<SearchResultsPanel/>", () => {
+
+    test("loading", () => {
+        const model = SearchResultsModel.create({isLoading: true});
+        const component = (
+            <SearchResultsPanel model={model}/>
+        );
+
+        const wrapper = mount(component);
+        expect(wrapper.find(LoadingSpinner)).toHaveLength(1);
+        expect(wrapper.find(Alert)).toHaveLength(0);
+        expect(wrapper.find(SearchResultCard)).toHaveLength(0);
+        wrapper.unmount();
+    });
+
+    test("with error", () => {
+        const model = SearchResultsModel.create({error: 'Test error message'});
+        const component = (
+            <SearchResultsPanel model={model}/>
+        );
+
+        const wrapper = mount(component);
+        expect(wrapper.find(LoadingSpinner)).toHaveLength(0);
+        expect(wrapper.find(Alert)).toHaveLength(1);
+        expect(wrapper.find(SearchResultCard)).toHaveLength(0);
+        wrapper.unmount();
+    });
+
+    test("with no search hits", () => {
+        const model = SearchResultsModel.create({entities: fromJS({hits: []})});
+        const component = (
+            <SearchResultsPanel model={model}/>
+        );
+
+        const wrapper = mount(component);
+        expect(wrapper.find(LoadingSpinner)).toHaveLength(0);
+        expect(wrapper.find(Alert)).toHaveLength(0);
+        expect(wrapper.find(SearchResultCard)).toHaveLength(0);
+        wrapper.unmount();
+    });
+
+    test("with search hits", () => {
+        const model = SearchResultsModel.create({entities: fromJS(entitiesJSON)});
+        const component = (
+            <SearchResultsPanel model={model}/>
+        );
+
+        const wrapper = mount(component);
+        expect(wrapper.find(LoadingSpinner)).toHaveLength(0);
+        expect(wrapper.find(Alert)).toHaveLength(0);
+        expect(wrapper.find(SearchResultCard)).toHaveLength(46);
+        wrapper.unmount();
+    });
+
+});

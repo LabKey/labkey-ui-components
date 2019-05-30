0abaf5e5485a506218aee63c4e87cefeea1a23b0
cnathe <cnathe@labkey.com>
2019-05-30 12:13:05 -0500

    Epic 5215: ManageDropdownButton storybook and jest

---
+++ packages/base/src/stories/ManageDropdownButton.tsx
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+import * as React from 'react'
+import { MenuItem } from "react-bootstrap";
+import { storiesOf } from '@storybook/react'
+import { boolean, button, number, radios, select, text, withKnobs } from '@storybook/addon-knobs'
+
+import { ManageDropdownButton } from "../components/buttons/ManageDropdownButton";
+import './stories.css'
+
+storiesOf("ManageDropdownButton", module)
+    .addDecorator(withKnobs)
+    .add("with knobs", () => {
+        // adding div with padding so that you can see the menu items when pullRight=true
+        return (
+            <div style={{padding: '20px 0 0 100px'}}>
+                <ManageDropdownButton
+                    id={'storybook-manage'}
+                    pullRight={boolean('pullRight', false)}
+                    collapsed={boolean('collapsed', false)}
+                >
+                    <MenuItem disabled={true}>First Item</MenuItem>
+                    <MenuItem disabled={true}>Second Item</MenuItem>
+                </ManageDropdownButton>
+            </div>
+        )
+    });

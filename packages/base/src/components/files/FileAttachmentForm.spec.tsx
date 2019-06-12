4cdf16bcb17fb238681199a9482933483f4af254
cnathe <cnathe@labkey.com>
2019-06-12 14:17:31 -0500

    Item #5836: jest tests for FileAttachmentForm previewGridProps prop

--- packages/base/src/components/files/FileAttachmentForm.spec.tsx
+++ packages/base/src/components/files/FileAttachmentForm.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { fromJS } from "immutable";
+import { fromJS, List, Map } from "immutable";
 import { FileAttachmentForm } from "./FileAttachmentForm";
 import { shallow, mount } from "enzyme";
 
@@ -35,6 +35,42 @@ describe("<FileAttachmentForm/>", () => {
         btn = wrapper.find('button.file-form-submit-btn');
         expect(btn).toHaveLength(1);
         expect(btn.props().disabled).toBe(false);
+
+        wrapper.unmount();
+    });
+
+    test('with previewGridProps', () => {
+        const wrapper = mount(<FileAttachmentForm
+            previewGridProps={{
+                previewCount: 1
+            }}
+        />);
+        expect(wrapper.find('FilePreviewGrid')).toHaveLength(0);
+        expect(wrapper.find('LoadingSpinner')).toHaveLength(0);
+
+        wrapper.setState({
+            previewStatus: 'With preview status',
+            errorMessage: undefined,
+            previewData: undefined
+        });
+        expect(wrapper.find('FilePreviewGrid')).toHaveLength(0);
+        expect(wrapper.find('LoadingSpinner')).toHaveLength(1);
+
+        wrapper.setState({
+            previewStatus: undefined,
+            errorMessage: 'With error message',
+            previewData: undefined
+        });
+        expect(wrapper.find('FilePreviewGrid')).toHaveLength(1);
+        expect(wrapper.find('LoadingSpinner')).toHaveLength(0);
+
+        wrapper.setState({
+            previewStatus: undefined,
+            errorMessage: undefined,
+            previewData: List<Map<string, any>>()
+        });
+        expect(wrapper.find('FilePreviewGrid')).toHaveLength(1);
+        expect(wrapper.find('LoadingSpinner')).toHaveLength(0);
 
         wrapper.unmount();
     });

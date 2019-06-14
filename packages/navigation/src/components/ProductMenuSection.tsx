cc497dfeb59dbf75b262a893d98c55fcdfc5d07c
cnathe <cnathe@labkey.com>
2019-06-14 15:47:35 -0500

    Item #5761: use naturalSort for ProductMenuSection items

--- packages/navigation/src/components/ProductMenuSection.tsx
+++ packages/navigation/src/components/ProductMenuSection.tsx
@@ -1,6 +1,6 @@
 import React from "reactn";
 import { Record } from 'immutable';
-import { AppURL } from '@glass/base'
+import { AppURL, naturalSort } from '@glass/base'
 
 import { MenuSectionModel } from '../model';
 
@@ -41,14 +41,14 @@ export class ProductMenuSection extends React.Component<MenuSectionProps, any> {
 
         return (
             <ul className={'col-' + totalColumns} key={section.key + 'col-' + columnNumber}>
-                {items.isEmpty() ?
-                    config.emptyText && <li key="empty" className="empty-section">{config.emptyText}</li>
-                    : items.map(item => {
-                    if (item.url) {
-                        let url = item.url instanceof AppURL ? item.url.toHref() : item.url;
-                        return <li key={item.label}><a href={url} target={item.key === "docs" ? "_blank" : "_self"}>{item.label}</a></li>;
-                    }
-                    return <li key={item.label}>{item.label}</li>
+                {items.isEmpty()
+                    ? config.emptyText && <li key="empty" className="empty-section">{config.emptyText}</li>
+                    : items.sortBy(item => item.label, naturalSort).map(item => {
+                        if (item.url) {
+                            const url = item.url instanceof AppURL ? item.url.toHref() : item.url;
+                            return <li key={item.label}><a href={url} target={item.key === "docs" ? "_blank" : "_self"}>{item.label}</a></li>;
+                        }
+                        return <li key={item.label}>{item.label}</li>
                     })
                 }
             </ul>

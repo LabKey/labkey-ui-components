c05a7583bf452d0c5ab91042f6f3c8f00d31fa5c
labkey-susanh <susanh@labkey.com>
2019-07-16 09:29:59 -0700

    Update wording on run delete confirmation modal.

--- packages/querygrid/src/components/assay/AssayRunDeleteConfirmModal.tsx
+++ packages/querygrid/src/components/assay/AssayRunDeleteConfirmModal.tsx
@@ -19,9 +19,11 @@ export class AssayRunDeleteConfirmModal extends React.Component<Props, any> {
                 title={"Permanently delete " + numToDelete  + noun + "?"}
                 msg={
                     <span>
-                        The selected {numToDelete > 1 ? numToDelete : ''} {noun} and all of {numToDelete === 1 ? 'its' : 'their'} data will be permanently deleted.&nbsp;
-                        <p className={'top-spacing'}><strong>Deletion cannot be undone.</strong>&nbsp;
-                            Do you want to proceed?</p>
+                        The entirety of the {numToDelete > 1 ? numToDelete : ''} selected {noun} and all of {numToDelete === 1 ? 'its' : 'their'} history will be permanently deleted.&nbsp;
+                        <p className={'top-spacing'}>
+                            <strong>Deletion cannot be undone.</strong>
+                            &nbsp;Do you want to proceed?
+                        </p>
                     </span>
                 }
                 onConfirm={onConfirm}

ca818a89cecc8bb1200dbef48add580aec44dddf
labkey-susanh <susanh@labkey.com>
2019-07-24 19:39:44 -0700

    Add story for AssayReimportHeader

---
+++ packages/querygrid/src/stories/AssayReimportHeader.tsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as React from 'react';
+import { fromJS } from "immutable";
+import { storiesOf } from "@storybook/react";
+import { boolean, withKnobs } from '@storybook/addon-knobs'
+import { AssayDefinitionModel } from "@glass/base";
+import './stories.scss'
+import { AssayReimportHeader } from '../components/assay/AssayReimportHeader';
+
+import assayDefJSON from '../test/data/assayDefinitionModel.json';
+
+storiesOf('AssayReimportHeader', module)
+    .addDecorator(withKnobs)
+    .add("with knobs", () => {
+
+        const assay = AssayDefinitionModel.create(assayDefJSON);
+
+        const runData = fromJS({
+            'RowId': {
+                'value': 10
+            },
+            'Name': {
+                'value': 'Test Name'
+            }
+        });
+        return (
+            <AssayReimportHeader
+                hasBatchProperties={boolean("Has batch properties?", false)}
+                assay={assay}
+                replacedRunData={runData}
+            />
+        )
+    });

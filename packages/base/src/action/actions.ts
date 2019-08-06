11f6f53ee4285fc7380460f7c34e98c1358d2709
labkey-susanh <susanh@labkey.com>
2019-08-06 08:57:37 -0700

    Add one to lines to include for preview to account for the header

--- packages/base/src/action/actions.ts
+++ packages/base/src/action/actions.ts
@@ -136,6 +136,7 @@ export function inferDomainFromFile(file: File, numLinesToInclude: number) : Pro
 /**
  * This is used for retrieving preview data for a file already on the server side
  * @param file  This can be a rowId for the file, or a path to the file
+ * @param numLinesToInclude: the number of lines of data to include (excludes the header)
  */
 export function getServerFilePreview(file: string, numLinesToInclude: number) : Promise<InferDomainResponse>{
     return new Promise((resolve, reject) => {
@@ -143,7 +144,10 @@ export function getServerFilePreview(file: string, numLinesToInclude: number) : 
         Ajax.request({
                 url: buildURL('property', 'getFilePreview.api'),
                 method: 'GET',
-                params: {file, numLinesToInclude},
+                params: {
+                    file,
+                    numLinesToInclude: numLinesToInclude ? (numLinesToInclude + 1) : undefined // add one to account for the header
+                },
                 success: Utils.getCallbackWrapper((response) => {
                     resolve(InferDomainResponse.create(response));
                 }),

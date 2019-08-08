ccb1c082ef76de0ab0eb1df32b8479ff98ec1dac
Alan Vezina <alanv@labkey.com>
2019-08-08 16:20:19 -0700

    Add mock.tsx to ReportList storybook
    Adds some mock XHR requests so we can render a PreviewGrid in the ReportList modal.

---
+++ packages/report-list/src/stories/mock.tsx
@@ -0,0 +1,45 @@
+import mock, { proxy } from 'xhr-mock';
+
+import mixturesQueryInfo from '../../../querygrid/src/test/data/mixtures-getQueryDetails.json';
+import mixturesQuery from '../../../querygrid/src/test/data/mixtures-getQuery.json';
+
+export function initMocks() {
+    console.log('INIT MOCKS');
+    mock.setup();
+
+    mock.get(/.*\/query\/.*\/getQueryDetails.*/, (req, res) => {
+        const queryParams = req.url().query;
+        let responseBody;
+        let lcSchemaName = queryParams.schemaName.toLowerCase();
+        let lcQueryName = queryParams.queryName.toLowerCase();
+
+        console.log('Mock API', lcSchemaName, lcQueryName);
+
+        if (lcSchemaName === 'exp.data' && lcQueryName === 'mixtures') {
+            responseBody = mixturesQueryInfo;
+        }
+
+        return res
+            .status(200)
+            .headers({'Content-Type': 'application/json'})
+            .body(JSON.stringify(responseBody));
+    });
+
+    mock.post(/.*\/query\/.*\/getQuery.*/,  (req, res) => {
+        const bodyParams = req.body().toLowerCase();
+        let responseBody;
+
+        if (bodyParams.indexOf("&query.queryname=mixtures&") > -1) {
+            responseBody = mixturesQuery;
+        }
+
+        return res
+            .status(200)
+            .headers({'Content-Type': 'application/json'})
+            .body(JSON.stringify(responseBody));
+    });
+
+    mock.use(proxy);
+}
+
+initMocks();

cb9c6e99dd19668a9a69fa35ed30067ecd1d7005
Binal <binalp@labkey.com>
2019-08-11 13:57:06 -0700

    Jest test for server side banner message. Test for updateDomainException() that creates client side warning.

--- packages/domainproperties/src/actions/actions.spec.ts
+++ packages/domainproperties/src/actions/actions.spec.ts
@@ -13,9 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { createFormInputId } from "./actions";
-import {DomainField} from "../models";
-import {DOMAIN_FIELD_PREFIX, FLAG_CONCEPT_URI, INT_RANGE_URI, STRING_RANGE_URI, USER_RANGE_URI} from "../constants";
+import { createFormInputId, getBannerMessages, updateDomainException } from "./actions";
+import {DomainDesign, DomainException, DomainField} from "../models";
+import {
+    DOMAIN_FIELD_PREFIX,
+    FLAG_CONCEPT_URI,
+    INT_RANGE_URI,
+    SEVERITY_LEVEL_ERROR, SEVERITY_LEVEL_WARN,
+    STRING_RANGE_URI,
+    USER_RANGE_URI
+} from "../constants";
+import {mount} from "enzyme";
+import toJson from "enzyme-to-json";
+import * as React from "react";
 
 describe("domain properties actions", () => {
 
@@ -48,5 +58,100 @@ describe("domain properties actions", () => {
             propertyURI: 'test'
         });
         expect(field3.dataType.name).toBe('users');
+    });
+
+    test('server side error on the banner', () => {
+
+        let fields = [];
+        fields.push({
+            name: 'column1',
+            rangeURI: INT_RANGE_URI,
+            propertyId: 1, //simulate existing field
+            propertyURI: 'test'
+        });
+
+        fields.push({
+            name: 'column1',
+            rangeURI: INT_RANGE_URI,
+            propertyId: undefined, //new field with duplicate column name
+            propertyURI: 'test'
+        });
+
+        let message = "The field name 'column1' is already taken. Please provide a unique name for each field.";
+        let fieldName = 'column1';
+        let severity = SEVERITY_LEVEL_ERROR;
+        let domainFieldError = [];
+        domainFieldError.push({message, fieldName, propertyId: undefined, severity});
+        let domainException = new DomainException({exception: message, success: false, severity, errors: domainFieldError});
+
+        let domain = DomainDesign.create({
+            name: "CancerCuringStudy",
+            schemaName : 'Study',
+            queryName : 'CancerCuringStudy',
+            description: 'description',
+            domainURI: 'test',
+            domainId: 123,
+            fields: fields,
+            indices: []
+        }, domainException);
+
+        expect(domain.domainException.errors.get(0).message).toBe(message);
+        expect(getBannerMessages(domain).get(0).message).toBe(message);
+
+        fields.push({
+            name: 'modified',//reserved field
+            rangeURI: INT_RANGE_URI,
+            propertyId: undefined,
+            propertyURI: 'test'
+        });
+
+        let multipleErrorMsg = "Multiple fields contain issues that need to be fixed. Review the red highlighted fields below for more information.";
+
+        message = "'modified' is a reserved field name in 'CancerCuringStudy'";
+        fieldName = 'modified';
+        domainFieldError.push({message, fieldName, propertyId: undefined, severity});
+        domainException = new DomainException({exception: message, success: false, severity, errors: domainFieldError});
+
+        domain = DomainDesign.create({
+            name: "CancerCuringStudy",
+            schemaName : 'Study',
+            queryName : 'CancerCuringStudy',
+            description: 'description',
+            domainURI: 'test',
+            domainId: 123,
+            fields: fields,
+            indices: []
+        }, domainException);
+
+        expect(getBannerMessages(domain).get(0).message).toBe(multipleErrorMsg);
+    });
+
+    test('client side warning on the banner', () => {
+
+        let fields = [];
+        fields.push({
+            name: '#column#',
+            rangeURI: INT_RANGE_URI,
+            propertyId: undefined,
+            propertyURI: 'test'
+        });
+
+        let fieldName = '#column#';
+        let message = "SQL queries, R scripts, and other code are easiest to write when field names only contain combination of letters, numbers, and underscores, and start with a letter or underscore.";
+        let severity = SEVERITY_LEVEL_WARN;
+        let domainFieldError = [];
+        domainFieldError.push({message, fieldName, propertyId: undefined, severity});
+
+        let domain = DomainDesign.create({
+            name: "CancerCuringStudy",
+            description: 'description',
+            domainURI: 'test',
+            domainId: 123,
+            fields: fields,
+            indices: []
+        }, undefined);
+
+        let updatedDomain = updateDomainException(domain, 0, domainFieldError);
+        expect(updatedDomain.domainException.get('errors').get(0)[0].message).toBe(message);
     });
 });

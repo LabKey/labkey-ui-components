bfe0bbf0864618fe6b8765ed65c659f064c5f947
labkey-susanh <susanh@labkey.com>
2019-11-08 19:32:34 -0800

    Change from schemaQuery property to QueryGridModel

--- packages/querygrid/src/components/editable/EditableGridModal.tsx
+++ packages/querygrid/src/components/editable/EditableGridModal.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
 import { Modal } from "react-bootstrap";
-import { QueryGridModel, SchemaQuery, WizardNavButtons } from '@glass/base';
-
-import { EditableGridLoader } from './EditableGridLoader';
+import { QueryGridModel, WizardNavButtons } from '@glass/base';
 import { AddRowsControlProps } from './Controls';
 import { gridInit } from '../../actions';
 import { getQueryGridModel } from '../../global';
-import { getStateQueryGridModel } from '../../models';
 import { EditableGridPanel } from './EditableGridPanel';
+import { EditableGridProps } from './EditableGrid';
 
-interface Props {
+interface Props extends EditableGridProps {
     show: boolean,
     title: string,
     onCancel: (any) => void
     onSave: (model: QueryGridModel) => void
-    schemaQuery: SchemaQuery
     cancelText?: string
     addControlProps?: Partial<AddRowsControlProps>
     saveText?: string
@@ -31,10 +28,8 @@ export class EditableGridModal extends React.PureComponent<Props, any> {
         this.init();
     }
 
-    componentWillReceiveProps(nextProps: Props) {
-        if (nextProps.schemaQuery !== this.props.schemaQuery) {
-            this.init();
-        }
+    componentWillReceiveProps() {
+        this.init();
     }
 
     init() {
@@ -42,12 +37,7 @@ export class EditableGridModal extends React.PureComponent<Props, any> {
     }
 
     getQueryGridModel() {
-        const model = getStateQueryGridModel('lookup-management', this.props.schemaQuery, {
-            loader: new EditableGridLoader(),
-            editable: true
-        });
-
-        return getQueryGridModel(model.getId()) || model;
+        return getQueryGridModel(this.props.model.getId());
     }
 
     onSave = () => {

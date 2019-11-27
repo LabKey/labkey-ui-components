957bcc83949f218342e02a749956ec6f2515c9d0
Alan Vezina <alanv@labkey.com>
2019-11-26 18:07:57 -0600

    Remove unused imports

--- packages/querygrid/src/global.spec.ts
+++ packages/querygrid/src/global.spec.ts
@@ -31,8 +31,7 @@ import {
 import { GRID_CHECKBOX_OPTIONS, QueryColumn, QueryGridModel, SchemaQuery } from '@glass/base'
 import { List } from 'immutable';
 
-import { DataViewInfo, EditorModel, LookupStore } from './models'
-import { DataViewInfoTypes } from './constants';
+import { EditorModel, LookupStore } from './models'
 
 beforeEach(() => {
     resetQueryGridState();

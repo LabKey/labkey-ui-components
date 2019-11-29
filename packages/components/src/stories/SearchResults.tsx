0859bd43ec6d0017ebad1f0fbc039287ce5c0226
cnathe <cnathe@labkey.com>
2019-11-29 16:34:58 -0600

    Split out search related models and actions from main ts files (missed from last commit)

--- packages/components/src/stories/SearchResults.tsx
+++ packages/components/src/stories/SearchResults.tsx
@@ -20,7 +20,7 @@ import { boolean, number, text, withKnobs } from '@storybook/addon-knobs'
 
 import { SearchResultCard } from "../components/search/SearchResultCard";
 import { SearchResultsPanel } from "../components/search/SearchResultsPanel";
-import { SearchResultsModel } from "../models";
+import { SearchResultsModel } from "../components/search/models";
 import entitiesJSON from "../test/data/searchResults.json";
 import './stories.scss'
 

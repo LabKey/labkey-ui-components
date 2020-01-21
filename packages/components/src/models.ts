33da122e124237b017f846eaff2bf2997631a4a2
ian <ians@labkey.com>
2020-01-21 12:30:51 -0800

    Change editablegrid uniqueness constraint to be case-insensitive - and update test keys to match

--- packages/components/src/models.ts
+++ packages/components/src/models.ts
@@ -617,7 +617,7 @@ export class EditorModel extends Record({
                     // there better be only one of these
                     const valueDescriptor = values.get(0);
                     if (valueDescriptor && this.hasRawValue(valueDescriptor)) {
-                        const stringVal = valueDescriptor.raw.toString();
+                        const stringVal = valueDescriptor.raw.toString().toLowerCase();
                         if (uniqueKeyMap.has(stringVal)) {
                             uniqueKeyMap = uniqueKeyMap.set(stringVal, uniqueKeyMap.get(stringVal).push(rn+1));
                         }

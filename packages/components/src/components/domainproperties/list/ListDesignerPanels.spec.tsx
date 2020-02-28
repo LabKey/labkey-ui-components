82797eda0a6d169bfb8820d13e7ee2dfc0b028ff
Rosaline Pyktel <rosalinep@labkey.com>
2020-02-28 12:16:23 -0800

    ListDesingerPanels and ListPropertiesPanel

--- packages/components/src/components/domainproperties/list/ListDesignerPanels.spec.tsx
+++ packages/components/src/components/domainproperties/list/ListDesignerPanels.spec.tsx
@@ -1,60 +1,88 @@
 import React from "react";
 import {ListDesignerPanels} from "./ListDesignerPanels";
 import {mount} from "enzyme";
-import {BasicPropertiesFields} from "./ListPropertiesPanelFormElements";
 import toJson from 'enzyme-to-json';
 import {ListModel} from "./models";
 import {DEFAULT_LIST_SETTINGS} from "../../../test/data/constants";
 import getDomainDetailsJSON from "../../../test/data/property-getDomainDetails.json";
+import DomainForm from "../DomainForm";
+import {ListPropertiesPanel} from "./ListPropertiesPanel";
+import {Alert} from "react-bootstrap";
 
 const emptyNewModel = ListModel.create(null, DEFAULT_LIST_SETTINGS);
 const populatedExistingModel = ListModel.create(getDomainDetailsJSON);
-const invalidModelHasException = populatedExistingModel.setIn(['domain', 'domainException'], {severity: "Error"}) as ListModel;
-
 
 describe('ListDesignerPanel', () => { // in progress
-    // test('new list', () => {
-    //     const listDesignerPanels = mount(
-    //         <ListDesignerPanels
-    //             onComplete={() => {}}
-    //             onCancel={() => {}}
-    //             initModel={emptyNewModel}
-    //         />
-    //     );
-    //
-    //     expect(toJson(listDesignerPanels)).toMatchSnapshot();
-    //     listDesignerPanels.unmount();
-    // });
-    //
-    // test('existing list', () => {
-    //     const listDesignerPanels = mount(
-    //         <ListDesignerPanels
-    //             onComplete={() => {}}
-    //             onCancel={() => {}}
-    //             initModel={populatedExistingModel}
-    //         />
-    //     );
-    //
-    //     expect(toJson(listDesignerPanels)).toMatchSnapshot();
-    //     listDesignerPanels.unmount();
-    // });
-    //
-    // test('model with exception', () => {
-    //     const listDesignerPanels = mount(
-    //         <ListDesignerPanels
-    //             onComplete={() => {}}
-    //             onCancel={() => {}}
-    //             initModel={invalidModelHasException}
-    //         />
-    //     );
-    //
-    //     expect(toJson(listDesignerPanels)).toMatchSnapshot();
-    //     listDesignerPanels.unmount();
-    // });
+    test('new list', () => {
+        const listDesignerPanels = mount(
+            <ListDesignerPanels
+                onComplete={() => {}}
+                onCancel={() => {}}
+                initModel={emptyNewModel}
+            />
+        );
 
-    test('import file', () => {
+        expect(toJson(listDesignerPanels)).toMatchSnapshot();
+        listDesignerPanels.unmount();
+    });
+
+    test('existing list', () => {
+        const listDesignerPanels = mount(
+            <ListDesignerPanels
+                onComplete={() => {}}
+                onCancel={() => {}}
+                initModel={populatedExistingModel}
+            />
+        );
+
+        expect(toJson(listDesignerPanels)).toMatchSnapshot();
+        listDesignerPanels.unmount();
+    });
+
+    test('visible properties', () => {
+        const listDesignerPanels = mount(
+            <ListDesignerPanels
+                onComplete={() => {}}
+                onCancel={() => {}}
+                initModel={emptyNewModel}
+            />
+        );
+
+        expect(listDesignerPanels.find(ListPropertiesPanel)).toHaveLength(1);
+        expect(listDesignerPanels.find(DomainForm)).toHaveLength(1);
+        expect(listDesignerPanels.find('.domain-assay-buttons')).toHaveLength(1);
     });
 
     test('open fields panel', () => {
+        const listDesignerPanels = mount(
+            <ListDesignerPanels
+                onComplete={() => {}}
+                onCancel={() => {}}
+                initModel={emptyNewModel}
+            />
+        );
+
+        const panelHeader = listDesignerPanels.find('div#domain-header');
+
+        expect(
+            listDesignerPanels
+                .find('#domain-header')
+                .at(2)
+                .hasClass('domain-panel-header-collapsed'))
+            .toBeTruthy();
+
+        panelHeader.simulate('click');
+
+        expect(
+            listDesignerPanels
+                .find('#domain-header')
+                .at(2)
+                .hasClass('domain-panel-header-expanded'))
+            .toBeTruthy();
+
+
+        expect(listDesignerPanels.find(Alert)).toHaveLength(2);
+        expect(listDesignerPanels.find(Alert).at(0).text()).toEqual('Contains errors or is missing required values.');
+        expect(listDesignerPanels.find(Alert).at(1).text()).toEqual('Please correct errors in the properties panel before saving.');
     });
 });
--- packages/components/src/components/domainproperties/list/ListPropertiesPanel.spec.tsx
+++ packages/components/src/components/domainproperties/list/ListPropertiesPanel.spec.tsx
@@ -5,6 +5,10 @@ import {ListModel} from "./models";
 import {DEFAULT_LIST_SETTINGS} from "../../../test/data/constants";
 import getDomainDetailsJSON from "../../../test/data/property-getDomainDetails.json";
 import {ListPropertiesPanel} from "./ListPropertiesPanel";
+import {AllowableActions, BasicPropertiesFields} from "./ListPropertiesPanelFormElements";
+import {AdvancedSettings} from "./ListPropertiesAdvancedSettings";
+import {CollapsiblePanelHeader} from "../CollapsiblePanelHeader";
+import { Alert } from '../../base/Alert';
 
 const emptyNewModel = ListModel.create(null, DEFAULT_LIST_SETTINGS);
 const populatedExistingModel = ListModel.create(getDomainDetailsJSON);
@@ -38,6 +42,75 @@ describe('ListPropertiesPanel', () => { // in progress
         listPropertiesPanel.unmount();
     });
 
+    test('list with error', () => {
+        const listPropertiesPanel = mount(
+            <ListPropertiesPanel
+                model={invalidModelHasException}
+                panelStatus={'TODO'}
+                onChange={() => {}}
+            />
+        );
+
+        expect(toJson(listPropertiesPanel)).toMatchSnapshot();
+        listPropertiesPanel.unmount();
+    });
+
+    //Note: visible properties are the same between new and existing lists
+    test('list visible properties', () => {
+        const listPropertiesPanel = mount(
+            <ListPropertiesPanel
+                model={populatedExistingModel}
+                panelStatus={'COMPLETE'}
+                onChange={() => {}}
+            />
+        );
+
+        expect(listPropertiesPanel.find(BasicPropertiesFields)).toHaveLength(1);
+        expect(listPropertiesPanel.find(AllowableActions)).toHaveLength(1);
+        expect(listPropertiesPanel.find(AdvancedSettings)).toHaveLength(1);
+        expect(listPropertiesPanel.find(CollapsiblePanelHeader)).toHaveLength(1);
+
+        expect(listPropertiesPanel.find(Alert)).toHaveLength(0);
+    });
+
     test('invalid', () => {
+        let listPropertiesPanel = mount(
+            <ListPropertiesPanel
+                model={populatedExistingModel}
+                panelStatus={'TODO'}
+                onChange={() => {}}
+            />
+        );
+        listPropertiesPanel.setState({isValid: false});
+        listPropertiesPanel.update();
+        expect(listPropertiesPanel.state()).toHaveProperty('isValid', false);
+
+        expect(listPropertiesPanel.find(BasicPropertiesFields)).toHaveLength(1);
+        expect(listPropertiesPanel.find(AllowableActions)).toHaveLength(1);
+        expect(listPropertiesPanel.find(AdvancedSettings)).toHaveLength(1);
+        expect(listPropertiesPanel.find(CollapsiblePanelHeader)).toHaveLength(1);
+
+        // expect(listPropertiesPanel.find(Alert)).toHaveLength(1); // alert is not being triggered
+    });
+
+    test('clicking advanced settings renders modal', () => {
+        const listPropertiesPanel = mount(
+            <ListPropertiesPanel
+                model={emptyNewModel}
+                panelStatus={'NONE'}
+                onChange={() => {}}
+            />
+        );
+
+        let advancedSettingsButton = listPropertiesPanel.find('button.domain-field-float-right');
+        expect(advancedSettingsButton).toHaveLength(1);
+
+        expect(listPropertiesPanel.find('.modal-title')).toHaveLength(0);
+
+        advancedSettingsButton.simulate('click');
+
+        expect(listPropertiesPanel.find('.modal-title')).toHaveLength(1);
+        expect(listPropertiesPanel.find('.modal-title').text()).toEqual(' Advanced List Settings ');
+
     });
 });

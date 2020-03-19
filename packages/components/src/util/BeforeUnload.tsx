d8310ff63896e25c25b12a99744ff9335ea2fad8
cnathe <cnathe@labkey.com>
2020-03-19 15:59:36 -0500

    Add comment to BeforeUnload.tsx HOC regarding when to use it and where

--- packages/components/src/util/BeforeUnload.tsx
+++ packages/components/src/util/BeforeUnload.tsx
@@ -4,6 +4,11 @@ interface Props {
     beforeunload: (event: any) => any
 }
 
+/**
+ * A HOC to be used for any LKS React page that needs to check for a dirty state on page navigation.
+ * Note that this HOC will not work for app usages which use react-router since the dirty check is to be done on
+ * route changes instead of on page navigation.
+ */
 export class BeforeUnload extends React.PureComponent<Props, any> {
 
     componentDidMount() {

38fb78ab40960232db31579a2047ef99f122896a
cnathe <cnathe@labkey.com>
2020-03-27 15:54:27 -0500

    Item 7002: Jest tests (new and updates)

---
+++ packages/components/src/components/domainproperties/SectionHeading.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { SectionHeading } from './SectionHeading';
+
+describe("<SectionHeading/>", () => {
+
+    test("title only", () => {
+        const component = (
+            <SectionHeading
+                title={'Section Heading Title'}
+            />
+        );
+
+        const tree = renderer.create(component).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test("title and cls", () => {
+        const component = (
+            <SectionHeading
+                title={'Section Heading Title'}
+                cls={'section-heading-cls'}
+            />
+        );
+
+        const tree = renderer.create(component).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});

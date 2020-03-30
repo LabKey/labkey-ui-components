fd31bb94329091619ee6eaeaeaf9a8b729cb7934
Alan Vezina <alanv@labkey.com>
2020-03-30 09:43:36 -0500

    Add Changeable SchemaQuery story

--- packages/components/src/stories/QueryModel.tsx
+++ packages/components/src/stories/QueryModel.tsx
@@ -1,10 +1,19 @@
 import React, { PureComponent } from 'react';
 import { storiesOf } from '@storybook/react';
-import { QueryModel } from '../QueryModel/QueryModel';
-import { ManageDropdownButton, SchemaQuery, SelectionMenuItem } from '..';
-import { GridPanel, GridPanelWithModel } from '../QueryModel/GridPanel';
-import { QueryConfigMap, RequiresModelAndActions } from '../QueryModel/withQueryModels';
-import { MenuItem } from 'react-bootstrap';
+import { Button, MenuItem } from 'react-bootstrap';
+import {
+    GridPanel,
+    GridPanelWithModel,
+    InjectedQueryModels,
+    ManageDropdownButton,
+    QueryConfigMap,
+    QueryModel,
+    RequiresModelAndActions,
+    SchemaQuery,
+    withQueryModels,
+} from '..';
+import './QueryModel.scss';
+import { verify } from 'crypto';
 
 class GridPanelButtonsExample extends PureComponent<RequiresModelAndActions> {
     render() {
@@ -17,6 +26,95 @@ class GridPanelButtonsExample extends PureComponent<RequiresModelAndActions> {
         );
     }
 }
+
+interface State {
+    schemaName?: string;
+    queryName?: string;
+    viewName?: string;
+}
+
+class ChangeableSchemaQueryImpl extends PureComponent<{} & InjectedQueryModels, State> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            schemaName: '',
+            queryName: '',
+            viewName: '',
+        };
+    }
+
+    onFormChange = (e) => {
+        const { name, value } = e.target;
+        this.setState(() => ({ [name]: value }));
+    };
+
+    applySchemaQuery = () => {
+        const { queryModels, actions } = this.props;
+        const { model } = queryModels;
+        let { schemaName, queryName, viewName } = this.state;
+        schemaName = schemaName.trim() || undefined;
+        queryName = queryName.trim() || undefined;
+        viewName = viewName.trim() || undefined;
+
+        if (schemaName === undefined || queryName === undefined) {
+            console.warn('Cannot have empty schemaName or queryName');
+            return;
+        }
+
+        const schemaQuery = SchemaQuery.create(schemaName, queryName, viewName);
+
+        if (model !== undefined) {
+            actions.setSchemaQuery(model.id, schemaQuery);
+        } else {
+            actions.addModel({ schemaQuery, id: 'model' }, true);
+        }
+    };
+
+    render() {
+        const { queryModels, actions } = this.props;
+        const { model } = queryModels;
+        const { schemaName, queryName, viewName } = this.state;
+        let body = (
+            <div>
+                Enter a Schema, Query, View
+            </div>
+        );
+
+        if (model !== undefined) {
+            body = <GridPanel actions={actions} model={model} />
+        }
+
+        return (
+            <div>
+                <div className="form-row">
+                    <div className="form-row__input">
+                        <label htmlFor="schemaName">Schema</label>
+                        <input id="schemaName" name="schemaName" type="text" value={schemaName} onChange={this.onFormChange}/>
+                    </div>
+
+                    <div className="form-row__input">
+                        <label htmlFor="queryName">Query</label>
+                        <input id="queryName" name="queryName" type="text" value={queryName} onChange={this.onFormChange}/>
+                    </div>
+
+                    <div className="form-row__input">
+                        <label htmlFor="viewName">View</label>
+                        <input id="viewName" name="viewName" type="text" value={viewName} onChange={this.onFormChange}/>
+                    </div>
+
+                    <div className="form-row__input">
+                        <Button onClick={this.applySchemaQuery}>Apply</Button>
+                    </div>
+                </div>
+
+                {body}
+            </div>
+        );
+    }
+}
+
+const ChangeableSchemaQuery = withQueryModels<{}>(ChangeableSchemaQueryImpl);
 
 storiesOf('QueryModel', module)
     .add('GridPanel', () => {
@@ -31,4 +129,7 @@ storiesOf('QueryModel', module)
                 <GridPanelWithModel queryConfigs={queryConfigs} ButtonsComponent={GridPanelButtonsExample} />
             </div>
         );
+    })
+    .add('Changeable SchemaQuery', () => {
+        return <ChangeableSchemaQuery />;
     });

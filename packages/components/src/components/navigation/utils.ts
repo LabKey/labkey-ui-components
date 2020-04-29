cfec168918bea8193ab5a3318f861d08763c9e03
labkey-susanh <susanh@labkey.com>
2020-04-29 08:16:06 -0700

    Add return type, preserve search string when constructing "cancel" URL and use replaceState instead of pushState so the usual cancel button will work after canceling here.

--- packages/components/src/components/navigation/utils.ts
+++ packages/components/src/components/navigation/utils.ts
@@ -1,26 +1,30 @@
 import { AppURL } from '../..';
 import { Location } from "history";
 
-export const ON_LEAVE_DIRTY_STATE_MESSAGE = 'You have unsaved changes which will be lost. Are you sure you want to continue?';
+export const ON_LEAVE_DIRTY_STATE_MESSAGE = 'You have unsaved changes that will be lost. Are you sure you want to continue?';
 
 /**
  * This function can be used as the callback for react-router's setRouteLeaveHook.  It should be preferred
  * over a callback that simply returns the confirm message because with react-router v3.x, the URL route
  * will have already been changed by the time this confirm is shown and will not be reset if the user does not confirm.
  * If the user tries to click on the initial link again after canceling, nothing will happen because the URL
- * in the browser will not change. (Issue 39633).  See also https://stackoverflow.com/questions/32841757/detecting-user-leaving-page-with-react-router
+ * in the browser will not change. (Issue 39633).
+ * See also https://stackoverflow.com/questions/32841757/detecting-user-leaving-page-with-react-router
  *
  * @param currentLocation the location of the current page
  */
-export function confirmLeaveWhenDirty(currentLocation: Location) {
+export function confirmLeaveWhenDirty(currentLocation: Location) : boolean {
     const result = confirm(ON_LEAVE_DIRTY_STATE_MESSAGE);
     if (result) {
         // navigation confirmed
         return true;
     } else {
         // navigation canceled, pushing the previous path
-        if (currentLocation)
-            window.history.pushState(null, null, AppURL.create(...currentLocation.pathname.substring(1).split("/")).toHref());
+        if (currentLocation) {
+            let appURL = AppURL.create(...decodeURI(currentLocation.pathname).substring(1).split("/"));
+            window.history.replaceState(null, null,  appURL.toHref() + currentLocation.search);
+        }
+
         return false;
     }
 }

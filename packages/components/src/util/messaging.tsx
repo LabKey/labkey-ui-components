141b42d1e3f9b6e1b787ab3a5975ac0d1159ca6d
labkey-susanh <susanh@labkey.com>
2020-04-29 08:14:22 -0700

    Use string template literals for composing error messages

--- packages/components/src/util/messaging.tsx
+++ packages/components/src/util/messaging.tsx
@@ -39,26 +39,20 @@ export function resolveErrorMessage(error: any, noun: string = undefined, nounPl
         if (
             lcMessage.indexOf('violates unique constraint') >= 0 ||
             lcMessage.indexOf('violation of unique key constraint') >= 0
-        )
-            return 'There was a problem ' + (verb || 'creating') + ' your ' + noun + '.  Check the existing ' + (nounPlural || noun) + ' for possible duplicates and make sure any referenced ' + (nounPlural || noun) + ' are still valid.';
-        else if (lcMessage.indexOf('bad sql grammar') >= 0) {
-            return 'There was a problem ' + (verb || 'creating') + ' your ' + noun + '.  Check that the format of the data matches the expected type for each field.';
-        }
-        else if (lcMessage.indexOf('existing row was not found') >= 0) {
-            return (
-                'We could not find the ' +
-                noun +
-                (verb ? ' to ' + verb : '') +
-                '.  Try refreshing your page to see if it has been deleted.'
-            );
+        ) {
+            return `There was a problem ${verb || 'creating'} your ${noun}.  Check the existing ${nounPlural || noun} for possible duplicates and make sure any referenced ${nounPlural || noun} are still valid.`;
+        } else if (lcMessage.indexOf('bad sql grammar') >= 0) {
+            return `There was a problem ${verb || 'creating'} your ${noun}.  Check that the format of the data matches the expected type for each field.`;
+        } else if (lcMessage.indexOf('existing row was not found') >= 0) {
+            return `We could not find the ${noun} + ${verb ? ' to ' + verb : ''}. Try refreshing your page to see if it has been deleted.`;
         } else if (
             lcMessage.indexOf('communication failure') >= 0 ||
             lcMessage.match(/query.*in schema.*doesn't exist/) !== null ||
             lcMessage.match(/query.*in schema.*does not exist/) !== null
         ) {
-            return 'There was a problem retrieving your ' + (noun || 'data') + '. Your session may have expired or the ' + (noun || 'data') + ' may no longer be valid.  Try refreshing your page.';
+            return `There was a problem retrieving your ${noun || 'data'}. Your session may have expired or the ${noun || 'data'} may no longer be valid.  Try refreshing your page.`;
         } else if (lcMessage.indexOf('either rowid or lsid is required') >= 0) {
-            return 'There was a problem retrieving or updating your ' + (noun || 'data') + '.  The request did not contain the proper identifiers.  Make sure the ' + (noun || 'data') + ' are still valid.';
+            return `There was a problem retrieving or updating your ${noun || 'data'}.  The request did not contain the proper identifiers.  Make sure the ${noun || 'data'} are still valid.`;
         } else if (lcMessage.indexOf(IllegalArgumentMessage) >= 0) {
             const startIndex = lcMessage.indexOf(IllegalArgumentMessage);
             return errorMsg.substring(startIndex + IllegalArgumentMessage.length);

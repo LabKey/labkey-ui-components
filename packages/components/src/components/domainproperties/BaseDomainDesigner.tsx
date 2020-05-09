f7883f15aa3384a93575521aa1446f0308006488
labkey-martyp <martyp@labkey.com>
2020-05-09 11:01:15 -0700

    Revert "Commit for testing"
    This reverts commit ed4f4c0264a680633fe63121389becd5099bdd8d.

--- packages/components/src/components/domainproperties/BaseDomainDesigner.tsx
+++ packages/components/src/components/domainproperties/BaseDomainDesigner.tsx
@@ -3,7 +3,7 @@ import { List } from 'immutable';
 
 import { Button } from 'react-bootstrap';
 
-import {Alert} from '../base/Alert';
+import { Alert } from '../base/Alert';
 
 import { getDomainBottomErrorMessage, getDomainHeaderName, getUpdatedVisitedPanelsList } from './actions';
 import { SEVERITY_LEVEL_ERROR } from './constants';
@@ -18,10 +18,8 @@ export interface InjectedBaseDomainDesignerProps {
     firstState: boolean;
     setSubmitting: (submitting: boolean, callback?: () => any) => any;
     onTogglePanel: (index: number, collapsed: boolean, callback: () => any) => any;
-    onFinish: (isValid: boolean, save: () => any) => any
+    onFinish: (isValid: boolean, save: () => any) => any;
 }
-
-
 
 interface State {
     submitting: boolean;
@@ -33,7 +31,7 @@ interface State {
 
 export function withBaseDomainDesigner<Props>(
     ComponentToWrap: ComponentType<Props & InjectedBaseDomainDesignerProps>
-) {
+): ComponentType<Props> {
     class ComponentWithBaseDomainDesigner extends PureComponent<Props, State> {
         constructor(props: Props) {
             super(props);

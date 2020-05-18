7ff18f271b544ecf4ecb45b057636b5395fd1c8a
Alan Vezina <alanv@labkey.com>
2020-05-18 11:30:54 -0500

    Add "Minimal GridPanel" story

--- packages/components/src/stories/QueryModel.tsx
+++ packages/components/src/stories/QueryModel.tsx
@@ -77,7 +77,7 @@ class ChangeableSchemaQueryImpl extends PureComponent<{} & InjectedQueryModels, 
         }
 
         return (
-            <div>
+            <div className="query-model-example">
                 <div className="form-row">
                     <div className="form-row__input">
                         <label htmlFor="schemaName">Schema</label>
@@ -123,8 +123,31 @@ storiesOf('QueryModel', module)
         };
 
         return (
-            <div style={{ marginTop: '2em' }}>
-                <GridPanelWithModel queryConfigs={queryConfigs} ButtonsComponent={GridPanelButtonsExample} />
+            <div className="query-model-example">
+                <GridPanelWithModel
+                    ButtonsComponent={GridPanelButtonsExample}
+                    title="Mixtures"
+                    queryConfigs={queryConfigs}
+                />
+            </div>
+        );
+    })
+    .add('Minimal GridPanel', () => {
+        const queryConfigs: QueryConfigMap = {
+            mixtures: {
+                schemaQuery: SchemaQuery.create('exp.data', 'mixturespaging'),
+            },
+        };
+
+        return (
+            <div className="query-model-example">
+                <GridPanelWithModel
+                    queryConfigs={queryConfigs}
+                    asPanel={false}
+                    showOmniBox={false}
+                    showButtonBar={false}
+                    allowSelections={false}
+                />
             </div>
         );
     })

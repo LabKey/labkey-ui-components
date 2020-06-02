c1d661c234983b5beffd0022268cfaedcd6d21a1
cnathe <cnathe@labkey.com>
2020-06-02 16:19:08 -0500

    FileTree snapshot test changes to be able to compare allowMultiSelect true/false

--- packages/components/src/components/files/FileTree.spec.tsx
+++ packages/components/src/components/files/FileTree.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
+import renderer from 'react-test-renderer';
 
 import { FileTree } from './FileTree';
 import { fetchFileTestTree } from './FileTreeTest';
@@ -8,8 +8,10 @@ import { fetchFileTestTree } from './FileTreeTest';
 const waitForLoad = jest.fn(component => Promise.resolve(!component.state().loading));
 
 describe('FileTree', () => {
+
     test('with data', () => {
-        const tree = shallow(<FileTree loadData={fetchFileTestTree} onFileSelect={jest.fn()} />);
+        const component = <FileTree loadData={fetchFileTestTree} onFileSelect={jest.fn()} />;
+        const tree = shallow(component);
 
         return waitForLoad(tree).then(() => {
             const node = tree.childAt(0).dive().childAt(0).dive().find('NodeHeader');
@@ -19,7 +21,25 @@ describe('FileTree', () => {
 
             return waitForLoad(tree).then(() => {
                 expect(tree.state()['data']['children'].length).toEqual(4);
-                expect(toJson(tree)).toMatchSnapshot();
+                expect(renderer.create(tree.getElement()).toJSON()).toMatchSnapshot();
+                tree.unmount();
+            });
+        });
+    });
+
+    test('with data allowMultiSelect false', () => {
+        const component = <FileTree allowMultiSelect={false} loadData={fetchFileTestTree} onFileSelect={jest.fn()} />;
+        const tree = shallow(component);
+
+        return waitForLoad(tree).then(() => {
+            const node = tree.childAt(0).dive().childAt(0).dive().find('NodeHeader');
+            expect(node.prop('node')['children'].length).toEqual(0);
+
+            node.simulate('click');
+
+            return waitForLoad(tree).then(() => {
+                expect(tree.state()['data']['children'].length).toEqual(4);
+                expect(renderer.create(tree.getElement()).toJSON()).toMatchSnapshot();
                 tree.unmount();
             });
         });

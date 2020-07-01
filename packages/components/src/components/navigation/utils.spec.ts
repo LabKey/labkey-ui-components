9411a50239794545a37d4a4f38233e6a26cb00b1
cnathe <cnathe@labkey.com>
2020-07-01 12:43:45 -0500

    createProductUrl jest tests

--- packages/components/src/components/navigation/utils.spec.ts
+++ packages/components/src/components/navigation/utils.spec.ts
@@ -1,4 +1,5 @@
-import { createProductUrlFromParts } from './utils';
+import { createProductUrlFromParts, createProductUrl } from './utils';
+import { AppURL } from "../..";
 
 describe('createProductUrlFromParts', () => {
     test('no productId', () => {
@@ -29,5 +30,45 @@ describe('createProductUrlFromParts', () => {
     test('with multiple parts', () => {
         const url = createProductUrlFromParts(undefined, 'currentProduct', { rowId: 42 }, 'destination', 'mars');
         expect(url.toString()).toEqual('/destination/mars?rowId=42');
+    });
+});
+
+describe('createProductUrl', () => {
+    test('no productId', () => {
+        const url = createProductUrl(undefined, 'currentProduct', AppURL.create('destination'));
+        expect(url.toString()).toEqual('/destination');
+    });
+
+    test('no currentProductId', () => {
+        const url = createProductUrl('urlProduct', undefined, AppURL.create('destination').addParam('rowId', 123));
+        expect(url).toEqual('/labkey/urlproduct/app.view#/destination?rowId=123');
+    });
+
+    test('not currentProductId', () => {
+        const url = createProductUrl('urlProduct', 'currentProduct', AppURL.create('destination').addParam('rowId', 123));
+        expect(url).toEqual('/labkey/urlproduct/app.view#/destination?rowId=123');
+    });
+
+    test('is current product', () => {
+        const url = createProductUrl('currentProduct', 'currentProduct', AppURL.create('destination').addParam('rowId', 123));
+        expect(url.toString()).toEqual('/destination?rowId=123');
+    });
+
+    test('with multiple params', () => {
+        const url = createProductUrl(undefined, 'currentProduct', AppURL.create('destination').addParam('rowId', 123).addParam('view', 'grid'));
+        expect(url.toString()).toEqual('/destination?rowId=123&view=grid');
+    });
+
+    test('with multiple parts', () => {
+        const url = createProductUrl(undefined, 'currentProduct', AppURL.create('destination', 'mars').addParam('rowId', 42));
+        expect(url.toString()).toEqual('/destination/mars?rowId=42');
+    });
+
+    test('as url string', () => {
+        let url = createProductUrl(undefined, 'currentProduct', '#/destination?rowId=123');
+        expect(url.toString()).toEqual('#/destination?rowId=123');
+
+        url = createProductUrl('urlProduct', 'currentProduct', '#/destination?rowId=123');
+        expect(url.toString()).toEqual('/labkey/urlproduct/app.view#/destination?rowId=123');
     });
 });

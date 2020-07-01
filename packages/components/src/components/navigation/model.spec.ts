ee630e4eafbcb26bd52917ea0eb65712bc751d10
cnathe <cnathe@labkey.com>
2020-07-01 12:53:12 -0500

    MenuItemModel.create jest tests

--- packages/components/src/components/navigation/model.spec.ts
+++ packages/components/src/components/navigation/model.spec.ts
@@ -15,7 +15,7 @@
  */
 import { List } from 'immutable';
 
-import { MenuSectionModel, ProductMenuModel } from './model';
+import { MenuItemModel, MenuSectionModel, ProductMenuModel } from './model';
 
 describe('ProductMenuModel', () => {
     const testSectionKey = 'test';
@@ -44,13 +44,13 @@ describe('ProductMenuModel', () => {
     });
 
     test('hasSectionItems not loaded', () => {
-        const model = new ProductMenuModel({ productIds: ['hasSectionItems'] });
+        const model = new ProductMenuModel({productIds: ['hasSectionItems']});
         expect(model.hasSectionItems(testSectionKey)).toBeFalsy();
         expect(model.hasSectionItems(emptySectionKey)).toBeFalsy();
     });
 
     test('hasSectionItems key match', () => {
-        let model = new ProductMenuModel({ productIds: ['hasSectionItems'] });
+        let model = new ProductMenuModel({productIds: ['hasSectionItems']});
         model = model.setLoadedSections(
             List<MenuSectionModel>([testSection, emptySection])
         );
@@ -59,10 +59,30 @@ describe('ProductMenuModel', () => {
     });
 
     test('hasSectionItems empty section', () => {
-        let model = new ProductMenuModel({ productIds: ['hasSectionItems'] });
+        let model = new ProductMenuModel({productIds: ['hasSectionItems']});
         model = model.setLoadedSections(
             List<MenuSectionModel>([testSection, emptySection])
         );
         expect(model.hasSectionItems(emptySectionKey)).toBeFalsy();
+    });
+});
+
+describe('MenuItemModel', () => {
+    test('currentProductId param match', () => {
+        const model = MenuItemModel.create({
+            productId: 'product1',
+            url: '#/menuItem'
+        }, 'sectionKey', 'product1');
+
+        expect(model.url).toBe('#/menuItem');
+    });
+
+    test('currentProductId param mismatch', () => {
+        const model = MenuItemModel.create({
+            productId: 'product2',
+            url: '#/menuItem'
+        }, 'sectionKey', 'product1');
+
+        expect(model.url).toBe('/labkey/product2/app.view#/menuItem');
     });
 });

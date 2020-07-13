73ca580803dc09902f34c871ac6fa1e8a30cdcb5
cnathe <cnathe@labkey.com>
2020-07-13 08:40:29 -0500

    ColorIcon - add label property

--- packages/components/src/components/base/ColorIcon.tsx
+++ packages/components/src/components/base/ColorIcon.tsx
@@ -4,22 +4,35 @@ interface Props {
     cls?: string;
     value: string;
     asSquare?: boolean;
+    label?: string;
 }
 
 export class ColorIcon extends PureComponent<Props> {
+    private defaultProps = {
+        cls: 'color-picker__chip',
+    };
+
     render(): ReactNode {
-        const { cls, value, asSquare } = this.props;
+        const { cls, value, asSquare, label } = this.props;
         const isWhite = value?.toLowerCase() === '#ffffff';
         const border = isWhite ? 'solid 1px #000000' : undefined;
 
-        if (asSquare) {
-            return <i className={cls} style={{ backgroundColor: value, border }} />;
+        let icon;
+        if (value) {
+            icon = <i className="fa fa-circle" style={{ color: value }} />;
+        } else if (isWhite) {
+            icon = <i className="fa fa-circle-thin" />;
         }
 
-        if (isWhite) {
-            return <i className="fa fa-circle-thin" />;
+        if (value && asSquare) {
+            icon = <i className={cls} style={{ backgroundColor: value, border }} />;
         }
 
-        return <i className="fa fa-circle" style={{ color: value }} />;
+        return (
+            <>
+                {icon}
+                {label && <span className={value ? 'spacer-left' : undefined}>{label}</span>}
+            </>
+        );
     }
 }

906c9372bf47ebf1665f58c64af6f4bafa1b1b23
labkey-martyp <martyp@labkey.com>
2020-09-16 10:14:27 -0700

    v0.92.2-fb-migrate-assay-delete.3

---
+++ packages/components/src/components/assay/AssayResultDeleteModal.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react'
+import { useState } from 'react'
+import {
+    AssayResultDeleteConfirmModal,
+    ConfirmModal,
+    deleteRows,
+    Progress,
+    createDeleteErrorNotification,
+    createDeleteSuccessNotification,
+    SchemaQuery,
+} from '@labkey/components';
+
+interface Props {
+    afterDelete: () => any;
+    afterDeleteFailure: () => any;
+    maxToDelete?: number;
+    onCancel: () => any;
+    schemaQuery: SchemaQuery;
+    selectedIds: string[];
+}
+
+export function AssayResultDeleteModal(props: Props) {
+    const { onCancel, afterDelete, afterDeleteFailure, maxToDelete, schemaQuery, selectedIds } = props;
+    const [showProgress, setShowProgress] = useState<boolean>();
+    const numToDelete = selectedIds.length;
+    const noun = numToDelete ? ' assay result' : ' assay results';
+
+    function onConfirm() {
+        setShowProgress(true);
+        const rows = selectedIds.map((id) => ({'RowId': id}));
+        deleteRows({
+            schemaQuery,
+            rows
+        }).then((response) => {
+            afterDelete();
+            createDeleteSuccessNotification(noun, numToDelete);
+        }).catch( (reason) => {
+            console.error(reason);
+            setShowProgress(false);
+            createDeleteErrorNotification(noun);
+            afterDeleteFailure();
+        });
+    }
+
+    if (maxToDelete && numToDelete > maxToDelete) {
+        return (
+            <ConfirmModal
+                title={"Cannot Delete Assay Results"}
+                onCancel={onCancel}
+                msg={"You cannot delete more than " + maxToDelete + " individual assay results at a time.  "
+                + " Please select fewer results and try again."}
+                onConfirm={undefined}
+                cancelButtonText={"Dismiss"}
+            />
+        )
+    }
+
+    return (
+        <>
+            {!showProgress &&
+                <AssayResultDeleteConfirmModal
+                    numToDelete={numToDelete}
+                    onConfirm={onConfirm}
+                    onCancel={onCancel}
+                />
+            }
+            <Progress
+                modal={true}
+                estimate={numToDelete * 10}
+                title={'Deleting ' + numToDelete + noun}
+                toggle={showProgress}
+            />
+        </>
+    )
+}

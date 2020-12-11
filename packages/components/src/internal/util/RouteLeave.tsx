27bd1869900b8aebf076ea93d2f7a273d4e0b3f7
ian <ians@labkey.com>
2020-12-11 12:28:21 -0800

    Clean up some lint warnings

--- packages/components/src/internal/util/RouteLeave.tsx
+++ packages/components/src/internal/util/RouteLeave.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Location} from 'history';
+import { Location } from 'history';
 import { WithRouterProps } from 'react-router';
 
 import { AppURL } from '../..';
@@ -23,17 +23,12 @@ export const ON_LEAVE_DIRTY_STATE_MESSAGE =
  * @param currentLocation the location of the current page
  * @param event object triggering leave transition
  */
-export function confirmLeaveWhenDirty(currentLocation: Location, event?: any,): boolean {
+export function confirmLeaveWhenDirty(currentLocation: Location, event?: any): boolean {
     const result = confirm(ON_LEAVE_DIRTY_STATE_MESSAGE);
 
     // navigation canceled, pushing the previous path
     if (!result && currentLocation) {
-        const appURL = AppURL.create(
-            ...currentLocation.pathname
-                .substring(1)
-                .split('/')
-                .map(decodeURIComponent)
-        );
+        const appURL = AppURL.create(...currentLocation.pathname.substring(1).split('/').map(decodeURIComponent));
 
         // Issue 41419: handle different cases for route leave PUSH vs POP events
         if (event?.action !== 'POP') {
@@ -59,23 +54,24 @@ export type RouteLeaveProps = RouteLeaveInjectedProps & WithRouterProps;
  */
 export const withRouteLeave = (Component: React.ComponentType) => {
     return class RouteLeaveHOCImpl extends React.Component<RouteLeaveProps> {
-        _dirty = false;
-
         componentDidMount(): void {
             // attach the hook to the current route, which will be the last index of the routes prop
             this.props.router.setRouteLeaveHook(this.props.routes[this.props.routes.length - 1], this.onRouteLeave);
         }
 
-        onBeforeUnload = event => {
+        _dirty = false;
+
+        onBeforeUnload = (event): string => {
             event.returnValue = ON_LEAVE_DIRTY_STATE_MESSAGE;
             return ON_LEAVE_DIRTY_STATE_MESSAGE;
         };
 
-        onRouteLeave = event => {
+        onRouteLeave = (event): boolean => {
             if (this._dirty) {
                 event.returnValue = true; // this is for the page reload case
                 return confirmLeaveWhenDirty(this.props.location, event);
             }
+            return false;
         };
 
         setDirty = (dirty: boolean): void => {

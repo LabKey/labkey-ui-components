c4f9f6302c9e3b936fabcfff30a378235cc0b9af
labkey-nicka <nickk@labkey.com>
2020-12-23 16:10:00 -0800

    Update storybook configuration

---
+++ packages/components/.storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+    stories: [
+        '../src/stories/AddEntityButton.stories.tsx',
+        '../src/stories/AssayDesignDeleteConfirmModal.stories.tsx',
+        '../src/stories/Cards.stories.tsx',
+        '../src/stories/ColorIcon.stories.tsx',
+        '../src/stories/ColorPickerInput.stories.tsx',
+        '../src/stories/CreateUsersModal.stories.tsx',
+        '../src/stories/DataClassDesigner.stories.tsx',
+        '../src/stories/DomainFieldsDisplay.stories.tsx',
+        '../src/stories/OmniBox.stories.tsx',
+        '../src/stories/UserDetailHeader.stories.tsx',
+        '../src/stories/WizardNavButtons.stories.tsx',
+        // '../src/stories/**/*.stories.@(ts|tsx)'
+    ],
+    addons: [
+        '@storybook/addon-essentials',
+    ],
+}
---
+++ packages/components/.storybook/preview.js
@@ -0,0 +1,5 @@
+import '../src/stories/stories.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}

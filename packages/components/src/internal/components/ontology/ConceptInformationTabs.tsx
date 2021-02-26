6a7dd2e8d60df76af24e8808e390d0d77d3fe2f1
cnathe <cnathe@labkey.com>
2021-02-26 10:50:37 -0600

    Jest tests for ConceptInformationTabs

--- packages/components/src/internal/components/ontology/ConceptInformationTabs.tsx
+++ packages/components/src/internal/components/ontology/ConceptInformationTabs.tsx
@@ -8,7 +8,7 @@ interface ConceptInformationTabsProps {
     concept?: ConceptModel;
 }
 
-const enum ConceptInfoTabs {
+export const enum ConceptInfoTabs {
     CONCEPT_OVERVIEW_TAB = 'overview',
     PATH_INFO_TAB = 'pathinfo',
 }
@@ -26,41 +26,37 @@ export const ConceptInformationTabs: FC<ConceptInformationTabsProps> = memo(prop
     );
 
     return (
-        <>
-            <div>
-                <Tab.Container
-                    id="concept-information-tabs"
-                    className="concept-information-tabs"
-                    onSelect={onSelectionChange}
-                    activeKey={activeTab}
-                    defaultActiveKey={ConceptInfoTabs.CONCEPT_OVERVIEW_TAB}
-                >
-                    <Row className="clearfix">
-                        <Col>
-                            <Nav bsStyle="tabs">
-                                <NavItem eventKey={ConceptInfoTabs.CONCEPT_OVERVIEW_TAB}>Overview</NavItem>
-                                <NavItem eventKey={ConceptInfoTabs.PATH_INFO_TAB}>Path Information</NavItem>
-                            </Nav>
-                        </Col>
-                        <Col>
-                            <Tab.Content animation>
-                                <Tab.Pane
-                                    className="ontology-concept-overview-container"
-                                    eventKey={ConceptInfoTabs.CONCEPT_OVERVIEW_TAB}
-                                >
-                                    <ConceptOverviewPanelImpl concept={concept} />
-                                </Tab.Pane>
-                                <Tab.Pane
-                                    className="ontology-concept-pathinfo-container"
-                                    eventKey={ConceptInfoTabs.PATH_INFO_TAB}
-                                >
-                                    <div className="placeholder">Coming soon...</div>
-                                </Tab.Pane>
-                            </Tab.Content>
-                        </Col>
-                    </Row>
-                </Tab.Container>
-            </div>
-        </>
+        <Tab.Container
+            id="concept-information-tabs"
+            className="concept-information-tabs"
+            onSelect={onSelectionChange}
+            activeKey={activeTab}
+            defaultActiveKey={ConceptInfoTabs.CONCEPT_OVERVIEW_TAB}
+        >
+            <Row className="clearfix">
+                <Col>
+                    <Nav bsStyle="tabs">
+                        <NavItem eventKey={ConceptInfoTabs.CONCEPT_OVERVIEW_TAB}>Overview</NavItem>
+                        <NavItem eventKey={ConceptInfoTabs.PATH_INFO_TAB}>Path Information</NavItem>
+                    </Nav>
+                </Col>
+                <Col>
+                    <Tab.Content animation>
+                        <Tab.Pane
+                            className="ontology-concept-overview-container"
+                            eventKey={ConceptInfoTabs.CONCEPT_OVERVIEW_TAB}
+                        >
+                            <ConceptOverviewPanelImpl concept={concept} />
+                        </Tab.Pane>
+                        <Tab.Pane
+                            className="ontology-concept-pathinfo-container"
+                            eventKey={ConceptInfoTabs.PATH_INFO_TAB}
+                        >
+                            <div className="placeholder">Coming soon...</div>
+                        </Tab.Pane>
+                    </Tab.Content>
+                </Col>
+            </Row>
+        </Tab.Container>
     );
 });

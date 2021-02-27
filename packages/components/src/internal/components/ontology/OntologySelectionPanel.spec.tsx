652d8048e7e9ac320cc616945fc125bbdab5de23
cnathe <cnathe@labkey.com>
2021-02-27 12:14:26 -0600

    OntologySelectionPanel jest test fix for activeModule check

--- packages/components/src/internal/components/ontology/OntologySelectionPanel.spec.tsx
+++ packages/components/src/internal/components/ontology/OntologySelectionPanel.spec.tsx
@@ -29,6 +29,7 @@ describe('OntologySelectionPanel', () => {
     });
 
     test('no ontologies, non root admin', () => {
+        LABKEY.container.activeModules = ['Core', 'Query', 'Ontology'];
         const ontologies = [];
         const wrapper = mount(<OntologySelectionPanelImpl {...DEFAULT_PROPS} ontologies={ontologies} />);
         validate(wrapper, 0);
@@ -38,6 +39,17 @@ describe('OntologySelectionPanel', () => {
     });
 
     test('no ontologies, as root admin', () => {
+        LABKEY.container.activeModules = ['Core', 'Query', 'Ontology'];
+        LABKEY.user.isRootAdmin = true;
+        const ontologies = [];
+        const wrapper = mount(<OntologySelectionPanelImpl {...DEFAULT_PROPS} ontologies={ontologies} />);
+        validate(wrapper, 0);
+        expect(wrapper.find('.alert-warning').text()).toContain('Click here to get started.');
+        wrapper.unmount();
+    });
+
+    test('no ontologies, as root admin without ontology module', () => {
+        LABKEY.container.activeModules = ['Core', 'Query'];
         LABKEY.user.isRootAdmin = true;
         const ontologies = [];
         const wrapper = mount(<OntologySelectionPanelImpl {...DEFAULT_PROPS} ontologies={ontologies} />);

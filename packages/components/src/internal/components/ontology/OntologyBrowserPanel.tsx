70140e1d94bac807647a5a7c35d82c331d614091
cnathe <cnathe@labkey.com>
2021-03-04 16:53:47 -0600

    OntologyBrowserPanel initial support for search WIP - move ontology details to panel header info icon -

--- packages/components/src/internal/components/ontology/OntologyBrowserPanel.tsx
+++ packages/components/src/internal/components/ontology/OntologyBrowserPanel.tsx
@@ -1,13 +1,16 @@
-import React, { FC, memo, useCallback, useEffect, useState } from 'react';
+import React, { ChangeEvent, FC, memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 
-import { Alert, LoadingSpinner } from '../../..';
+import { Alert, LabelHelpTip, LoadingSpinner, searchUsingIndex } from '../../..';
 
 import { fetchConceptForCode, getOntologyDetails } from './actions';
 import { ConceptModel, OntologyModel, PathModel } from './models';
 import { ConceptInformationTabs } from './ConceptInformationTabs';
 import { OntologyTreePanel } from './OntologyTreePanel';
 import { OntologySelectionPanel } from './OntologySelectionPanel';
+
+const CONCEPT_CATEGORY = 'concept';
+const SEARCH_LIMIT = 10;
 
 export interface OntologyBrowserProps {
     initOntologyId?: string;
@@ -113,18 +116,18 @@ export const OntologyBrowserPanelImpl: FC<OntologyBrowserPanelImplProps> = memo(
     const root = ontology.getPathModel();
 
     const body = (
-        <>
-            {description && <p className="ontology-description">{description}</p>}
-            <Row>
-                <Col xs={6} className="left-panel">
-                    <p className="ontology-concept-count">{conceptCount} total concepts</p>
-                    <OntologyTreePanel root={root} onNodeSelection={setSelectedConcept} />
-                </Col>
-                <Col xs={6} className="right-panel">
-                    <ConceptInformationTabs concept={selectedConcept} />
-                </Col>
-            </Row>
-        </>
+        <Row>
+            <Col xs={6} className="left-panel">
+                <OntologyTreeSearchContainer ontology={ontology} />
+                <OntologyTreePanel
+                    root={root}
+                    onNodeSelection={setSelectedConcept}
+                />
+            </Col>
+            <Col xs={6} className="right-panel">
+                <ConceptInformationTabs concept={selectedConcept} />
+            </Col>
+        </Row>
     );
 
     if (!asPanel) {
@@ -133,8 +136,108 @@ export const OntologyBrowserPanelImpl: FC<OntologyBrowserPanelImplProps> = memo(
 
     return (
         <div className="panel panel-default ontology-browser-container">
-            <div className="panel-heading">Browse {ontology.getDisplayName()}</div>
+            <div className="panel-heading">
+                Browse {ontology.getDisplayName()}
+                &nbsp;
+                <LabelHelpTip
+                    title="Ontology Details"
+                    placement="bottom"
+                    iconComponent={<i className="fa fa-info-circle" />}
+                >
+                    {description && <p className="ontology-description">{description}</p>}
+                    <p className="ontology-concept-count">{Number(conceptCount).toLocaleString()} total concepts</p>
+                </LabelHelpTip>
+            </div>
             <div className="panel-body">{body}</div>
+        </div>
+    );
+});
+
+// TODO move this to separate file
+interface OntologyTreeSearchContainerProps {
+    ontology: OntologyModel,
+}
+
+export const OntologyTreeSearchContainer: FC<OntologyTreeSearchContainerProps> = memo(props => {
+    const { ontology } = props;
+    const [isFocused, setIsFocused] = useState<boolean>();
+    const [searchTerm, setSearchTerm] = useState<string>();
+    const [searchHits, setSearchHits] = useState<ConceptModel[]>();
+    const [totalHits, setTotalHits] = useState<number>();
+    const [error, setError] = useState<string>();
+    const showMenu = useMemo(() => isFocused && (searchHits !== undefined || error !== undefined), [isFocused, searchHits, error]);
+
+    const onSearchChange = useCallback(
+        (evt: ChangeEvent<HTMLInputElement>) => {
+            const { value } = evt.currentTarget;
+            setSearchTerm(value?.length > 2 ? value : undefined);
+        },
+        [setSearchTerm]
+    );
+
+    const onSearchFocus = useCallback(() => { setIsFocused(true); }, [setIsFocused]);
+    const onSearchBlur = useCallback(() => { setIsFocused(false); }, [onSearchFocus]);
+
+    useEffect(() => {
+        setError(undefined);
+        setTotalHits(undefined);
+        if (!searchTerm) {
+            setSearchHits(undefined);
+        } else {
+            const timeOutId = setTimeout(() => {
+                searchUsingIndex({ q: searchTerm, category: CONCEPT_CATEGORY, limit: SEARCH_LIMIT }, undefined, [CONCEPT_CATEGORY])
+                    .then(response => {
+                        setSearchHits(
+                            response.hits.map(hit => {
+                                return new ConceptModel({
+                                    code: hit.identifiers,
+                                    label: hit.title,
+                                    description: hit.summary.split('\n')[1], // format is "<code> <label>\n<description>" see ConceptDocumentProvider
+                                });
+                            })
+                        );
+                        setTotalHits(response.totalHits);
+                    })
+                    .catch(reason => {
+                        setError('Error: unable to get search results. ' + reason?.exception);
+                        setSearchHits(undefined);
+                    });
+            }, 500);
+
+            return () => clearTimeout(timeOutId);
+        }
+    }, [searchTerm, setError, setSearchHits, setTotalHits]);
+
+    return (
+        <div className="concept-search-container">
+            <form autoComplete="off">
+                <input
+                    type="text"
+                    className="form-control"
+                    name="concept-search"
+                    placeholder={'Search ' + ontology.abbreviation}
+                    onChange={onSearchChange}
+                    onFocus={onSearchFocus}
+                    onBlur={onSearchBlur}
+                />
+            </form>
+            {showMenu && (
+                <div>
+                    <ul className="result-menu">
+                        <Alert>{error}</Alert>
+                        {searchHits === undefined && <li key="none">No search results found.</li>}
+                        {searchHits?.map(hit => (
+                            <li key={hit.code}>{hit.label}</li>
+                        ))}
+                        {searchHits?.length < totalHits && (
+                            <div>
+                                More then {SEARCH_LIMIT} results found. Update your search term to further refine your
+                                results.
+                            </div>
+                        )}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 });

ed41f820109d90147b14b98a97f9cbb2324890f3
Marty Pradere <martyp@labkey.com>
2021-03-16 18:23:18 -0700

    Updates for Workflow Migration into Biologics (#455)
    * Add `<DisabledMenuItem/>`.
    * Update assays model getDefinitionsByTypes to list included/excluded types
    * Add isBiologicsEnables and isFreezerManagerEnabledInBiologics checks

---
+++ packages/components/src/internal/components/menus/DisabledMenuItem.tsx
@@ -0,0 +1,20 @@
+import React, { FC, memo } from 'react';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
+
+interface props {
+    idPrefix: string;
+    pluralNoun: string;
+    item: React.ReactNode;
+}
+
+export const DisabledMenuItem: FC<props> = memo(props => {
+    const { idPrefix, pluralNoun, item } = props;
+
+    const overlay = <Popover id={idPrefix + '-disabled-warning'}>Select one or more {pluralNoun}.</Popover>;
+
+    return (
+        <OverlayTrigger overlay={overlay} placement="right">
+            {item}
+        </OverlayTrigger>
+    );
+});

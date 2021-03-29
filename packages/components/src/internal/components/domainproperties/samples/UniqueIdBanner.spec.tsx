de26f087547103ffd97b39b1f77543f62b7bfc11
labkey-susanh <susanh@labkey.com>
2021-03-29 08:26:23 -0700

    v2.18.0-generate-unique.10

---
+++ packages/components/src/internal/components/domainproperties/samples/UniqueIdBanner.spec.tsx
@@ -0,0 +1,19 @@
+describe("<UniqueIdBanner/>", () => {
+    test("new in properties panel, no uniqueID fields", () => {
+
+    });
+
+    test("new in fields panel, no uniqueID rields", () => {
+
+    });
+
+    test("new in properties panel, with uniqueID fields", () => {
+
+    });
+
+    test("new in fields panel, with uniqueID fields", () => {
+
+    });
+
+
+})

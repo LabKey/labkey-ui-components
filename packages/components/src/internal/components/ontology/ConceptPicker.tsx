929d1421ae90e256cb78d2c59ecfd3ea0158a2fa
ian <ians@labkey.com>
2021-05-25 13:32:56 -0700

    v2.33.0-fb-conceptdisplaycolumn.0

---
+++ packages/components/src/internal/components/ontology/ConceptPicker.tsx
@@ -0,0 +1,58 @@
+import React, { FC, memo, useCallback, useEffect, useState } from 'react';
+import { OntologyBrowserModal } from './OntologyBrowserModal';
+import { ConceptModel } from './models';
+import { fetchConceptForCode } from './actions';
+import { LoadingSpinner } from '../../../index';
+
+interface Props {
+    ontologyId: string;
+    fieldName: string;
+    fieldLabel: string;
+    fieldValue?: string;
+    onConceptSelection?: (concept: ConceptModel) => void
+}
+
+export const ConceptPicker: FC<Props> = memo( (props:Props) => {
+    const {ontologyId, fieldLabel, onConceptSelection, fieldValue = '' } = props;
+    const [concept, setConcept] = useState<ConceptModel>();
+    const [showPicker, setShowPicker] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (fieldValue) {
+            fetchConceptForCode(fieldValue)
+                .then(setConcept)
+                .catch(() => {
+                    // If concept not found clear existing concept, unfound values are allowed if not recommended
+                    setConcept(null);
+                })
+            ;
+        }
+    }, [fieldValue]);
+
+    const togglePicker = useCallback(() => {
+        setShowPicker(!showPicker);
+    },[showPicker, setShowPicker]);
+
+    const onApplyConcept = useCallback(
+        (selectedConcept: ConceptModel) => {
+            setConcept(selectedConcept);
+            setShowPicker(false);
+            onConceptSelection(selectedConcept);
+        },
+        [setConcept, setShowPicker, onConceptSelection]
+    );
+
+    const label = concept?.label ?? null;
+
+    return (
+        <>
+            <div className="concept-label-text">{label}</div>
+            <div>
+                <a className='show-toggle' onClick={togglePicker}>{`Find ${fieldLabel} By Tree`}</a>
+                {showPicker && (
+                    <OntologyBrowserModal initOntologyId={ontologyId} onApply={onApplyConcept} onCancel={togglePicker} title={"Ontology Browser"} initConcept={concept} />
+                )}
+            </div>
+        </>
+    );
+});

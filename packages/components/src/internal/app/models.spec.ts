3ec7681203283bef7e94e4d2cd797719556e1b5d
cnathe <cnathe@labkey.com>
2021-05-26 09:43:43 -0500

    Remove AppModel getLogoutTitle() and getLogoutReason()

--- packages/components/src/internal/app/models.spec.ts
+++ packages/components/src/internal/app/models.spec.ts
@@ -4,7 +4,7 @@
  */
 import { TEST_USER_GUEST, TEST_USER_READER } from '../../test/data/users';
 
-import { AppModel, LogoutReason } from './models';
+import { AppModel } from './models';
 
 describe('AppModel', () => {
     test('hasUserChanged', () => {
@@ -23,27 +23,5 @@ describe('AppModel', () => {
         expect(model.shouldReload()).toBeFalsy();
         model = new AppModel({ reloadRequired: true, initialUserId: TEST_USER_READER.id, user: TEST_USER_READER });
         expect(model.shouldReload()).toBeTruthy();
-    });
-
-    test('getLogoutTitle', () => {
-        let model = new AppModel({});
-        expect(model.getLogoutTitle()).toBe(undefined);
-        model = new AppModel({ logoutReason: LogoutReason.SERVER_LOGOUT });
-        expect(model.getLogoutTitle()).toBe('Logged Out');
-        model = new AppModel({ logoutReason: LogoutReason.SESSION_EXPIRED });
-        expect(model.getLogoutTitle()).toBe('Session Expired');
-        model = new AppModel({ logoutReason: LogoutReason.SERVER_UNAVAILABLE });
-        expect(model.getLogoutTitle()).toBe('Server Unavailable');
-    });
-
-    test('getLogoutReason', () => {
-        let model = new AppModel({});
-        expect(model.getLogoutReason()).toBe(undefined);
-        model = new AppModel({ logoutReason: LogoutReason.SERVER_LOGOUT });
-        expect(model.getLogoutReason()).toContain('logged out');
-        model = new AppModel({ logoutReason: LogoutReason.SESSION_EXPIRED });
-        expect(model.getLogoutReason()).toContain('session has expired');
-        model = new AppModel({ logoutReason: LogoutReason.SERVER_UNAVAILABLE });
-        expect(model.getLogoutReason()).toContain('server is currently unavailable');
     });
 });

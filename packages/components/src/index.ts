99b101fafa1c0b9392ede76539ca2fdbdac60a4f
cnathe <cnathe@labkey.com>
2021-05-28 14:26:33 -0500

    yarn run lint-branch-fix

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -357,7 +357,11 @@ import {
     getDataDeleteConfirmationData,
     getSampleDeleteConfirmationData,
 } from './internal/components/entities/actions';
-import { DataClassDataType, SampleTypeDataType, ParentEntityRequiredColumns } from './internal/components/entities/constants';
+import {
+    DataClassDataType,
+    SampleTypeDataType,
+    ParentEntityRequiredColumns,
+} from './internal/components/entities/constants';
 import { createEntityParentKey, getUniqueIdColumnMetadata } from './internal/components/entities/utils';
 import { SampleTypeModel } from './internal/components/domainproperties/samples/models';
 

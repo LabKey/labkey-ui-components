400f45ba94b3ac29a212317e3034139188a444fd
XingY <xyang@labkey.com>
2021-07-02 12:15:19 -0700

    jest test

---
+++ packages/components/src/internal/components/samples/SharedSampleTypeEditConfirmModal.spec.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount } from "enzyme";
+import { SharedSampleTypeEditConfirmModal}  from "./SharedSampleTypeEditConfirmModal";
+
+
+describe("<SharedSampleTypeEditConfirmModal/>", () => {
+
+    test("isEdit true", () => {
+        const sampleTypeName = 'sharedType';
+        const wrapper = mount(
+            <SharedSampleTypeEditConfirmModal
+                sampleTypeId={100}
+                sampleTypeLabel={sampleTypeName}
+                onCancel={jest.fn()}
+                isEdit={true}
+            />
+        );
+        expect(wrapper.find("ModalTitle").text()).toBe("You are about to leave the application, continue?");
+        expect(wrapper.find('.modal-body').text()).toBe('Shared sample type \'' + sampleTypeName + '\' can only be modified in LabKey Server. Do you want to proceed?');
+        wrapper.unmount();
+    });
+
+    test("isEdit false", () => {
+        const sampleTypeName = 'sharedType-1';
+        const wrapper = mount(
+            <SharedSampleTypeEditConfirmModal
+                sampleTypeId={100}
+                sampleTypeLabel={sampleTypeName}
+                onCancel={jest.fn()}
+                isEdit={false}
+            />
+        );
+        expect(wrapper.find("ModalTitle").text()).toBe("You are about to leave the application, continue?");
+        expect(wrapper.find('.modal-body').text()).toBe('Shared sample type \'' + sampleTypeName + '\' can only be deleted in LabKey Server. Do you want to proceed?');
+        wrapper.unmount();
+    });
+
+})

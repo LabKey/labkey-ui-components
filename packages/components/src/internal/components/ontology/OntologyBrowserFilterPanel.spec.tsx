335b6415961c12c8fe668f056f696eba420ceeff
cnathe <cnathe@labkey.com>
2021-07-14 10:07:11 -0500

    yarn run lint-branch-fix

--- packages/components/src/internal/components/ontology/OntologyBrowserFilterPanel.spec.tsx
+++ packages/components/src/internal/components/ontology/OntologyBrowserFilterPanel.spec.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { OntologyBrowserFilterPanel } from './OntologyBrowserFilterPanel';
+
 import { Filter } from '@labkey/api';
 import { mount, ReactWrapper } from 'enzyme';
-import { OntologyBrowserPanel } from './OntologyBrowserPanel';
+
 import { Alert } from '../base/Alert';
 import { waitForLifecycle } from '../../testHelpers';
+
+import { OntologyBrowserPanel } from './OntologyBrowserPanel';
+import { OntologyBrowserFilterPanel } from './OntologyBrowserFilterPanel';
 import { PathModel } from './models';
 
 const DEFAULT_PROPS = {
@@ -20,21 +23,19 @@ jest.mock('./actions.ts', () => {
     const originalModule = jest.requireActual('./actions.ts');
     return {
         ...originalModule,
-        fetchPathModel: jest.fn().mockReturnValue(
-            {
-                path: 'root',
-                code: 'testroot',
-                label: 'test root',
-                hasChildren: false,
-                children: undefined,
-            } as PathModel
-        ),
+        fetchPathModel: jest.fn().mockReturnValue({
+            path: 'root',
+            code: 'testroot',
+            label: 'test root',
+            hasChildren: false,
+            children: undefined,
+        } as PathModel),
     };
 });
 
-const EqStub = {getURLSuffix: () => 'eq'} as Filter.IFilterType;
-const InSubtreeStub = {getURLSuffix: () => 'concept:insubtree'} as Filter.IFilterType;
-const NotInSubtreeStub = {getURLSuffix: () => 'concept:notinsubtree'} as Filter.IFilterType;
+const EqStub = { getURLSuffix: () => 'eq' } as Filter.IFilterType;
+const InSubtreeStub = { getURLSuffix: () => 'concept:insubtree' } as Filter.IFilterType;
+const NotInSubtreeStub = { getURLSuffix: () => 'concept:notinsubtree' } as Filter.IFilterType;
 
 describe('OntologyBrowserFilterPanel', () => {
     const validate = (wrapper: ReactWrapper): void => {
@@ -65,21 +66,24 @@ describe('OntologyBrowserFilterPanel', () => {
         validate(wrapper);
         expect(changeHandler).toHaveBeenCalledTimes(0);
 
-        await waitForLifecycle(wrapper.setProps({filterValue: 'Mock:Code2'}));
+        await waitForLifecycle(wrapper.setProps({ filterValue: 'Mock:Code2' }));
         // Shouldn't call out to handler unless change is from the panel
         expect(changeHandler).toHaveBeenCalledTimes(0);
-        expect(wrapper.find(OntologyBrowserPanel).prop("filters")).toHaveProperty('size', 1);
+        expect(wrapper.find(OntologyBrowserPanel).prop('filters')).toHaveProperty('size', 1);
 
-        //Multi valued filter
-        await waitForLifecycle(wrapper.setProps({filterValue: 'Mock:Code2;Mock:Code3;Mock:Code1'}));
+        // Multi valued filter
+        await waitForLifecycle(wrapper.setProps({ filterValue: 'Mock:Code2;Mock:Code3;Mock:Code1' }));
         expect(changeHandler).toHaveBeenCalledTimes(0);
-        expect(wrapper.find(OntologyBrowserPanel).prop("filters")).toHaveProperty('size', 3);
-        expect(wrapper.find(OntologyBrowserPanel).prop("filters").get('Mock:Code3')).toHaveProperty('code', 'Mock:Code3');
+        expect(wrapper.find(OntologyBrowserPanel).prop('filters')).toHaveProperty('size', 3);
+        expect(wrapper.find(OntologyBrowserPanel).prop('filters').get('Mock:Code3')).toHaveProperty(
+            'code',
+            'Mock:Code3'
+        );
 
-        //Path valued filter
-        await waitForLifecycle(wrapper.setProps({filterValue: 'Mock:Code2/Mock:Code3/Mock:Code1'}));
+        // Path valued filter
+        await waitForLifecycle(wrapper.setProps({ filterValue: 'Mock:Code2/Mock:Code3/Mock:Code1' }));
         expect(changeHandler).toHaveBeenCalledTimes(0);
-        expect(wrapper.find(OntologyBrowserPanel).prop("filters")).toHaveProperty('size', 1);
+        expect(wrapper.find(OntologyBrowserPanel).prop('filters')).toHaveProperty('size', 1);
 
         wrapper.unmount();
     });
@@ -98,16 +102,16 @@ describe('OntologyBrowserFilterPanel', () => {
         validate(wrapper);
         expect(changeHandler).toHaveBeenCalledTimes(0);
 
-        await waitForLifecycle(wrapper.setProps({filterType: InSubtreeStub}));
+        await waitForLifecycle(wrapper.setProps({ filterType: InSubtreeStub }));
         // Shouldn't call out to handler unless change is from the panel
         expect(changeHandler).toHaveBeenCalledTimes(0);
 
-        //Multi valued filter
-        await waitForLifecycle(wrapper.setProps({filterType: NotInSubtreeStub}));
+        // Multi valued filter
+        await waitForLifecycle(wrapper.setProps({ filterType: NotInSubtreeStub }));
         expect(changeHandler).toHaveBeenCalledTimes(0);
 
-        //Path valued filter
-        await waitForLifecycle(wrapper.setProps({filterType: EqStub}));
+        // Path valued filter
+        await waitForLifecycle(wrapper.setProps({ filterType: EqStub }));
         expect(changeHandler).toHaveBeenCalledTimes(0);
 
         wrapper.unmount();

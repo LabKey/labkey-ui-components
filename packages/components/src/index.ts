d13e5b4382bf93c9f684446412afd41e114eb2f9
cnathe <cnathe@labkey.com>
2021-07-14 16:55:13 -0500

    Item 9204: SampleTypeImportAliasRenderer and SourceTypeImportAliasRenderer

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -218,6 +218,10 @@ import { DefaultRenderer } from './internal/renderers/DefaultRenderer';
 import { FileColumnRenderer } from './internal/renderers/FileColumnRenderer';
 import { MultiValueRenderer } from './internal/renderers/MultiValueRenderer';
 import { LabelColorRenderer } from './internal/renderers/LabelColorRenderer';
+import {
+    SampleTypeImportAliasRenderer,
+    SourceTypeImportAliasRenderer,
+} from './internal/renderers/SampleTypeImportAliasRenderer';
 import { BulkUpdateForm } from './internal/components/forms/BulkUpdateForm';
 import { LabelOverlay } from './internal/components/forms/LabelOverlay';
 import { resolveDetailFieldValue, resolveRenderer } from './internal/components/forms/renderers';
@@ -713,6 +717,8 @@ export {
     LabelColorRenderer,
     MultiValueRenderer,
     StorageStatusRenderer,
+    SampleTypeImportAliasRenderer,
+    SourceTypeImportAliasRenderer,
     resolveDetailRenderer,
     resolveRenderer,
     // form related items
---
+++ packages/components/src/internal/renderers/SampleTypeImportAliasRenderer.tsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React, { PureComponent, FC, memo, ReactNode } from 'react';
+import { Map } from 'immutable';
+
+import { AppURL } from '../url/AppURL';
+import { SAMPLES_KEY, SOURCES_KEY } from '../app/constants';
+
+interface Props {
+    data: Map<any, any>;
+}
+
+export class SampleTypeImportAliasRenderer extends PureComponent<Props> {
+    render(): ReactNode {
+        return <ImportAliasRenderer type={SAMPLES_KEY} data={this.props.data} />;
+    }
+}
+
+export class SourceTypeImportAliasRenderer extends PureComponent<Props> {
+    render(): ReactNode {
+        return <ImportAliasRenderer type={SOURCES_KEY} data={this.props.data} />;
+    }
+}
+
+interface RendererProps extends Props {
+    type: string;
+}
+
+const ImportAliasRenderer: FC<RendererProps> = memo(props => {
+    const { type, data } = props;
+    const aliasMap = data?.get('displayValue');
+
+    return (
+        <>
+            {aliasMap?.keySeq().map(key => {
+                return (
+                    <div key={key}>
+                        <a href={AppURL.create(type, aliasMap.get(key)).toHref()}>{key}</a>
+                    </div>
+                );
+            })}
+        </>
+    );
+});

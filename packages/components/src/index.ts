02535e1e4910d95aef3514f40108a604e1191731
labkey-martyp <martyp@labkey.com>
2021-07-29 05:55:19 -0700

    Export validator components

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -451,6 +451,10 @@ import { DataClassDesigner } from './internal/components/domainproperties/datacl
 import { DataClassModel } from './internal/components/domainproperties/dataclasses/models';
 import { deleteDataClass, fetchDataClass } from './internal/components/domainproperties/dataclasses/actions';
 import { DomainFieldLabel } from './internal/components/domainproperties/DomainFieldLabel';
+import { ValidatorModal } from './internal/components/domainproperties/validation/ValidatorModal';
+import { RangeValidationOptions } from './internal/components/domainproperties/validation/RangeValidationOptions';
+import { PropertyValidator } from './internal/components/domainproperties/models';
+import { DOMAIN_RANGE_VALIDATOR } from './internal/components/domainproperties/constants';
 import { AssayImportPanels } from './internal/components/assay/AssayImportPanels';
 import { makeQueryInfo, mountWithServerContextOptions, sleep } from './internal/testHelpers';
 import { QueryModel } from './public/QueryModel/QueryModel';
@@ -997,6 +1001,10 @@ export {
     DomainDesign,
     DomainField,
     DomainFieldLabel,
+    ValidatorModal,
+    RangeValidationOptions,
+    PropertyValidator,
+    DOMAIN_RANGE_VALIDATOR,
     DomainDetails,
     inferDomainFromFile,
     getServerFilePreview,

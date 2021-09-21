0fe8276b6de2c050fdc40544b03ff10ab9f04bdf
labkey-susanh <susanh@labkey.com>
2021-09-21 10:10:30 -0700

    Add missing check

--- packages/components/src/internal/util/messaging.tsx
+++ packages/components/src/internal/util/messaging.tsx
@@ -51,7 +51,7 @@ export function resolveErrorMessage(error: any, noun = 'data', nounPlural?: stri
                 nounPlural || noun
             } for possible duplicates and make sure any referenced ${nounPlural || noun} are still valid.`;
         } else if (lcMessage.indexOf('violates foreign key constraint') >= 0 ||
-                   lcMessage.indexOf('conflicted with the foreign key constraint')) {
+                   lcMessage.indexOf('conflicted with the foreign key constraint') >= 0) {
             return `There was a problem ${verb || 'creating'} your ${noun || 'data'}. Check the data fields to make
             sure they contain or reference valid values.`;
         } else if (lcMessage.indexOf('bad sql grammar') >= 0 || lcMessage.indexOf(ClassCastMessage) >= 0) {

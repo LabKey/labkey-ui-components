7e4d7cc56cd0f420568c4ce0c4d8d4960447e4ff
cnathe <cnathe@labkey.com>
2021-12-15 16:24:01 -0600

    TextChoiceOptions query to get distinct values for existing column - to be used as initial values when changing a Text field to Text Choice - to be used for marking Text Choice values as locked (i.e. in use)

--- packages/components/src/internal/components/domainproperties/TextChoiceOptions.tsx
+++ packages/components/src/internal/components/domainproperties/TextChoiceOptions.tsx
@@ -1,10 +1,15 @@
-import React, { FC, memo, useCallback, useState } from 'react';
+import React, { FC, memo, useCallback, useEffect, useState } from 'react';
 import { Button, Col, FormGroup, Row } from 'react-bootstrap';
 import classNames from 'classnames';
+import { Query } from '@labkey/api';
 
 import { ChoicesListItem } from '../base/ChoicesListItem';
 
 import { AddEntityButton } from '../buttons/AddEntityButton';
+
+import { LoadingSpinner } from '../base/LoadingSpinner';
+
+import { LockIcon } from '../base/LockIcon';
 
 import { DOMAIN_VALIDATOR_TEXTCHOICE, MAX_VALID_TEXT_CHOICES } from './constants';
 import { DEFAULT_TEXT_CHOICE_VALIDATOR, DomainField, ITypeDependentProps, PropertyValidator } from './models';
@@ -18,6 +23,15 @@ const HELP_TIP_BODY = (
     <p>Add a set of text choice values to be used as drop-down options to restrict data entry into this field.</p>
 );
 
+const VALUE_IN_USE = (
+    <LockIcon
+        iconCls="pull-right choices-list__locked"
+        body={<p>This text choice value cannot be changed or deleted because it is in use.</p>}
+        id="text-choice-value-lock-icon"
+        title="Text Choice In Use"
+    />
+);
+
 interface TextChoiceProps extends ITypeDependentProps {
     field: DomainField;
     queryName?: string;
@@ -25,24 +39,15 @@ interface TextChoiceProps extends ITypeDependentProps {
 }
 
 export const TextChoiceOptions: FC<TextChoiceProps> = memo(props => {
-    const { label, field, onChange, domainIndex, index } = props;
+    const { label, field, onChange, domainIndex, index, schemaName, queryName } = props;
+    const [loading, setLoading] = useState<boolean>(true);
+    const [fieldValues, setFieldValues] = useState<string[]>([]);
     const [validValues, setValidValues] = useState<string[]>(field.textChoiceValidator?.properties.validValues ?? []);
     const [selectedIndex, setSelectedIndex] = useState<number>();
     const [currentValue, setCurrentValue] = useState<string>();
     const [showAddValuesModal, setShowAddValuesModal] = useState<boolean>();
     const fieldId = createFormInputId(DOMAIN_VALIDATOR_TEXTCHOICE, domainIndex, index);
-
-    const onSelect = useCallback(
-        index => {
-            setSelectedIndex(index);
-            setCurrentValue(validValues[index]);
-        },
-        [validValues]
-    );
-
-    const onValueChange = useCallback(evt => {
-        setCurrentValue(evt.target.value);
-    }, []);
+    const currentInUse = fieldValues.indexOf(currentValue) > -1;
 
     const replaceValues = useCallback(
         (newValues?: string[]) => {
@@ -59,6 +64,52 @@ export const TextChoiceOptions: FC<TextChoiceProps> = memo(props => {
         },
         [field.textChoiceValidator, fieldId, onChange]
     );
+
+    useEffect(
+        () => {
+            // for an existing field, we query for the distinct set of values in the Text column to be used for
+            // the initial set of values and/or setting fields as locked (i.e. in use)
+            if (!field.isNew() && schemaName && queryName) {
+                Query.selectDistinctRows({
+                    containerFilter: Query.ContainerFilter.allFolders, // to account for a shared domain at project or /Shared
+                    schemaName,
+                    queryName,
+                    column: field.name,
+                    sort: field.name,
+                    success: result => {
+                        const values = result.values.filter(value => value !== undefined && value !== null);
+                        setFieldValues(values);
+
+                        // if this is an existing text field that is being changed to a Text Choice data type,
+                        // then we will use the existing distinct values for that field as the initial options
+                        if (!field.textChoiceValidator?.rowId) {
+                            replaceValues(values);
+                        }
+
+                        setLoading(false);
+                    },
+                    failure: error => {
+                        console.error('Error fetching distinct values for the text field: ', error);
+                        setLoading(false);
+                    },
+                });
+            } else {
+                setLoading(false);
+            }
+        }, [/* none, only call once on mount*/]
+    );
+
+    const onSelect = useCallback(
+        ind => {
+            setSelectedIndex(ind);
+            setCurrentValue(validValues[ind]);
+        },
+        [validValues]
+    );
+
+    const onValueChange = useCallback(evt => {
+        setCurrentValue(evt.target.value);
+    }, []);
 
     const updateValue = useCallback(
         (updatedValue?: string) => {
@@ -108,75 +159,86 @@ export const TextChoiceOptions: FC<TextChoiceProps> = memo(props => {
                     </div>
                 </Col>
             </Row>
-            <Row>
-                <Col xs={6} lg={4} className={classNames({ 'domain-text-choices-left-panel': validValues.length > 0 })}>
-                    <div className="list-group domain-text-choices-list">
-                        {validValues.map((value, index) => {
-                            return (
-                                <ChoicesListItem
-                                    active={index === selectedIndex}
-                                    index={index}
-                                    key={index}
-                                    label={value}
-                                    itemType={value === '' && 'Empty Value'}
-                                    onSelect={onSelect}
-                                />
-                            );
-                        })}
-                    </div>
-                    <AddEntityButton
-                        disabled={validValues.length >= MAX_VALID_TEXT_CHOICES}
-                        entity="Values"
-                        onClick={toggleAddValues}
-                        title={`Add Values (max ${MAX_VALID_TEXT_CHOICES})`}
-                    />
-                </Col>
-                <Col xs={6} lg={8}>
-                    {validValues.length > 0 && selectedIndex === undefined && (
-                        <p className="choices-detail__empty-message">
-                            Select a value from the list on the left to view details.
-                        </p>
-                    )}
-                    {selectedIndex !== undefined && (
-                        <form className="form-horizontal content-form">
-                            <FormGroup>
-                                <div className="col-sm-4">
-                                    <DomainFieldLabel label="Value" required />
-                                </div>
-                                <div className="col-sm-8">
-                                    <input
-                                        className="form-control"
-                                        // disabled={isUse}
-                                        name="value"
-                                        onChange={onValueChange}
-                                        placeholder="Enter a text choice value"
-                                        type="text"
-                                        value={currentValue ?? ''}
+            {loading && (
+                <Row>
+                    <Col xs={12}>
+                        <LoadingSpinner />
+                    </Col>
+                </Row>
+            )}
+            {!loading && (
+                <Row>
+                    <Col
+                        xs={6}
+                        lg={4}
+                        className={classNames({ 'domain-text-choices-left-panel': validValues.length > 0 })}
+                    >
+                        <div className="list-group domain-text-choices-list">
+                            {validValues.map((value, ind) => {
+                                const inUse = fieldValues.indexOf(value) > -1;
+                                return (
+                                    <ChoicesListItem
+                                        active={ind === selectedIndex}
+                                        index={ind}
+                                        key={ind}
+                                        label={value}
+                                        itemType={value === '' && 'Empty Value'}
+                                        onSelect={onSelect}
+                                        componentRight={inUse && VALUE_IN_USE}
                                     />
+                                );
+                            })}
+                        </div>
+                        <AddEntityButton
+                            disabled={validValues.length >= MAX_VALID_TEXT_CHOICES}
+                            entity="Values"
+                            onClick={toggleAddValues}
+                            title={`Add Values (max ${MAX_VALID_TEXT_CHOICES})`}
+                        />
+                    </Col>
+                    <Col xs={6} lg={8}>
+                        {validValues.length > 0 && selectedIndex === undefined && (
+                            <p className="choices-detail__empty-message">
+                                Select a value from the list on the left to view details.
+                            </p>
+                        )}
+                        {selectedIndex !== undefined && (
+                            <form className="form-horizontal content-form">
+                                <FormGroup>
+                                    <div className="col-sm-4">
+                                        <DomainFieldLabel label="Value" required />
+                                    </div>
+                                    <div className="col-sm-8">
+                                        <input
+                                            className="form-control"
+                                            disabled={currentInUse}
+                                            name="value"
+                                            onChange={onValueChange}
+                                            placeholder="Enter a text choice value"
+                                            type="text"
+                                            value={currentValue ?? ''}
+                                        />
+                                    </div>
+                                </FormGroup>
+                                <div>
+                                    <Button bsStyle="default" disabled={currentInUse} onClick={onDelete}>
+                                        <span className="fa fa-trash" />
+                                        <span>&nbsp;Delete</span>
+                                    </Button>
+                                    <Button
+                                        bsStyle="success"
+                                        className="pull-right"
+                                        disabled={currentValue === validValues[selectedIndex]}
+                                        onClick={onApply}
+                                    >
+                                        Apply
+                                    </Button>
                                 </div>
-                            </FormGroup>
-                            <div>
-                                <Button
-                                    bsStyle="default"
-                                    // disabled={inUse}
-                                    onClick={onDelete}
-                                >
-                                    <span className="fa fa-trash" />
-                                    <span>&nbsp;Delete</span>
-                                </Button>
-                                <Button
-                                    bsStyle="success"
-                                    className="pull-right"
-                                    disabled={currentValue === validValues[selectedIndex]}
-                                    onClick={onApply}
-                                >
-                                    Apply
-                                </Button>
-                            </div>
-                        </form>
-                    )}
-                </Col>
-            </Row>
+                            </form>
+                        )}
+                    </Col>
+                </Row>
+            )}
             {showAddValuesModal && (
                 <TextChoiceAddValuesModal
                     title={field.name}

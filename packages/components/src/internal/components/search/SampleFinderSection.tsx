846d998ea5025ceae21553f07b5eae11451db13e
labkey-susanh <susanh@labkey.com>
2021-12-28 12:43:53 -0800

    Add SampleFinderSamples component

--- packages/components/src/internal/components/search/SampleFinderSection.tsx
+++ packages/components/src/internal/components/search/SampleFinderSection.tsx
@@ -3,19 +3,31 @@ import { Section } from '../base/Section';
 import { EntityDataType } from '../entities/models';
 import { capitalizeFirstChar } from '../../util/utils';
 import { EntityFieldFilterModal } from './EntityFieldFilterModal';
-import { Filter } from '@labkey/api';
+import { AuditBehaviorTypes, Filter } from '@labkey/api';
 import { SchemaQuery } from '../../../public/SchemaQuery';
 import { FilterCardProps, FilterCards } from './FilterCards';
+import { SamplesTabbedGridPanel } from '../samples/SamplesTabbedGridPanel';
+import { SAMPLE_DATA_EXPORT_CONFIG, SAMPLE_STATUS_REQUIRED_COLUMNS } from '../samples/constants';
+import { SCHEMAS } from '../../schemas';
+import { InjectedQueryModels, withQueryModels } from '../../../public/QueryModel/withQueryModels';
+import { User } from '../base/models/User';
+import { SamplesEditableGridProps } from '../samples/SamplesEditableGrid';
 
 const SAMPLE_FINDER_TITLE = "Find Samples";
 const SAMPLE_FINDER_CAPTION = "Find samples that meet all of these criteria";
 
-interface Props {
+interface SampleFinderSamplesGridProps {
+    user: User;
+    getSampleAuditBehaviorType: () => AuditBehaviorTypes;
+    samplesEditableGridProps: Partial<SamplesEditableGridProps>;
+}
+
+interface Props extends SampleFinderSamplesGridProps {
     parentEntityDataTypes: EntityDataType[];
 }
 
 export const SampleFinderSection: FC<Props> = memo((props) => {
-    const { parentEntityDataTypes } = props;
+    const { parentEntityDataTypes, ...gridProps } = props;
 
     const [showFilterModal, setShowFilterModal] = useState<boolean>(false);
     const [chosenEntityType, setChosenEntityType] = useState<EntityDataType>(undefined);
@@ -83,7 +95,13 @@ export const SampleFinderSection: FC<Props> = memo((props) => {
                     <div className="filter-hint">{hintText}</div>
                 </>
                 :
-                <FilterCards cards={filterCards} />
+                <>
+                    <FilterCards cards={filterCards} />
+                    <SampleFinderSamples
+                        {...gridProps}
+                        cards={filterCards}
+                    />
+                </>
             }
             {showFilterModal && (
                 <EntityFieldFilterModal
@@ -94,4 +112,65 @@ export const SampleFinderSection: FC<Props> = memo((props) => {
             )}
         </Section>
     )
-})
+});
+
+interface SampleFinderSamplesProps extends SampleFinderSamplesGridProps {
+    cards: FilterCardProps[];
+
+}
+
+export const SampleFinderSamplesImpl: FC<SampleFinderSamplesProps & InjectedQueryModels> = memo(props => {
+    const { actions, queryModels } = props;
+
+    return(
+        <>
+            <h4>Filtering and sample-type tabs coming soon...</h4>
+            <SamplesTabbedGridPanel
+                {...props}
+                asPanel={false}
+                actions={actions}
+                queryModels={queryModels}
+                tabbedGridPanelProps={{
+                    alwaysShowTabs: true,
+                    advancedExportOptions: SAMPLE_DATA_EXPORT_CONFIG,
+                }}
+            />
+        </>
+    )
+});
+
+// exported for jest testing
+export const SampleFinderSamplesWithQueryModels = withQueryModels<SampleFinderSamplesProps>(SampleFinderSamplesImpl);
+
+
+// exported for jest testing
+export const SampleFinderSamples: FC<SampleFinderSamplesProps> = memo((props) => {
+    const { cards } = props;
+
+    const baseFilters = [];
+    // TODO this is not really correct.  Probably there will be a specialized filter type used for these lineage queries.
+    // cards.forEach(card => {
+    //     if (card.filterArray.length) {
+    //         baseFilters.push(...card.filterArray);
+    //     } else  {
+    //         baseFilters.push(card.entityDataType.inputColumnName.replace("First", card.schemaQuery.queryName), null, Filter.Types.NONBLANK);
+    //     }
+    // });
+
+    const queryConfigs = {
+        'allSamples': {
+            id: 'allSamples',
+            title: 'All Samples',
+            schemaQuery: SCHEMAS.EXP_TABLES.MATERIALS,
+            requiredColumns: SAMPLE_STATUS_REQUIRED_COLUMNS,
+        }
+    }
+
+    return (
+        <SampleFinderSamplesWithQueryModels
+            {...props}
+            autoLoad={false}
+            queryConfigs={queryConfigs}
+        />
+    )
+});

9ae8b458275116dfc33f1a863468e8c014401012
rosalinep <rosalinep@labkey.com>
2022-01-06 19:26:10 -0800

    Finalize assay messaging

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -499,7 +499,7 @@ import { PipelineJobsListingPage } from './internal/components/pipeline/Pipeline
 import { PipelineJobsPage } from './internal/components/pipeline/PipelineJobsPage';
 import { PipelineSubNav } from './internal/components/pipeline/PipelineSubNav';
 import { PipelineStatusDetailPage } from './internal/components/pipeline/PipelineStatusDetailPage';
-import { renderWorkflowLinkMsg, getSuccessMsg } from './internal/components/pipeline/utils';
+import { getImportNotificationMsg } from './internal/components/pipeline/utils';
 import {
     ALIQUOT_CREATION,
     CHILD_SAMPLE_CREATION,
@@ -1300,8 +1300,7 @@ export {
     BACKGROUND_IMPORT_MIN_FILE_SIZE,
     BACKGROUND_IMPORT_MIN_ROW_SIZE,
     DATA_IMPORT_FILE_SIZE_LIMITS,
-    renderWorkflowLinkMsg,
-    getSuccessMsg,
+    getImportNotificationMsg,
     // Test Helpers
     sleep,
     createMockWithRouterProps,
--- packages/components/src/internal/components/pipeline/utils.tsx
+++ packages/components/src/internal/components/pipeline/utils.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
 
 import { AppURL } from '../../url/AppURL';
-import { App, AssayUploadResultModel } from '../../../index';
+import {App, AssayDefinitionModel, AssayUploadResultModel} from '../../../index';
 
-export function renderWorkflowLinkMsg(workflowJobId, workflowTaskId) {
+export function getSuccessMsg(
+    response: AssayUploadResultModel,
+    isBackgroundJob: boolean,
+    reimport: boolean,
+    assayDefinition: AssayDefinitionModel = null
+): JSX.Element {
+    console.log("getSuccessMsg isBackgroundJob", isBackgroundJob);
+    if (!isBackgroundJob) {
+        const msg = `Successfully ${reimport ? 're-imported' : 'created'} assay run`;
+        if (assayDefinition) {
+            // Displayed if 'Save and Import Another Run' chosen
+            const href = AppURL.create(
+                App.ASSAYS_KEY,
+                assayDefinition.type,
+                assayDefinition.name,
+                'runs',
+                response.runId
+            ).toHref();
+            return (
+                <>
+                    {msg} <a href={href}>#{response.runId}</a>.{' '}
+                </>
+            );
+        } else {
+            // Displayed if 'Save and Finish' chosen
+            return <> {msg}.{' '} </>;
+        }
+    } else {
+        return (
+            <>
+                Successfully started {reimport ? 're-importing' : 'creating'} assay run. You'll be notified when it's
+                done.{' '}
+            </>
+        );
+    }
+}
+
+export function getPipelineLinkMsg(response: AssayUploadResultModel): JSX.Element {
+    return (
+        <>
+            Click{' '}
+            <a href={AppURL.create('pipeline', response.jobId).toHref()}>  here </a> to check the status of the background
+            import.{' '}
+        </>
+    );
+}
+
+export function getWorkflowLinkMsg(workflowJobId, workflowTaskId): JSX.Element {
     if (workflowJobId) {
         let jobTasksUrl = AppURL.create(App.WORKFLOW_KEY, workflowJobId, 'tasks');
         if (workflowTaskId) {
@@ -14,33 +61,28 @@ export function renderWorkflowLinkMsg(workflowJobId, workflowTaskId) {
 
         return (
             <>
-                {' Click '}
-                <a href={jobTasksUrl.toHref()} className="alert-link">
-                    here
-                </a>
-                {' to go back to the workflow task.'}
+                Click
+                <a href={jobTasksUrl.toHref()} className="alert-link"> here </a>
+                to go back to the workflow task.
             </>
         );
     }
     return null;
 }
 
-export function getSuccessMsg(
+export function getImportNotificationMsg(
     response: AssayUploadResultModel,
     isBackgroundJob: boolean,
-    reimport: boolean
-): string | JSX.Element {
-    if (!isBackgroundJob) {
-        const verb = reimport ? 're-imported' : 'created';
-        return `Successfully ${verb} assay run`;
-    } else {
-        const verb = reimport ? 're-importing' : 'creating';
-        return (
-            <>
-                Successfully started {verb} assay run.&nbsp;You'll be notified when it's done.&nbsp; Click{' '}
-                <a href={AppURL.create('pipeline', response.jobId).toHref()} className="alert-link">here</a> to check the status of the
-                background import
-            </>
-        );
-    }
+    reimport: boolean,
+    assayDefinition: AssayDefinitionModel,
+    workflowJobId,
+    workflowTaskId
+): JSX.Element {
+    return (
+        <span>
+            {getSuccessMsg(response, isBackgroundJob, reimport, assayDefinition)}
+            {isBackgroundJob ? getPipelineLinkMsg(response) : null}
+            {!assayDefinition ? getWorkflowLinkMsg(workflowJobId, workflowTaskId) : null}
+        </span>
+    );
 }

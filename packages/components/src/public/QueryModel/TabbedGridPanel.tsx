82bba8a54c90cfb5a99fdb017cde345d9390708d
labkey-susanh <susanh@labkey.com>
2022-01-10 09:37:28 -0800

    If rowCount is not defined, don't show empty parentheses

--- packages/components/src/public/QueryModel/TabbedGridPanel.tsx
+++ packages/components/src/public/QueryModel/TabbedGridPanel.tsx
@@ -40,7 +40,7 @@ const GridTab: FC<GridTabProps> = memo(({ isActive, model, onSelect, pullRight, 
         <li className={className}>
             <a onClick={onClick}>
                 {title || queryInfo?.queryLabel || queryInfo?.name}
-                {showRowCount && <> ({rowCount})</>}
+                {showRowCount && rowCount !== undefined && <> ({rowCount})</>}
             </a>
         </li>
     );

75de92b8661359ef87291bf4505a93d56557b4d5
cnathe <cnathe@labkey.com>
2022-01-20 16:07:26 -0600

    yarn run lint-branch-fix

--- packages/components/src/internal/components/lineage/node/DetailsList.spec.tsx
+++ packages/components/src/internal/components/lineage/node/DetailsList.spec.tsx
@@ -3,12 +3,12 @@ import { mount, ReactWrapper } from 'enzyme';
 
 import lineageSampleData from '../../../../test/data/experiment-lineage-runSteps.json';
 
-import {DetailsList, DetailsListSteps} from './DetailsList';
-import {LineageNode} from "../models";
-import {LineageDataLink} from "../LineageDataLink";
+import { LineageNode } from '../models';
+import { LineageDataLink } from '../LineageDataLink';
+
+import { DetailsList, DetailsListSteps } from './DetailsList';
 
 describe('DetailsListSteps', () => {
-
     function validate(wrapper: ReactWrapper, stepCount: number): void {
         expect(wrapper.find(DetailsList)).toHaveLength(1);
         expect(wrapper.find('.lineage-sm-icon').hostNodes()).toHaveLength(stepCount);
@@ -17,28 +17,31 @@ describe('DetailsListSteps', () => {
     }
 
     test('not exp run', () => {
-        const wrapper = mount(<DetailsListSteps
-            node={LineageNode.create('abc:123', { expType: 'ProtocolApplication' })}
-            onSelect={jest.fn}
-        />);
+        const wrapper = mount(
+            <DetailsListSteps
+                node={LineageNode.create('abc:123', { expType: 'ProtocolApplication' })}
+                onSelect={jest.fn}
+            />
+        );
         expect(wrapper.find(DetailsList)).toHaveLength(0);
         wrapper.unmount();
     });
 
     test('no run steps', () => {
-        const wrapper = mount(<DetailsListSteps
-            node={LineageNode.create('abc:123', { expType: 'ExperimentRun' })}
-            onSelect={jest.fn}
-        />);
+        const wrapper = mount(
+            <DetailsListSteps node={LineageNode.create('abc:123', { expType: 'ExperimentRun' })} onSelect={jest.fn} />
+        );
         validate(wrapper, 0);
         wrapper.unmount();
     });
 
     test('with run steps', () => {
-        const wrapper = mount(<DetailsListSteps
-            node={LineageNode.create('abc:123', lineageSampleData.nodes[lineageSampleData.seed])}
-            onSelect={jest.fn}
-        />);
+        const wrapper = mount(
+            <DetailsListSteps
+                node={LineageNode.create('abc:123', lineageSampleData.nodes[lineageSampleData.seed])}
+                onSelect={jest.fn}
+            />
+        );
         validate(wrapper, 2);
         expect(wrapper.find('.lineage-sm-name').hostNodes().first().text()).toBe('RecordingOneStepOne');
         expect(wrapper.find('.lineage-sm-name').hostNodes().last().text()).toBe('RecordingOneStepTwo');

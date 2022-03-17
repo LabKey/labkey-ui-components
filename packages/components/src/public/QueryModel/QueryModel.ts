3196de99bbd41d6cbb0e07f5bc9acfe74c97288a
Alan Vezina <alanv@labkey.com>
2022-03-17 15:50:03 -0500

    Fix Docs URLs in QueryModel.ts

--- packages/components/src/public/QueryModel/QueryModel.ts
+++ packages/components/src/public/QueryModel/QueryModel.ts
@@ -42,7 +42,7 @@ export interface GridMessage {
 
 export interface QueryConfig {
     /**
-     * An array of base [Filter.IFilter](https://labkey.github.io/labkey-api-js/interfaces/_filter_filter_.ifilter.html)
+     * An array of base [Filter.IFilter](https://labkey.github.io/labkey-api-js/interfaces/Filter.IFilter.html)
      * filters to be applied to the [[QueryModel]] data load. These base filters will be concatenated with URL filters,
      * the keyValue filter, and view filters when applicable.
      */
@@ -52,7 +52,7 @@ export interface QueryConfig {
      */
     bindURL?: boolean;
     /**
-     * One of the values of [Query.ContainerFilter](https://labkey.github.io/labkey-api-js/enums/_query_utils_.containerfilter.html)
+     * One of the values of [Query.ContainerFilter](https://labkey.github.io/labkey-api-js/enums/Query.ContainerFilter.html)
      * that sets the scope of this query. Defaults to ContainerFilter.current, and is interpreted relative to
      * config.containerPath.
      */
@@ -130,7 +130,7 @@ const DEFAULT_MAX_ROWS = 20;
 
 /**
  * This is the base model used to store all the data for a query. At a high level the QueryModel API is a wrapper around
- * the [selectRows](https://labkey.github.io/labkey-api-js/modules/_query_selectrows_.html#selectrows) API.
+ * the [selectRows](https://labkey.github.io/labkey-api-js/modules/Query.html#selectRows) API.
  * If you need to retrieve data from a LabKey table or query, so you can render it in a React
  * component, then the QueryModel API is most likely what you want.
  *
@@ -150,7 +150,7 @@ export class QueryModel {
     // Fields from QueryConfig
     // Some of the fields we have in common with QueryConfig are not optional because we give them default values.
     /**
-     * An array of base [Filter.IFilter](https://labkey.github.io/labkey-api-js/interfaces/_filter_filter_.ifilter.html)
+     * An array of base [Filter.IFilter](https://labkey.github.io/labkey-api-js/interfaces/Filter.IFilter.html)
      * filters to be applied to the QueryModel data load. These base filters will be concatenated with URL filters,
      * they keyValue filter, and view filters when applicable.
      */
@@ -160,7 +160,7 @@ export class QueryModel {
      */
     readonly bindURL: boolean;
     /**
-     * One of the values of [Query.ContainerFilter](https://labkey.github.io/labkey-api-js/enums/_query_utils_.containerfilter.html)
+     * One of the values of [Query.ContainerFilter](https://labkey.github.io/labkey-api-js/enums/Query.ContainerFilter.html)
      * that sets the scope of this query. Defaults to ContainerFilter.current, and is interpreted relative to
      * config.containerPath.
      */
@@ -234,7 +234,7 @@ export class QueryModel {
 
     // QueryModel only fields
     /**
-     * An array of [Filter.IFilter](https://labkey.github.io/labkey-api-js/interfaces/_filter_filter_.ifilter.html)
+     * An array of [Filter.IFilter](https://labkey.github.io/labkey-api-js/interfaces/Filter.IFilter.html)
      * filters to be applied to the QueryModel data load. These filters will be concatenated with base filters, URL filters,
      * they keyValue filter, and view filters when applicable.
      */
@@ -436,7 +436,7 @@ export class QueryModel {
     }
 
     /**
-     * An array of [Filter.IFilter](https://labkey.github.io/labkey-api-js/interfaces/_filter_filter_.ifilter.html) objects
+     * An array of [Filter.IFilter](https://labkey.github.io/labkey-api-js/interfaces/Filter.IFilter.html) objects
      * for the QueryModel. If a keyValue is provided, this will be a filter on the primary key column concatenated with
      * the detailFilters. Otherwise, this will be a concatenation of the baseFilters, filterArray, and [[QueryInfo]] view filters.
      */

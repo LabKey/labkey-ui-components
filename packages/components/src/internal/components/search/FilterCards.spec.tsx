8dd7d084434f1b90cc10e73c3597378464742359
labkey-susanh <susanh@labkey.com>
2022-03-25 08:53:18 -0700

    Tests for GroupedFilterValues

--- packages/components/src/internal/components/search/FilterCards.spec.tsx
+++ packages/components/src/internal/components/search/FilterCards.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 
 import { Filter } from '@labkey/api';
 
@@ -7,8 +7,9 @@ import { SchemaQuery } from '../../../public/SchemaQuery';
 import { capitalizeFirstChar } from '../../util/utils';
 import { TestTypeDataType } from '../../../test/data/constants';
 
-import { FilterCard } from './FilterCards';
+import { FilterCard, GroupedFilterValues } from './FilterCards';
 import { FieldFilter } from './models';
+import renderer from 'react-test-renderer';
 
 describe('FilterCard', () => {
     const capParentNoun = capitalizeFirstChar(TestTypeDataType.nounAsParentSingular);
@@ -86,5 +87,99 @@ describe('FilterCard', () => {
         expect(content.find('.filter-display__row').length).toBe(2);
         expect(content.text()).toBe('Field1:1Field2:2');
         wrapper.unmount();
+    });
+});
+
+describe("GroupedFilterValues", () => {
+    test("no filter array", () => {
+        const component = renderer.create(
+            <GroupedFilterValues cardIndex={1} filterArray={null} onFilterValueExpand={jest.fn} />
+        );
+        expect(component.toJSON()).toBeNull();
+    });
+
+    function validateRow(rows: ReactWrapper, rowNum: number, labelText: string, valueText: string) : void {
+        const cols = rows.at(rowNum).find("td");
+        expect(cols).toHaveLength(2);
+        expect(cols.at(0).text()).toBe(labelText);
+        expect(cols.at(1).text()).toBe(valueText);
+    }
+
+    test("one filter", () => {
+        const wrapper = mount(
+            <GroupedFilterValues cardIndex={1} onFilterValueExpand={jest.fn} filterArray={[
+                {
+                    fieldKey: "test",
+                    fieldCaption: "Test",
+                    jsonType: "string",
+                    filter: Filter.create("test", "a", Filter.Types.EQ)
+                }]}
+             />
+        );
+        const rows = wrapper.find("tr");
+        expect(rows).toHaveLength(1);
+        validateRow(rows, 0, "Test:", "a");
+    });
+
+    test("one filter per field", () => {
+        const wrapper = mount(
+            <GroupedFilterValues cardIndex={1} onFilterValueExpand={jest.fn} filterArray={[
+                {
+                    fieldKey: "test",
+                    fieldCaption: "Test",
+                    jsonType: "string",
+                    filter: Filter.create("test", "a", Filter.Types.NEQ)
+                },
+                {
+                    fieldKey: "otherField",
+                    fieldCaption: "Other",
+                    jsonType: "string",
+                    filter: Filter.create("otherField", "b", Filter.Types.GT)
+                }
+            ]}
+            />
+        );
+        const rows = wrapper.find("tr");
+        expect(rows).toHaveLength(2);
+        validateRow(rows, 0, "Test:", "≠ a");
+        validateRow(rows, 1, "Other:", "> b");
+    });
+
+    test("multiple filters per field", () => {
+        const wrapper = mount(
+            <GroupedFilterValues cardIndex={1} onFilterValueExpand={jest.fn} filterArray={[
+                {
+                    fieldKey: "test",
+                    fieldCaption: "Test",
+                    jsonType: "string",
+                    filter: Filter.create("test", "a", Filter.Types.GTE)
+                },
+                {
+                    fieldKey: "test",
+                    fieldCaption: "Test",
+                    jsonType: "string",
+                    filter: Filter.create("test", "zebra", Filter.Types.LTE)
+                },
+                {
+                    fieldKey: "otherField",
+                    fieldCaption: "Other",
+                    jsonType: "string",
+                    filter: Filter.create("otherField", "alice", Filter.Types.GT)
+                },
+                {
+                    fieldKey: "otherField",
+                    fieldCaption: "Other",
+                    jsonType: "string",
+                    filter: Filter.create("otherField", "jack;jill;bob", Filter.Types.IN)
+                },
+            ]}
+            />
+        );
+        const rows = wrapper.find("tr");
+        expect(rows).toHaveLength(4);
+        validateRow(rows, 0, "Test:", "≥ a");
+        validateRow(rows, 1, "and", "≤ zebra");
+        validateRow(rows, 2, "Other:", "> alice");
+        validateRow(rows, 3, "and", "jackjillbob");
     });
 });

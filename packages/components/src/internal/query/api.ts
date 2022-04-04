f19ccb88301121f6a9ad76037bfc732634ffbc51
labkey-nicka <nickk@labkey.com>
2022-04-04 11:25:15 -0700

    Update deprecation message

--- packages/components/src/internal/query/api.ts
+++ packages/components/src/internal/query/api.ts
@@ -353,7 +353,7 @@ export interface ISelectRowsResult {
 }
 
 /**
- * @deprecated
+ * @deprecated use selectRows() instead.
  * Fetches an API response and normalizes the result JSON according to schema.
  * This makes every API response have the same shape, regardless of how nested it was.
  */

4c07f7d4fabf727bfd9bdaceec192c1b1bcd26e0
cnathe <cnathe@labkey.com>
2022-04-04 14:06:42 -0500

    Jest tests for SearchBox.tsx

---
+++ packages/components/src/public/QueryModel/SearchBox.spec.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { mount, ReactWrapper } from 'enzyme';
+import { Filter } from '@labkey/api';
+
+import { SearchBox } from './SearchBox';
+import { SearchAction } from '../../internal/components/omnibox/actions/Search';
+
+describe('SearchBox', () => {
+    const ON_SEARCH = jest.fn();
+    const DEFAULT_PROPS = {
+        actionValues: [],
+        onSearch: ON_SEARCH,
+    };
+
+    const searchAction = {
+        action: new SearchAction('query'),
+        value: 'foo',
+        valueObject: Filter.create('*', 'foo', Filter.Types.Q),
+    };
+
+    function validate(wrapper: ReactWrapper, hasAppliedSearchTerm: boolean): void {
+        expect(wrapper.find('.grid-panel__search-form')).toHaveLength(1);
+        expect(wrapper.find('.grid-panel__search-icon')).toHaveLength(1);
+        expect(wrapper.find('.grid-panel__search-input')).toHaveLength(1);
+        expect(wrapper.find('.grid-panel__remove-icon')).toHaveLength(hasAppliedSearchTerm ? 1 : 0);
+    }
+
+    test('no applied search term', () => {
+        const wrapper = mount(<SearchBox {...DEFAULT_PROPS} />);
+        validate(wrapper, false);
+        expect(wrapper.find('.grid-panel__search-input').prop('value')).toBe('');
+        expect(ON_SEARCH).toHaveBeenCalledTimes(0);
+        wrapper.find('.grid-panel__search-input').simulate('change', { target: { value: 'test' } });
+        expect(ON_SEARCH).toHaveBeenCalledTimes(0);
+        validate(wrapper, false);
+        expect(wrapper.find('.grid-panel__search-input').prop('value')).toBe('test');
+        wrapper.unmount();
+    });
+
+    test('with applied search term', () => {
+        const wrapper = mount(<SearchBox {...DEFAULT_PROPS} actionValues={[searchAction]} />);
+        validate(wrapper, true);
+        expect(ON_SEARCH).toHaveBeenCalledTimes(0);
+        expect(wrapper.find('.grid-panel__search-input').prop('value')).toBe(searchAction.value);
+        wrapper.find('.grid-panel__search-input').simulate('change', { target: { value: 'test' } });
+        expect(ON_SEARCH).toHaveBeenCalledTimes(0);
+        validate(wrapper, true);
+        expect(wrapper.find('.grid-panel__search-input').prop('value')).toBe('test');
+        wrapper.unmount();
+    });
+
+    test('remove applied search term', () => {
+        const wrapper = mount(<SearchBox {...DEFAULT_PROPS} actionValues={[searchAction]} />);
+        validate(wrapper, true);
+        expect(ON_SEARCH).toHaveBeenCalledTimes(0);
+        expect(wrapper.find('.grid-panel__search-input').prop('value')).toBe(searchAction.value);
+        wrapper.find('.grid-panel__remove-icon').simulate('click');
+        expect(ON_SEARCH).toHaveBeenCalledTimes(1);
+        expect(wrapper.find('.grid-panel__search-input').prop('value')).toBe('');
+        wrapper.unmount();
+    });
+});

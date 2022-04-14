90b62dcd54b53c6253b2d61d951f352d8496b3dc
labkey-susanh <susanh@labkey.com>
2022-04-14 06:59:53 -0700

    Update tests

--- packages/components/src/internal/components/search/SampleFinderSection.spec.tsx
+++ packages/components/src/internal/components/search/SampleFinderSection.spec.tsx
@@ -12,6 +12,7 @@ import { SAMPLE_MANAGER_APP_PROPERTIES } from '../../app/constants';
 
 import { FilterCards } from './FilterCards';
 import { SampleFinderHeaderButtons, SampleFinderSection } from './SampleFinderSection';
+import { SchemaQuery } from '../../../public/SchemaQuery';
 
 describe('SampleFinderSection', () => {
     LABKEY.moduleContext = {
@@ -24,9 +25,14 @@ describe('SampleFinderSection', () => {
             <SampleFinderHeaderButtons
                 parentEntityDataTypes={[
                     TestTypeDataType,
-                    { ...TestTypeDataType, nounSingular: 'Other', nounAsParentSingular: 'Other Parent' },
+                    { ...TestTypeDataType,
+                        typeListingSchemaQuery: SchemaQuery.create("TestClasses", "query2"),
+                        nounSingular: 'Other',
+                        nounAsParentSingular: 'Other Parent'
+                    },
                 ]}
                 onAddEntity={jest.fn}
+                enabledEntityTypes={[TestTypeDataType.typeListingSchemaQuery.queryName, "query2"]}
             />
         );
         const buttons = wrapper.find('button');
@@ -34,7 +40,34 @@ describe('SampleFinderSection', () => {
         expect(buttons.at(0).text()).toBe(
             ' ' + capitalizeFirstChar(TestTypeDataType.nounAsParentSingular) + ' Properties'
         );
+        expect(buttons.at(0).prop("disabled")).toBe(false);
         expect(buttons.at(1).text()).toBe(' Other Parent Properties');
+        expect(buttons.at(1).prop("disabled")).toBe(false);
+    });
+
+    test('SampleFinderHeaderButtons, nothing enabled', () => {
+        const wrapper = mount(
+            <SampleFinderHeaderButtons
+                parentEntityDataTypes={[
+                    TestTypeDataType,
+                    { ...TestTypeDataType,
+                        typeListingSchemaQuery: SchemaQuery.create("TestClasses", "query2"),
+                        nounSingular: 'Other',
+                        nounAsParentSingular: 'Other Parent'
+                    },
+                ]}
+                onAddEntity={jest.fn}
+                enabledEntityTypes={[]}
+            />
+        );
+        const buttons = wrapper.find('button');
+        expect(buttons.length).toBe(2);
+        expect(buttons.at(0).text()).toBe(
+            ' ' + capitalizeFirstChar(TestTypeDataType.nounAsParentSingular) + ' Properties'
+        );
+        expect(buttons.at(0).prop("disabled")).toBe(true);
+        expect(buttons.at(1).text()).toBe(' Other Parent Properties');
+        expect(buttons.at(1).prop("disabled")).toBe(true);
     });
 
     test('No cards', () => {

77655519f35ce4b29c608b6395330a4bc0b56da6
cnathe <cnathe@labkey.com>
2022-04-27 08:26:30 -0500

    Jest fixes for FiltersButton changes

--- packages/components/src/public/QueryModel/FiltersButton.spec.tsx
+++ packages/components/src/public/QueryModel/FiltersButton.spec.tsx
@@ -4,23 +4,29 @@ import { mount, ReactWrapper } from 'enzyme';
 import { FiltersButton } from './FiltersButton';
 
 describe('FiltersButton', () => {
-    const ON_FILTER = jest.fn();
-    const DEFAULT_PROPS = {
-        onFilter: ON_FILTER,
-    };
-
-    function validate(wrapper: ReactWrapper): void {
-        expect(wrapper.find('.grid-panel__button').hostNodes()).toHaveLength(2);
-        expect(wrapper.find('.fa-filter')).toHaveLength(2);
-    }
-
     test('default props', () => {
-        const wrapper = mount(<FiltersButton {...DEFAULT_PROPS} />);
-        validate(wrapper);
+        const ON_FILTER = jest.fn();
+        const wrapper = mount(<FiltersButton onFilter={ON_FILTER} />);
+        const button = wrapper.find('.grid-panel__button').hostNodes();
+        expect(button).toHaveLength(1);
+        expect(button.text()).toBe(' Filters');
+        expect(wrapper.find('.fa-filter')).toHaveLength(1);
         expect(ON_FILTER).toHaveBeenCalledTimes(0);
-        wrapper.find('.grid-panel__button').hostNodes().first().simulate('click');
-        wrapper.find('.grid-panel__button').hostNodes().last().simulate('click');
-        expect(ON_FILTER).toHaveBeenCalledTimes(2);
+        button.simulate('click');
+        expect(ON_FILTER).toHaveBeenCalledTimes(1);
+        wrapper.unmount();
+    });
+
+    test('iconOnly', () => {
+        const ON_FILTER = jest.fn();
+        const wrapper = mount(<FiltersButton onFilter={ON_FILTER} iconOnly />);
+        const button = wrapper.find('.grid-panel__button').hostNodes();
+        expect(button).toHaveLength(1);
+        expect(button.text()).toBe('');
+        expect(wrapper.find('.fa-filter')).toHaveLength(1);
+        expect(ON_FILTER).toHaveBeenCalledTimes(0);
+        button.simulate('click');
+        expect(ON_FILTER).toHaveBeenCalledTimes(1);
         wrapper.unmount();
     });
 });

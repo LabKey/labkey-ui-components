1f18405b618561ad61f5e18b7d0149c8528c055a
cnathe <cnathe@labkey.com>
2022-04-28 08:35:59 -0500

    Jest test for ResponsiveMenuButton

---
+++ packages/components/src/internal/components/buttons/ResponsiveMenuButton.spec.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { DropdownButton } from 'react-bootstrap';
+
+import { PicklistButton } from '../picklist/PicklistButton';
+import { TEST_USER_READER } from '../../userFixtures';
+
+import { makeTestQueryModel } from '../../../public/QueryModel/testUtils';
+import { SchemaQuery } from '../../../public/SchemaQuery';
+
+import { mountWithServerContext } from '../../testHelpers';
+
+import { ResponsiveMenuButton } from './ResponsiveMenuButton';
+import { SubMenuItem } from '../menus/SubMenuItem';
+
+describe('ResponsiveMenuButton', () => {
+    const items = <PicklistButton model={makeTestQueryModel(SchemaQuery.create('s', 'q'))} user={TEST_USER_READER} />;
+    const DEFAULT_PROPS = {
+        id: 'test-id',
+        items,
+        text: 'Test Menu',
+    };
+
+    test('default props', () => {
+        const wrapper = mountWithServerContext(<ResponsiveMenuButton {...DEFAULT_PROPS} />, {
+            user: TEST_USER_READER,
+        });
+        expect(wrapper.find(DropdownButton)).toHaveLength(1);
+        expect(wrapper.find(SubMenuItem)).toHaveLength(0);
+        expect(wrapper.find(PicklistButton)).toHaveLength(1);
+        wrapper.unmount();
+    });
+
+    test('asSubMenu', () => {
+        const wrapper = mountWithServerContext(<ResponsiveMenuButton {...DEFAULT_PROPS} asSubMenu />, {
+            user: TEST_USER_READER,
+        });
+        expect(wrapper.find(DropdownButton)).toHaveLength(0);
+        expect(wrapper.find(SubMenuItem)).toHaveLength(1);
+        expect(wrapper.find(PicklistButton)).toHaveLength(1);
+        wrapper.unmount();
+    });
+});

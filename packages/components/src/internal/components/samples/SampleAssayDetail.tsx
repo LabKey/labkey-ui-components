08eb468b89522ef479def1792ad8267b2fd4f793
ian <ians@labkey.com>
2022-05-03 15:44:01 -0700

    add an export prefix string for SourceSamples/Assays

--- packages/components/src/internal/components/samples/SampleAssayDetail.tsx
+++ packages/components/src/internal/components/samples/SampleAssayDetail.tsx
@@ -40,6 +40,7 @@ interface Props {
     showAliquotViewSelector?: boolean;
     sampleAliquotType?: ALIQUOT_FILTER_MODE;
     sourceId?: number | string;
+    exportPrefix?: string;
     sourceSampleRows?: Array<Record<string, any>>;
     sourceAliquotRows?: Array<Record<string, any>>;
     emptyAssayDefDisplay?: ReactNode;
@@ -187,6 +188,7 @@ export const SampleAssayDetailBodyImpl: FC<SampleAssayDetailBodyProps & Injected
         onTabChange,
         activeTabId,
         user,
+        exportPrefix,
     } = props;
     const allModels = Object.values(queryModels);
     const allLoaded = allModels.every(model => !model.isLoading);
@@ -284,8 +286,6 @@ export const SampleAssayDetailBodyImpl: FC<SampleAssayDetailBodyProps & Injected
         );
     }
 
-    const sampleName = sampleModel.getRowValue('Name');
-
     return (
         <TabbedGridPanel
             actions={actions}
@@ -308,8 +308,7 @@ export const SampleAssayDetailBodyImpl: FC<SampleAssayDetailBodyProps & Injected
             onTabSelect={onTabChange}
             activeModelId={activeTabId}
             title="Assay Results"
-            // exportFilename={sampleModel.name + '_assay_results'}
-            exportFilename={sampleName + '_assay_results'}
+            exportFilename={exportPrefix && exportPrefix + '_assay_results'}
         />
     );
 });

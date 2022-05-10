3d4f5a5bf86948af5e2cbf5624370ff7de42382b
Xing Yang <xyang@labkey.com>
2022-05-10 16:13:42 -0700

    Item 10305: Compound Registry Type (SMILES) (#832)

--- packages/components/src/internal/components/domainproperties/constants.ts
+++ packages/components/src/internal/components/domainproperties/constants.ts
@@ -150,6 +150,7 @@ export const STORAGE_UNIQUE_ID_CONCEPT_URI = 'http://www.labkey.org/types#storag
 export const TEXT_CHOICE_CONCEPT_URI = 'http://www.labkey.org/types#textChoice';
 export const CREATED_TIMESTAMP_CONCEPT_URI = 'http://www.labkey.org/types#createdTimestamp';
 export const MODIFIED_TIMESTAMP_CONCEPT_URI = 'http://www.labkey.org/types#modifiedTimestamp';
+export const SMILES_CONCEPT_URI = 'http://www.labkey.org/exp/xml#smiles';
 
 export const UNLIMITED_TEXT_LENGTH = 2147483647; // Integer.MAX_VALUE
 export const MAX_TEXT_LENGTH = 4000;

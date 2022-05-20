bfc2c443f9d8491bb473d645e3fde6ef8efb753e
labkey-susanh <susanh@labkey.com>
2022-05-20 11:47:16 -0700

    Update styling and remove hard-coded value for dirty flag

--- packages/components/src/public/QueryModel/TabbedGridPanel.tsx
+++ packages/components/src/public/QueryModel/TabbedGridPanel.tsx
@@ -188,7 +188,7 @@ export const TabbedGridPanel: FC<TabbedGridPanelProps & InjectedQueryModels> = m
     return (
         <div className={classNames('tabbed-grid-panel', { panel: asPanel, 'panel-default': asPanel })}>
             {tabOrder.length === 1 && !alwaysShowTabs && (
-                <GridTitle asPanel={asPanel} dirty={true} title={title} viewName={queryModels[activeId].viewName} />
+                <GridTitle asPanel={asPanel} title={title} viewName={queryModels[activeId].viewName} />
             )}
             <div className={classNames('tabbed-grid-panel__body', { 'panel-body': asPanel })}>
                 {(tabOrder.length > 1 || alwaysShowTabs) && (

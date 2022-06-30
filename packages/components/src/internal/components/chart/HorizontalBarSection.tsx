5c69c0e4e8bfd96379655988542afbbc852dcaea
Alan Vezina <alanv@labkey.com>
2022-06-30 14:49:11 -0500

    HorizontalBarSection: Update design, no more tables

--- packages/components/src/internal/components/chart/HorizontalBarSection.tsx
+++ packages/components/src/internal/components/chart/HorizontalBarSection.tsx
@@ -1,100 +1,68 @@
-import React, { FC, memo } from 'react';
+import React, { CSSProperties, FC, memo, ReactNode } from 'react';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
 import classNames from 'classnames';
 
 const DEFAULT_EMPTY_TEXT = 'No data available.';
 
 export interface HorizontalBarData {
-    percent: number;
-    count: number;
-    totalCount: number;
     backgroundColor?: string;
-    name?: string;
-    title: string;
-    href?: string;
     className?: string;
+    count: number;
     filled: boolean;
+    href?: string;
+    name?: string;
+    percent: number;
+    title: string;
+    totalCount: number;
 }
 
 interface Props {
-    title?: string;
-    subtitle?: React.ReactNode;
-    emptyText?: string;
     data: HorizontalBarData[];
+    emptyText?: string;
+    subtitle?: React.ReactNode;
+    title?: string;
 }
 
 export const HorizontalBarSection: FC<Props> = memo(props => {
     const { subtitle, title, data, emptyText } = props;
-    const horizontalBars = [];
+    let horizontalBars: ReactNode = <div className="horizontal-bar--empty-text">{emptyText ?? DEFAULT_EMPTY_TEXT}</div>;
 
     if (data?.length) {
-        let hasBegun = false;
-        const dataCount = data.length;
-        data.forEach((row, index) => {
-            if (row.percent > 0) {
-                const pct = row.percent;
-                const styleProps = {
-                    width: pct + '%',
-                };
-                if (row.backgroundColor) {
-                    styleProps['background'] = row.backgroundColor;
-                }
-                horizontalBars.push(
-                    <OverlayTrigger
-                        key={index}
-                        overlay={
-                            <Popover bsClass="popover" id="grid-cell-popover">
-                                {row.title}
-                            </Popover>
-                        }
-                        placement="top"
-                    >
+        horizontalBars = data
+            .filter(row => row.percent > 0)
+            .map((row, index) => {
+                const style: CSSProperties = { width: row.percent + '%', background: row.backgroundColor };
+                const overlay = (
+                    <Popover bsClass="popover" id="grid-cell-popover">
+                        {row.title}
+                    </Popover>
+                );
+                return (
+                    <OverlayTrigger key={index} overlay={overlay} placement="top">
                         <div
-                            key={index}
-                            style={styleProps}
+                            style={style}
                             data-title={row.title}
                             className={classNames('horizontal-bar-part', row.className, {
-                                'horizontal-bar--begin': !hasBegun,
-                                'horizontal-bar--filled': row.filled,
                                 'horizontal-bar--linked': !!row.href,
                                 'horizontal-bar--open': !row.filled || !row.backgroundColor,
-                                'horizontal-bar--end': index === dataCount - 1,
                             })}
                         >
                             {row.href && (
                                 <a href={row.href} className="horizontal-bar--link">
-                                    <div className="horizontal-bar--linkSpanner">&nbsp;</div>
+                                    &nbsp;
                                 </a>
                             )}
                         </div>
                     </OverlayTrigger>
                 );
-                hasBegun = true;
-            }
-        });
-    } else {
-        horizontalBars.push(
-            <div className="horizontal-bar--empty-text" key="empty-text">
-                {emptyText ?? DEFAULT_EMPTY_TEXT}
-            </div>
-        );
+            });
     }
 
     return (
-        <table className="horizontal-bar-section">
-            <tbody>
-                <tr>
-                    {title && <td className="horizontal-bar--title">{title}</td>}
-                    <td>{horizontalBars}</td>
-                </tr>
-                {subtitle && (
-                    <tr>
-                        <td className="horizontal-bar--subtitle" colSpan={2}>
-                            {subtitle}
-                        </td>
-                    </tr>
-                )}
-            </tbody>
-        </table>
+        <div className="horizontal-bar-section">
+            {title && <div className="horizontal-bar--title">{title}</div>}
+            <div className="horizontal-bars">{horizontalBars}</div>
+            {subtitle && <div className="horizontal-bar--subtitle">{subtitle}</div>}
+        </div>
     );
 });

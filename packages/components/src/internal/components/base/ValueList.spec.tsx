247b7a7b3d25fa4dfc0acf27eb4cad9c8b4c1364
XingY <xyang@labkey.com>
2022-08-03 18:53:07 -0700

    lint

--- packages/components/src/internal/components/base/ValueList.spec.tsx
+++ packages/components/src/internal/components/base/ValueList.spec.tsx
@@ -5,9 +5,7 @@ import { ValueList } from './ValueList';
 
 describe('<ValueList />', () => {
     test('values.length = 2, maxCount = 3', () => {
-        const wrapper = mount(
-            <ValueList values={['a', 'b']} maxCount={3}/>
-        );
+        const wrapper = mount(<ValueList values={['a', 'b']} maxCount={3} />);
         expect(wrapper.find('li')).toHaveLength(2);
         expect(wrapper.find('li').at(0).text()).toBe('a');
         expect(wrapper.find('li').at(1).text()).toBe('b');
@@ -15,9 +13,7 @@ describe('<ValueList />', () => {
     });
 
     test('values.length > 3, maxCount = 3', () => {
-        const wrapper = mount(
-            <ValueList values={['a', 'b', 'c', 'd', 'e']} maxCount={3}/>
-        );
+        const wrapper = mount(<ValueList values={['a', 'b', 'c', 'd', 'e']} maxCount={3} />);
         expect(wrapper.find('li')).toHaveLength(3);
         expect(wrapper.find('li').at(2).text()).toBe('c');
         expect(wrapper.text()).toBe('abc...');
--- packages/components/src/internal/components/base/ValueList.tsx
+++ packages/components/src/internal/components/base/ValueList.tsx
@@ -1,18 +1,16 @@
 import React, { FC } from 'react';
 
 export interface Props {
-    values: string[];
     maxCount: number;
+    values: string[];
 }
 
-export const ValueList: FC<Props> = ({ values, maxCount= 5 }) => (
+export const ValueList: FC<Props> = ({ values, maxCount = 5 }) => (
     <ul>
         {values.map((value, ind) => {
-            if (ind > maxCount)
-                return null;
+            if (ind > maxCount) return null;
 
-            if (ind === maxCount)
-                return '...';
+            if (ind === maxCount) return '...';
 
             return <li key={ind}>{value}</li>;
         })}

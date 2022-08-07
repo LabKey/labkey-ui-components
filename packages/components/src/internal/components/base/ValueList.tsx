15f4d62b55949b490ca504c7b7a5a7642204b084
XingY <xyang@labkey.com>
2022-08-07 16:45:45 -0700

    horizontal list

--- packages/components/src/internal/components/base/ValueList.tsx
+++ packages/components/src/internal/components/base/ValueList.tsx
@@ -3,16 +3,30 @@ import React, { FC } from 'react';
 export interface Props {
     maxCount: number;
     values: string[];
+    vertical?: boolean;
 }
 
-export const ValueList: FC<Props> = ({ values, maxCount = 5 }) => (
-    <ul>
-        {values.map((value, ind) => {
-            if (ind > maxCount) return null;
+export const ValueList: FC<Props> = ({ values, maxCount = 5 , vertical = false}) => {
+    if (vertical) {
+        return (
+            <ul>
+                {values.map((value, ind) => {
+                    if (ind > maxCount) return null;
 
-            if (ind === maxCount) return '...';
+                    if (ind === maxCount) return '...';
 
-            return <li key={ind}>{value}</li>;
-        })}
-    </ul>
-);
+                    return <li key={ind}>{value}</li>;
+                })}
+            </ul>
+        );
+    }
+
+    let valueDisplay;
+    if (values.length <= maxCount)
+        valueDisplay = values.join(", ");
+    else
+        valueDisplay = values.slice(0, maxCount).join(", ") + ` and ${values.length - maxCount} more`;
+
+    return <ul><li>{valueDisplay}</li></ul>;
+};
+

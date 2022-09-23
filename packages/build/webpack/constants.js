e22de958b51e9e836908fe57072af2b727722c33
Marty Pradere <martyp@labkey.com>
2022-09-23 13:30:45 -0700

    Module loader exclude path for Windows (#963)
    * Update typescript_dev module loader rule regex to match Windows and Unix path operators.

--- packages/build/webpack/constants.js
+++ packages/build/webpack/constants.js
@@ -240,7 +240,7 @@ module.exports = {
                 // For some reason the dev build needs to explicitly not exclude our packages so they get transpiled
                 // by Babel. For unknown reasons this is not needed for the prod build.
                 test: /^(?!.*spec\.tsx?$).*\.(js|ts|tsx)?$/,
-                exclude: /node_modules\/(?!(@labkey)\/).*/,
+                exclude: /node_modules[\\\/](?!(.labkey)[\\\/]).*/,
                 use: [BABEL_CONFIG]
             }
         ],

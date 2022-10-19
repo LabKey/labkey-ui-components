be6b74232ececb2d0587ec8252f66139b6ac9010
Alan Vezina <alanv@labkey.com>
2022-10-19 13:41:22 -0500

    Revert menu position fix. Add 40px bottom-padding to pages to emulate old layout.

--- packages/components/src/internal/renderers.tsx
+++ packages/components/src/internal/renderers.tsx
@@ -219,11 +219,14 @@ export const HeaderCellDropdown: FC<HeaderCellDropdownProps> = memo(props => {
                     const menuRect = menuEl.getBoundingClientRect();
                     let top = headerRect.y + headerRect.height + 'px';
 
-                    if (headerRect.bottom + menuRect.height > window.innerHeight) {
-                        // Issue 45553 If the header is too close to the bottom of the window to render the whole menu
-                        // below it then we need to render it above the header.
-                        top = headerRect.y - menuRect.height - 5 + 'px';
-                    }
+                    // TODO: we need to do this so the menu doesn't render in a cut-off manner, however we need to also
+                    //  render the menu in a portal or any grids with a scrollbar will completely hide the menu. This
+                    //  will be addressed in a PR in the near future (it is prioritized). See Issue 45553.
+                    // if (headerRect.bottom + menuRect.height > window.innerHeight) {
+                    //     // Issue 45553 If the header is too close to the bottom of the window to render the whole menu
+                    //     // below it then we need to render it above the header.
+                    //     top = headerRect.y - menuRect.height - 5 + 'px';
+                    // }
 
                     Object.assign(menuEl.style, {
                         top,

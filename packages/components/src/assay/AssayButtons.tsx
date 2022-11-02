5b0948a6205f62d9abe3abc08f9d7a900698b721
labkey-susanh <susanh@labkey.com>
2022-11-02 07:57:12 -0700

    Move buttons for run details pages

--- packages/components/src/assay/AssayButtons.tsx
+++ packages/components/src/assay/AssayButtons.tsx
@@ -1,10 +1,27 @@
 import { RequiresPermission } from '../internal/components/base/Permissions';
 import { PermissionTypes } from '@labkey/api';
-import React, { FC } from 'react';
+import React, { FC, memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { MenuItem } from 'react-bootstrap';
 import classNames from 'classnames';
-import { buildURL } from '../internal/url/AppURL';
+import { AppURL, buildURL } from '../internal/url/AppURL';
 import { AssayContextConsumer } from '../internal/components/assay/withAssayModels';
+import { isAssayDesignExportEnabled, isELNEnabled } from '../internal/app/utils';
+import { getOperationConfirmationData } from '../internal/components/entities/actions';
+import { AssayRunDataType } from '../internal/components/entities/constants';
+import { onAssayDesignChange, onAssayRunChange } from './actions';
+import { ASSAY_DESIGN_KEY, ASSAYS_KEY, AUDIT_KEY } from '../internal/app/constants';
+import { CreatedModified } from '../internal/components/base/CreatedModified';
+import { ManageDropdownButton } from '../internal/components/buttons/ManageDropdownButton';
+import { AssayReimportRunButton } from './AssayReimportRunButton';
+import { DisableableMenuItem } from '../internal/components/samples/DisableableMenuItem';
+import { getAssayRunDeleteMessage } from './utils';
+import { AssayRunDeleteModal } from './AssayRunDeleteModal';
+import { AssayDefinitionModel } from '../internal/AssayDefinitionModel';
+import { AssayProtocolModel } from '../internal/components/domainproperties/assay/models';
+import { QueryModel } from '../public/QueryModel/QueryModel';
+import { clearAssayDefinitionCache } from '../internal/components/assay/actions';
+import { ASSAY_AUDIT_QUERY } from '../internal/components/auditlog/constants';
+import { AssayDesignDeleteModal } from './AssayDesignDeleteModal';
 
 interface InMenuProps {
     asMenuItem?: boolean;
@@ -55,3 +72,167 @@ export const AssayExportDesignButton: FC<InMenuProps> = props => (
         </AssayContextConsumer>
     </RequiresPermission>
 );
+
+export interface AssayHeaderButtonProps {
+    allowReimport?: boolean;
+    allowDelete?: boolean;
+    assayDefinition?: AssayDefinitionModel;
+    assayProtocol?: AssayProtocolModel;
+    navigate: (url: string | AppURL, replace?: boolean) => void;
+    menuInit: (invalidate?: boolean) => void;
+    runId?: string;
+    model?: QueryModel;
+}
+
+export const AssayRunDetailHeaderButtons: FC<AssayHeaderButtonProps> = memo(props => {
+    const {assayDefinition, assayProtocol, navigate, model, runId, allowReimport, allowDelete} = props;
+    const [showConfirmDelete, setShowConfirmDelete] = useState<boolean>(false);
+    const [canDelete, setCanDelete] = useState<boolean>(false);
+    const [confirmationDataError, setConfirmationDataError] = useState<boolean>(false);
+
+    const runData = useMemo(() => {
+        return model.getRow();
+    }, [model]);
+
+    useEffect((): void => {
+        if (isELNEnabled()) {
+            (async () => {
+                try {
+                    const confirmationData = await getOperationConfirmationData(undefined, AssayRunDataType, [runId]);
+                    setCanDelete(confirmationData.allowed.length === 1);
+                }
+                catch (e) {
+                    console.error('There was a problem retrieving the delete confirmation data.', e);
+                    setCanDelete(false);
+                    setConfirmationDataError(true);
+                }
+            })();
+        } else {
+            setCanDelete(true);
+        }
+    }, [runId]);
+
+    const onDeleteRun = useCallback(() => {
+        setShowConfirmDelete(true);
+    }, []);
+
+    const afterDelete = useCallback(() => {
+        onAssayRunChange(assayDefinition?.protocolSchemaName);
+        navigate(AppURL.create(ASSAYS_KEY, assayProtocol.providerName, assayProtocol.name));
+    }, [assayDefinition?.protocolSchemaName, assayProtocol.name, assayProtocol.providerName, navigate]);
+
+    const hideConfirm = useCallback(() => {
+        setShowConfirmDelete(false);
+    }, []);
+
+    return (
+        <>
+            <CreatedModified row={runData}/>
+            {(allowReimport || allowDelete) && (
+                <ManageDropdownButton id="assay-run-details" pullRight={true} collapsed={true}>
+                    {allowReimport &&
+                        <AssayReimportRunButton runId={runId} replacedByRunId={runData?.ReplacedByRun?.value}/>
+                    }
+                    {allowDelete &&
+                        <DisableableMenuItem
+                            operationPermitted={canDelete}
+                            onClick={onDeleteRun}
+                            disabledMessage={getAssayRunDeleteMessage(canDelete, confirmationDataError)}
+                        >
+                            Delete Run
+                        </DisableableMenuItem>
+                    }
+                </ManageDropdownButton>
+            )}
+            {showConfirmDelete && (
+                <AssayRunDeleteModal
+                    afterDelete={afterDelete}
+                    afterDeleteFailure={hideConfirm}
+                    onCancel={hideConfirm}
+                    selectedRowId={runId}
+                />
+            )}
+        </>
+    );
+});
+
+export const AssayDesignHeaderButtons: FC<AssayHeaderButtonProps> = props => {
+    const {assayDefinition, assayProtocol, navigate, menuInit} = props;
+    const [showConfirmDeleteAssayDesign, setShowConfirmDeleteAssayDesign] = useState<boolean>(false);
+
+    const resetState = useCallback(() => {
+        setShowConfirmDeleteAssayDesign(false);
+    }, []);
+
+    const onDeleteAssayDesign = useCallback(() => {
+        setShowConfirmDeleteAssayDesign(true);
+    }, []);
+
+    const beforeAssayDesignDelete = useCallback(() => {
+        onAssayDesignChange(assayDefinition.protocolSchemaName);
+    }, [assayDefinition?.protocolSchemaName]);
+
+    const afterAssayDesignDelete = useCallback((success: boolean) => {
+        if (success) {
+            menuInit();
+            clearAssayDefinitionCache();
+            navigate(AppURL.create(ASSAYS_KEY));
+        } else {
+            // delay to make sure grid invalidate form beforeAssayDesignDelete is finished
+            window.setTimeout(resetState, 100);
+        }
+    }, [menuInit, navigate]);
+
+    return (
+        <>
+            <RequiresPermission permissionCheck="any"
+                                perms={[PermissionTypes.Read, PermissionTypes.DesignAssay, PermissionTypes.CanSeeAuditLog]}>
+                <ManageDropdownButton id={'assayheader'} pullRight collapsed>
+                    <RequiresPermission perms={PermissionTypes.DesignAssay}>
+                        <MenuItem
+                            href={AppURL.create(
+                                ASSAY_DESIGN_KEY,
+                                assayDefinition.type,
+                                assayDefinition.name
+                            ).toHref()}
+                        >
+                            Edit Assay Design
+                        </MenuItem>
+                    </RequiresPermission>
+                    <RequiresPermission perms={PermissionTypes.DesignAssay}>
+                        <MenuItem
+                            href={AppURL.create(
+                                ASSAY_DESIGN_KEY,
+                                assayProtocol.providerName,
+                                assayProtocol.name,
+                                'copy'
+                            ).toHref()}
+                        >
+                            Copy Assay Design
+                        </MenuItem>
+                    </RequiresPermission>
+                    {isAssayDesignExportEnabled() && <AssayExportDesignButton asMenuItem/>}
+                    <RequiresPermission perms={PermissionTypes.DesignAssay}>
+                        <MenuItem onClick={onDeleteAssayDesign}>Delete Assay Design</MenuItem>
+                    </RequiresPermission>
+                    <RequiresPermission perms={PermissionTypes.CanSeeAuditLog}>
+                        <MenuItem href={AppURL.create(AUDIT_KEY).addParams({
+                            eventType: ASSAY_AUDIT_QUERY.value,
+                            'query.q': assayDefinition.name
+                        }).toHref()}>
+                            View Audit History
+                        </MenuItem>
+                    </RequiresPermission>
+                </ManageDropdownButton>
+            </RequiresPermission>
+            {showConfirmDeleteAssayDesign && (
+                <AssayDesignDeleteModal
+                    assay={assayDefinition}
+                    beforeDelete={beforeAssayDesignDelete}
+                    afterDelete={afterAssayDesignDelete}
+                    onCancel={resetState}
+                />
+            )}
+        </>
+    );
+};
--- packages/components/src/assay/AssayHeader.tsx
+++ packages/components/src/assay/AssayHeader.tsx
@@ -1,65 +1,23 @@
-import React, { FC, ReactNode, useCallback, useState } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { ProductMenuModel } from '../internal/components/navigation/model';
-import { AppURL } from '../internal/url/AppURL';
-import { AssayProtocolModel } from '../internal/components/domainproperties/assay/models';
-import { AssayDefinitionModel } from '../internal/AssayDefinitionModel';
-import { clearAssayDefinitionCache } from '../internal/components/assay/actions';
 import { getTitleDisplay, hasActivePipelineJob } from '../internal/components/pipeline/utils';
 import { PageDetailHeader } from '../internal/components/forms/PageDetailHeader';
 import { TemplateDownloadButton } from '../public/files/TemplateDownloadButton';
-import { RequiresPermission } from '../internal/components/base/Permissions';
-import { PermissionTypes } from '@labkey/api';
-import { ManageDropdownButton } from '../internal/components/buttons/ManageDropdownButton';
-import { MenuItem } from 'react-bootstrap';
-import { ASSAY_DESIGN_KEY, ASSAYS_KEY, AUDIT_KEY } from '../internal/app/constants';
-import { isAssayDesignExportEnabled } from '../internal/app/utils';
-import { AssayExportDesignButton } from './AssayButtons';
-import { ASSAY_AUDIT_QUERY } from '../internal/components/auditlog/constants';
-import { AssayDesignDeleteModal } from './AssayDesignDeleteModal';
+import { ASSAYS_KEY } from '../internal/app/constants';
+import { AssayDesignHeaderButtons, AssayHeaderButtonProps, AssayRunDetailHeaderButtons } from './AssayButtons';
 import { Notifications } from '../internal/components/notifications/Notifications';
-import { onAssayDesignChange } from './actions';
 
-interface Props {
-    assayDefinition?: AssayDefinitionModel;
-    assayProtocol?: AssayProtocolModel;
+
+interface Props extends AssayHeaderButtonProps {
     title?: ReactNode;
     subTitle?: ReactNode;
     staticTitle?: ReactNode;
     description?: ReactNode;
-    renderButtons?: () => ReactNode;
-    menu: ProductMenuModel,
-    menuInit: (invalidate?: boolean) => void;
-    navigate: (url: string | AppURL, replace?: boolean) => void;
+    menu: ProductMenuModel;
 }
 
 export const AssayHeader: FC<Props> = props => {
-    const { assayDefinition, assayProtocol, staticTitle, title, subTitle, description, renderButtons, menu, menuInit, navigate } = props;
-
-    const [showConfirmDeleteAssayDesign, setShowConfirmDeleteAssayDesign] = useState<boolean>(false);
-
-    const resetState = useCallback(() => {
-        setShowConfirmDeleteAssayDesign(false)
-    }, []);
-
-    const onDeleteAssayDesign = useCallback(() => {
-        setShowConfirmDeleteAssayDesign(true);
-    }, []);
-
-    const beforeAssayDesignDelete = useCallback(() => {
-        onAssayDesignChange(assayDefinition.protocolSchemaName);
-    }, [assayDefinition?.protocolSchemaName]);
-
-    const afterAssayDesignDelete = useCallback((success: boolean) => {
-        if (success) {
-            menuInit();
-            clearAssayDefinitionCache();
-            navigate(AppURL.create(ASSAYS_KEY));
-        } else {
-            // delay to make sure grid invalidate form beforeAssayDesignDelete is finished
-            window.setTimeout(resetState, 100);
-        }
-    }, [menuInit, navigate]);
-
+    const { assayDefinition, assayProtocol, staticTitle, title, subTitle, description, menu, runId, ...buttonProps } = props;
 
     const isJobActive = assayDefinition ? hasActivePipelineJob(menu, ASSAYS_KEY, assayDefinition.name) : false;
     let titleDisplay = staticTitle ?? title;
@@ -83,59 +41,23 @@ export const AssayHeader: FC<Props> = props => {
                 leftColumns={9}
             >
                 <TemplateDownloadButton templateUrl={assayDefinition?.templateLink} className="button-right-spacing" />
-                {!!renderButtons && renderButtons()}
-                {!renderButtons && assayDefinition && (
-                    <RequiresPermission permissionCheck="any" perms={[PermissionTypes.Read, PermissionTypes.DesignAssay, PermissionTypes.CanSeeAuditLog]}>
-                        <ManageDropdownButton id={'assayheader'} pullRight collapsed>
-
-                            <RequiresPermission perms={PermissionTypes.DesignAssay}>
-                                <MenuItem
-                                    href={AppURL.create(
-                                        ASSAY_DESIGN_KEY,
-                                        assayDefinition.type,
-                                        assayDefinition.name
-                                    ).toHref()}
-                                >
-                                    Edit Assay Design
-                                </MenuItem>
-                            </RequiresPermission>
-                            <RequiresPermission perms={PermissionTypes.DesignAssay}>
-                                <MenuItem
-                                    href={AppURL.create(
-                                        ASSAY_DESIGN_KEY,
-                                        assayProtocol.providerName,
-                                        assayProtocol.name,
-                                        'copy'
-                                    ).toHref()}
-                                >
-                                    Copy Assay Design
-                                </MenuItem>
-                            </RequiresPermission>
-                            {isAssayDesignExportEnabled() && <AssayExportDesignButton asMenuItem />}
-                            <RequiresPermission perms={PermissionTypes.DesignAssay}>
-                                <MenuItem onClick={onDeleteAssayDesign}>Delete Assay Design</MenuItem>
-                            </RequiresPermission>
-                            <RequiresPermission perms={PermissionTypes.CanSeeAuditLog}>
-                                <MenuItem href={AppURL.create(AUDIT_KEY).addParams({
-                                    eventType: ASSAY_AUDIT_QUERY.value,
-                                    'query.q': assayDefinition.name
-                                }).toHref()}>
-                                    View Audit History
-                                </MenuItem>
-                            </RequiresPermission>
-                        </ManageDropdownButton>
-                    </RequiresPermission>
+                {runId != null && (
+                    <AssayRunDetailHeaderButtons
+                        assayDefinition={assayDefinition}
+                        assayProtocol={assayProtocol}
+                        runId={runId}
+                        {...buttonProps}
+                    />
+                )}
+                {runId == null && assayDefinition && (
+                    <AssayDesignHeaderButtons
+                        assayDefinition={assayDefinition}
+                        assayProtocol={assayProtocol}
+                        {...buttonProps}
+                    />
                 )}
             </PageDetailHeader>
             <Notifications />
-            {showConfirmDeleteAssayDesign && (
-                <AssayDesignDeleteModal
-                    assay={assayDefinition}
-                    beforeDelete={beforeAssayDesignDelete}
-                    afterDelete={afterAssayDesignDelete}
-                    onCancel={resetState}
-                />
-            )}
         </>
     );
 }

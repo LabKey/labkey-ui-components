ecc9a6b3717ee942432d14bfc9a015edf854a968
labkey-susanh <susanh@labkey.com>
2022-11-10 13:37:56 -0800

    v2.249.1-assayDisplayPages.0

---
+++ packages/components/src/assay/AssayOverrideBanner.tsx
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018-2019 LabKey Corporation. All rights reserved. No portion of this work may be reproduced in
+ * any form or by any electronic or mechanical means without written permission from LabKey Corporation.
+ */
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { AssayDefinitionModel, AssayLink } from '../internal/AssayDefinitionModel';
+import { getLocation } from '../internal/util/URL';
+import { Alert } from '../internal/components/base/Alert';
+
+interface Props {
+    assay: AssayDefinitionModel
+    link: AssayLink
+}
+
+export class AssayOverrideBanner extends React.Component<Props, any> {
+    static defaultProps = {
+        override: AssayLink.BEGIN
+    };
+
+    onClick = (e) => {
+        // We really should be generating a real link with an href in this component, but due to the design of the
+        // QueryGridPanel this component is not updated when filters change, so we never re-render the href when a user
+        // adds or removes a filter. The only way to get the most up to date query string is to compute on click.
+        e.stopPropagation();
+        e.preventDefault();
+        const { assay, link } = this.props;
+        let href = assay.links.get(link);
+        const { query } = getLocation();
+        // Convert query param object to query string, query params on location object are already encoded.
+        const queryString = query.map((v, k) => `${k}=${v}`).join('&');
+
+        if (queryString) {
+            href = `${href}&${queryString}`;
+        }
+
+        window.location.href = href;
+    };
+
+    render() {
+        const { assay, link } = this.props;
+
+        if (assay && assay.type.toLowerCase() !== 'general') {
+            return (
+                <Alert bsStyle="warning" className="test-loc-assay-override">
+                    <i className="fa fa-exclamation-circle" style={{paddingRight: '10px'}}/>
+                    This assay has custom views that provide more specific insights. It is recommended you view this in
+                    LabKey Server.
+                    {assay.links.get(link) !== undefined && (
+                        <div className="pull-right">
+                            <Button bsStyle="warning" bsSize="xs" onClick={this.onClick}>View in LabKey Server</Button>
+                        </div>
+                    )}
+                </Alert>
+            );
+        }
+
+        return null;
+    }
+}
---
+++ packages/components/src/assay/AssayOverviewPage.tsx
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2016-2019 LabKey Corporation. All rights reserved. No portion of this work may be reproduced in
+ * any form or by any electronic or mechanical means without written permission from LabKey Corporation.
+ */
+import React, { FC } from 'react';
+import { Filter } from '@labkey/api';
+
+import { useAssayAppContext } from './AssayAppContext';
+import { InjectedAssayModel } from '../internal/components/assay/withAssayModels';
+import { InjectedQueryModels, QueryConfigMap, withQueryModels } from '../public/QueryModel/withQueryModels';
+import {
+    Alert,
+    AppURL,
+    AssayLink,
+    DetailPanel,
+    GENERAL_ASSAY_PROVIDER_NAME,
+    Hooks,
+    LoadingPage,
+    Page,
+    ProductMenuModel,
+    QueryColumn,
+    QueryModel,
+    SCHEMAS
+} from '../index';
+import { AssayHeader } from './AssayHeader';
+import { AssayDesignHeaderButtons } from './AssayButtons';
+import { assayPage } from './AssayPageHOC';
+import { AssayOverrideBanner } from './AssayOverrideBanner';
+import { AssayGridPanel } from './AssayGridPanel';
+
+const REQUIRED_COLUMN_NAMES = ['Description', 'Created', 'CreatedBy', 'Status'];
+
+interface CommonPageProps {
+    goBack: (n?: number) => void;
+    menu: ProductMenuModel;
+    menuInit: (invalidate?: boolean) => void;
+    navigate: (url: string | AppURL, replace?: boolean) => void;
+}
+
+type AssayDetailsProps = CommonPageProps & InjectedAssayModel & InjectedQueryModels;
+
+const AssayDetailsPageBody: FC<AssayDetailsProps> = props => {
+    const { assayDefinition, assayProtocol, actions, navigate, menu, menuInit, queryModels } = props;
+    const { model } = queryModels;
+    const { name, type, description } = assayDefinition;
+    const protocolContext = Hooks.useContainerUser(assayProtocol.domains.first()?.container);
+    const { showProviderName, detailRenderer } = useAssayAppContext();
+    const subtitle = showProviderName ? (type === GENERAL_ASSAY_PROVIDER_NAME ? "Standard" : type) + " Assay Overview" : undefined;
+
+    const getDetailsColumns = (model: QueryModel): QueryColumn[] => {
+        return REQUIRED_COLUMN_NAMES.map((name) => model.getColumn(name));
+    };
+
+    if (model.isLoading || !protocolContext.isLoaded) {
+        return <LoadingPage title="Assay Overview" />;
+    }
+
+    return (
+        <Page hasHeader title={`${name} - Assay Overview`}>
+            <AssayHeader
+                menu={menu}
+                subTitle={subtitle}
+                description={description}
+            >
+                <AssayDesignHeaderButtons menuInit={menuInit} navigate={navigate}/>
+            </AssayHeader>
+
+            <Alert>{protocolContext.error}</Alert>
+
+            <AssayOverrideBanner assay={assayDefinition} link={AssayLink.BEGIN} />
+
+            <DetailPanel
+                asPanel
+                detailRenderer={detailRenderer}
+                actions={actions}
+                model={model}
+                queryColumns={getDetailsColumns(model)}
+            />
+
+            <AssayGridPanel
+                assayDefinition={assayDefinition}
+                canDelete={true}
+                queryName="Runs"
+                header="Runs"
+                nounPlural="Runs"
+            />
+
+        </Page>
+    );
+};
+
+const AssayDetailsPageWithModels = withQueryModels<InjectedAssayModel>(AssayDetailsPageBody);
+
+const AssayDetailsPageImpl: FC<InjectedAssayModel> = props => {
+    const { assayDefinition } = props;
+
+    const queryConfigs: QueryConfigMap = {
+        model: {
+            schemaQuery: SCHEMAS.ASSAY_TABLES.ASSAY_DETAILS_SQ,
+            baseFilters: [Filter.create('Name', assayDefinition.name)],
+            omittedColumns: ['Name', 'LSID'],
+            requiredColumns: REQUIRED_COLUMN_NAMES,
+        },
+    };
+
+    return <AssayDetailsPageWithModels autoLoad key={assayDefinition.name} queryConfigs={queryConfigs} {...props} />;
+};
+
+export const AssayDetailsPage = assayPage(AssayDetailsPageImpl);

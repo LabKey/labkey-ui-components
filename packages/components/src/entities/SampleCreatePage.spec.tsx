e91addbd4774c23bbbc69891644d55af1811f52c
cnathe <cnathe@labkey.com>
2022-11-16 09:09:42 -0600

    Jest tests for SampleCreatePage

--- packages/components/src/entities/SampleCreatePage.spec.tsx
+++ packages/components/src/entities/SampleCreatePage.spec.tsx
@@ -4,9 +4,10 @@ import { ReactWrapper } from 'enzyme';
 import { InsufficientPermissionsPage } from '../internal/components/permissions/InsufficientPermissionsPage';
 import { EntityInsertPanel } from '../internal/components/entities/EntityInsertPanel';
 import { mountWithAppServerContext, waitForLifecycle } from '../internal/testHelpers';
-import { TEST_USER_APP_ADMIN, TEST_USER_AUTHOR, TEST_USER_READER } from '../internal/userFixtures';
+import { TEST_USER_APP_ADMIN, TEST_USER_AUTHOR, TEST_USER_EDITOR, TEST_USER_READER } from '../internal/userFixtures';
 import { createMockWithRouterProps } from '../internal/mockUtils';
 import { ProductMenuModel } from '../internal/components/navigation/model';
+import { BACKGROUND_IMPORT_MIN_FILE_SIZE } from '../internal/components/pipeline/constants';
 
 import { SampleCreatePage, SampleCreatePageProps } from './SampleCreatePage';
 import { SampleTypeBasePage } from './SampleTypeBasePage';
@@ -24,14 +25,14 @@ function getDefaultProps(): SampleCreatePageProps {
 const SAMPLE_TYPE_APP_CONTEXT = {} as SampleTypeAppContext;
 
 describe('SampleCreatePage', () => {
-    function validate(wrapper: ReactWrapper, hasPermission = true, title = 'Create New Samples'): void {
+    function validate(wrapper: ReactWrapper, hasPermission = true): void {
         expect(wrapper.find(InsufficientPermissionsPage)).toHaveLength(!hasPermission ? 1 : 0);
+        expect(wrapper.find(SampleTypeBasePage)).toHaveLength(hasPermission ? 1 : 0);
+        expect(wrapper.find(EntityInsertPanel)).toHaveLength(hasPermission ? 1 : 0);
 
         if (hasPermission) {
-            expect(wrapper.find(SampleTypeBasePage)).toHaveLength(1);
-            expect(wrapper.find(SampleTypeBasePage).prop('subtitle')).toBe(title);
-            expect(wrapper.find(EntityInsertPanel)).toHaveLength(1);
-
+            expect(wrapper.find(SampleTypeBasePage).prop('title')).toBe('Sample Type');
+            expect(wrapper.find(SampleTypeBasePage).prop('subtitle')).toBe('Create New Samples');
             const entityDataType = wrapper.find(EntityInsertPanel).prop('entityDataType');
             expect(entityDataType.filterArray.length).toBe(1);
             expect(entityDataType.filterArray[0].getURLParameterName()).toBe('query.Category~neqornull');
@@ -39,7 +40,7 @@ describe('SampleCreatePage', () => {
         }
     }
 
-    test('insert reader permissions', async () => {
+    test('reader', async () => {
         const wrapper = mountWithAppServerContext(
             <SampleCreatePage {...getDefaultProps()} />,
             { sampleType: SAMPLE_TYPE_APP_CONTEXT },
@@ -52,20 +53,7 @@ describe('SampleCreatePage', () => {
         wrapper.unmount();
     });
 
-    test('insert author permissions', async () => {
-        const wrapper = mountWithAppServerContext(
-            <SampleCreatePage {...getDefaultProps()} />,
-            { sampleType: SAMPLE_TYPE_APP_CONTEXT },
-            {
-                user: TEST_USER_AUTHOR,
-            }
-        );
-        await waitForLifecycle(wrapper);
-        validate(wrapper);
-        wrapper.unmount();
-    });
-
-    test('author permissions', async () => {
+    test('author', async () => {
         const wrapper = mountWithAppServerContext(
             <SampleCreatePage {...getDefaultProps()} />,
             { sampleType: SAMPLE_TYPE_APP_CONTEXT },
@@ -79,8 +67,21 @@ describe('SampleCreatePage', () => {
         wrapper.unmount();
     });
 
-    test('admin permissions', async () => {
-        // works when you put it first, but breaks insert reader permissions
+    test('editor', async () => {
+        const wrapper = mountWithAppServerContext(
+            <SampleCreatePage {...getDefaultProps()} />,
+            { sampleType: SAMPLE_TYPE_APP_CONTEXT },
+            {
+                user: TEST_USER_EDITOR,
+            }
+        );
+        await waitForLifecycle(wrapper);
+        validate(wrapper);
+        expect(wrapper.find(EntityInsertPanel).prop('canEditEntityTypeDetails')).toBeFalsy();
+        wrapper.unmount();
+    });
+
+    test('admin', async () => {
         const wrapper = mountWithAppServerContext(
             <SampleCreatePage {...getDefaultProps()} />,
             { sampleType: SAMPLE_TYPE_APP_CONTEXT },
@@ -91,6 +92,24 @@ describe('SampleCreatePage', () => {
         await waitForLifecycle(wrapper);
         validate(wrapper);
         expect(wrapper.find(EntityInsertPanel).prop('canEditEntityTypeDetails')).toBeTruthy();
+        expect(wrapper.find(EntityInsertPanel).prop('asyncSize')).toBe(BACKGROUND_IMPORT_MIN_FILE_SIZE);
+        wrapper.unmount();
+    });
+
+    test('useAsync', async () => {
+        const wrapper = mountWithAppServerContext(
+            <SampleCreatePage
+                {...getDefaultProps()}
+                location={{ ...getDefaultProps().location, query: { useAsync: 'true' } }}
+            />,
+            { sampleType: SAMPLE_TYPE_APP_CONTEXT },
+            {
+                user: TEST_USER_APP_ADMIN,
+            }
+        );
+        await waitForLifecycle(wrapper);
+        validate(wrapper);
+        expect(wrapper.find(EntityInsertPanel).prop('asyncSize')).toBe(1);
         wrapper.unmount();
     });
 

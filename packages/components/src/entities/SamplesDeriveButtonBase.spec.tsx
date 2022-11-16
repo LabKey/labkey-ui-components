a327e8d238d4bc3ff3ebf054a45befe175f28172
cnathe <cnathe@labkey.com>
2022-11-16 08:57:25 -0600

    Jest tests for SamplesDeriveButtonBase changes

--- packages/components/src/entities/SamplesDeriveButtonBase.spec.tsx
+++ packages/components/src/entities/SamplesDeriveButtonBase.spec.tsx
@@ -11,6 +11,7 @@ import { TEST_USER_EDITOR, TEST_USER_READER } from '../internal/userFixtures';
 
 import { SamplesDeriveButtonBase } from './SamplesDeriveButtonBase';
 import { CreateSamplesSubMenu } from './CreateSamplesSubMenu';
+import { DisableableButton } from '../internal/components/buttons/DisableableButton';
 
 describe('SamplesDeriveButtonBase', () => {
     const DEFAULT_PROPS = {
@@ -25,9 +26,10 @@ describe('SamplesDeriveButtonBase', () => {
         isSelectingSamples: () => true,
     };
 
-    function validate(wrapper: ReactWrapper, rendered = true, asSubMenu = false): void {
-        expect(wrapper.find(DropdownButton)).toHaveLength(rendered && !asSubMenu ? 1 : 0);
-        expect(wrapper.find(CreateSamplesSubMenu)).toHaveLength(rendered ? 3 : 0);
+    function validate(wrapper: ReactWrapper, rendered = true, asSubMenu = false, disabled = false): void {
+        expect(wrapper.find(DropdownButton)).toHaveLength(rendered && !asSubMenu && !disabled ? 1 : 0);
+        expect(wrapper.find(CreateSamplesSubMenu)).toHaveLength(rendered && !disabled ? 3 : 0);
+        expect(wrapper.find(DisableableButton)).toHaveLength(disabled ? 1 : 0);
     }
 
     test('default props', () => {
@@ -53,6 +55,18 @@ describe('SamplesDeriveButtonBase', () => {
             user: TEST_USER_READER,
         });
         validate(wrapper, false);
+        expect(wrapper.find(SubMenuItem)).toHaveLength(0);
+        wrapper.unmount();
+    });
+
+    test('over max selections', () => {
+        const model = makeTestQueryModel(SchemaQuery.create('schema', 'query')).mutate({
+            selections: new Set(Array.from(Array(1001).keys()).map(key => key + '')),
+        });
+        const wrapper = mountWithServerContext(<SamplesDeriveButtonBase {...DEFAULT_PROPS} model={model} />, {
+            user: TEST_USER_EDITOR,
+        });
+        validate(wrapper, false, false, true);
         expect(wrapper.find(SubMenuItem)).toHaveLength(0);
         wrapper.unmount();
     });

fd49fbed0e3a3dfabd9ec1998b54416e6610583f
labkey-susanh <susanh@labkey.com>
2022-11-18 08:03:19 -0800

    Replace mega-create menu with simple Create Assay Design button

--- packages/components/src/assay/AssayListingPage.tsx
+++ packages/components/src/assay/AssayListingPage.tsx
@@ -1,11 +1,11 @@
-import React, { FC, memo, ReactNode } from 'react';
+import React, { FC, memo } from 'react';
 
 import { CommonPageProps } from '../internal/models';
 import { useServerContext } from '../internal/components/base/ServerContext';
 import { InsufficientPermissionsPage } from '../internal/components/permissions/InsufficientPermissionsPage';
 import { userCanReadAssays } from '../internal/app/utils';
 import { LoadingPage } from '../internal/components/base/LoadingPage';
-import { ASSAYS_KEY } from '../internal/app/constants';
+import { ASSAYS_KEY, NEW_ASSAY_DESIGN_HREF, NEW_STANDARD_ASSAY_DESIGN_HREF } from '../internal/app/constants';
 import { Page } from '../internal/components/base/Page';
 
 import { AssayDesignEmptyAlert } from '../internal/components/assay/AssayDesignEmptyAlert';
@@ -13,15 +13,12 @@ import { Section } from '../internal/components/base/Section';
 
 import { AssayTypeSummary } from './AssayTypeSummary';
 import { useAssayAppContext } from './AssayAppContext';
+import { Button } from 'react-bootstrap';
 
 const ASSAY_CAPTION = 'Capture analytical data about samples';
 
-interface Props {
-    buttons: ReactNode;
-}
-
-export const AssayListingPage: FC<Props & CommonPageProps> = memo(props => {
-    const { buttons, menu, navigate } = props;
+export const AssayListingPage: FC<CommonPageProps> = memo(props => {
+    const { menu, navigate } = props;
     const { user } = useServerContext();
     const { assayTypes, excludedAssayProviders } = useAssayAppContext();
     const pageTitle = 'Assays';
@@ -32,11 +29,17 @@ export const AssayListingPage: FC<Props & CommonPageProps> = memo(props => {
         return <LoadingPage title={pageTitle} />;
     }
 
+    let button = undefined;
+    if (user.hasDesignAssaysPermission()) {
+        const href = assayTypes?.length === 1 ? NEW_STANDARD_ASSAY_DESIGN_HREF.toHref() : NEW_ASSAY_DESIGN_HREF.toHref();
+        button = <Button bsStyle="success" href={href}>Create Assay Design</Button>
+    }
+
     const hasItems = menu.hasSectionItems(ASSAYS_KEY);
 
     return (
         <Page title={pageTitle}>
-            <Section caption={ASSAY_CAPTION} context={buttons} title={pageTitle}>
+            <Section caption={ASSAY_CAPTION} context={button} title={pageTitle}>
                 {hasItems && (
                     <AssayTypeSummary
                         assayTypes={assayTypes}

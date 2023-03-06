c9c1de877bf9049128db60cfc840e6d69a7338e3
labkey-susanh <susanh@labkey.com>
2023-03-06 09:58:26 -0800

    Take out testing code

--- packages/components/src/internal/components/editable/EditableGrid.tsx
+++ packages/components/src/internal/components/editable/EditableGrid.tsx
@@ -1383,8 +1383,7 @@ export class EditableGrid extends PureComponent<EditableGridProps, EditableGridS
 
         if (showAsTab) {
             const bulkDisabled = selected.size === 0;
-            // const gridDisabled = editorModel.rowCount > maxRows;
-            const gridDisabled = true;
+            const showGrid = editorModel.rowCount > maxRows;
 
             return (
                 <>
@@ -1398,7 +1397,7 @@ export class EditableGrid extends PureComponent<EditableGridProps, EditableGridS
                                         Edit in Bulk
                                     </NavItem>
                                 )}
-                                {!gridDisabled && (
+                                {!showGrid && (
                                     <NavItem eventKey={EditableGridTabs.Grid}>
                                         Edit Individually
                                     </NavItem>

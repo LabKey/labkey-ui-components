6fd34df3cb82d788936867f8740ae2ed7c69cbb0
XingY <xyang@labkey.com>
2023-03-31 13:06:16 -0700

    fix jest

--- packages/components/src/internal/components/domainproperties/utils.spec.ts
+++ packages/components/src/internal/components/domainproperties/utils.spec.ts
@@ -1,47 +1,41 @@
-import { SampleTypeModel } from './samples/models';
+import { parentAliasInvalid } from './utils';
 
 describe('domain property utils', () => {
     test('parentAliasInvalid', () => {
-        expect(SampleTypeModel.create().parentAliasInvalid(undefined)).toBeTruthy();
-        expect(SampleTypeModel.create().parentAliasInvalid(null)).toBeTruthy();
-        expect(SampleTypeModel.create().parentAliasInvalid({})).toBeTruthy();
-        expect(
-            SampleTypeModel.create().parentAliasInvalid({ alias: 'ali', parentValue: { value: 'val' } })
-        ).toBeFalsy();
+        expect(parentAliasInvalid(undefined)).toBeTruthy();
+        expect(parentAliasInvalid(null)).toBeTruthy();
+        expect(parentAliasInvalid({})).toBeTruthy();
+        expect(parentAliasInvalid({ alias: 'ali', parentValue: { value: 'val' } })).toBeFalsy();
 
         expect(
-            SampleTypeModel.create().parentAliasInvalid({
+            parentAliasInvalid({
                 alias: undefined,
                 parentValue: { value: 'val' },
             })
         ).toBeTruthy();
-        expect(
-            SampleTypeModel.create().parentAliasInvalid({ alias: null, parentValue: { value: 'val' } })
-        ).toBeTruthy();
-        expect(SampleTypeModel.create().parentAliasInvalid({ alias: '', parentValue: { value: 'val' } })).toBeTruthy();
-        expect(SampleTypeModel.create().parentAliasInvalid({ alias: ' ', parentValue: { value: 'val' } })).toBeTruthy();
+        expect(parentAliasInvalid({ alias: null, parentValue: { value: 'val' } })).toBeTruthy();
+        expect(parentAliasInvalid({ alias: '', parentValue: { value: 'val' } })).toBeTruthy();
+        expect(parentAliasInvalid({ alias: ' ', parentValue: { value: 'val' } })).toBeTruthy();
 
-        expect(SampleTypeModel.create().parentAliasInvalid({ alias: 'ali', parentValue: undefined })).toBeTruthy();
+        expect(parentAliasInvalid({ alias: 'ali', parentValue: undefined })).toBeTruthy();
         expect(
-            SampleTypeModel.create().parentAliasInvalid({
+            parentAliasInvalid({
                 alias: 'ali',
                 parentValue: { value: undefined },
             })
         ).toBeTruthy();
-        expect(
-            SampleTypeModel.create().parentAliasInvalid({ alias: 'ali', parentValue: { value: null } })
-        ).toBeTruthy();
-        expect(SampleTypeModel.create().parentAliasInvalid({ alias: 'ali', parentValue: { value: '' } })).toBeTruthy();
+        expect(parentAliasInvalid({ alias: 'ali', parentValue: { value: null } })).toBeTruthy();
+        expect(parentAliasInvalid({ alias: 'ali', parentValue: { value: '' } })).toBeTruthy();
 
         expect(
-            SampleTypeModel.create().parentAliasInvalid({
+            parentAliasInvalid({
                 alias: 'ali',
                 parentValue: { value: 'val' },
                 isDupe: true,
             })
         ).toBeTruthy();
         expect(
-            SampleTypeModel.create().parentAliasInvalid({
+            parentAliasInvalid({
                 alias: 'ali',
                 parentValue: { value: 'val' },
                 isDupe: false,

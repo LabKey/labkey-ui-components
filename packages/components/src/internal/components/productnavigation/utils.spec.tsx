d626750a4c19cff6c065eb0a306e2b1f4beb8b35
cnathe <cnathe@labkey.com>
2023-05-12 15:59:49 -0500

    Update shouldShowProductNavigation() to include check for LKB app instead of just hasPremiumModule

---
+++ packages/components/src/internal/components/productnavigation/utils.spec.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+import { TEST_USER_APP_ADMIN, TEST_USER_EDITOR } from '../../userFixtures';
+import { TEST_LIMS_STARTER_MODULE_CONTEXT, TEST_LKSM_STARTER_MODULE_CONTEXT } from '../../productFixtures';
+
+import { shouldShowProductNavigation } from './utils';
+
+describe('shouldShowProductNavigation', () => {
+    test('LKB non-premium admin user', () => {
+        let moduleContext = { ...TEST_LIMS_STARTER_MODULE_CONTEXT, api: { applicationMenuDisplayMode: 'ADMIN' } };
+        expect(shouldShowProductNavigation(TEST_USER_APP_ADMIN, moduleContext)).toBeTruthy();
+
+        moduleContext = { ...TEST_LIMS_STARTER_MODULE_CONTEXT, api: { applicationMenuDisplayMode: 'ALWAYS' } };
+        expect(shouldShowProductNavigation(TEST_USER_APP_ADMIN, moduleContext)).toBeTruthy();
+    });
+    test('LKB non-premium non-admin user', () => {
+        let moduleContext = { ...TEST_LIMS_STARTER_MODULE_CONTEXT, api: { applicationMenuDisplayMode: 'ADMIN' } };
+        expect(shouldShowProductNavigation(TEST_USER_EDITOR, moduleContext)).toBeFalsy();
+
+        moduleContext = { ...TEST_LIMS_STARTER_MODULE_CONTEXT, api: { applicationMenuDisplayMode: 'ALWAYS' } };
+        expect(shouldShowProductNavigation(TEST_USER_EDITOR, moduleContext)).toBeTruthy();
+    });
+
+    test('LKB premium admin user', () => {
+        let moduleContext = {
+            ...TEST_LIMS_STARTER_MODULE_CONTEXT,
+            api: { applicationMenuDisplayMode: 'ADMIN', moduleNames: ['premium'] },
+        };
+        expect(shouldShowProductNavigation(TEST_USER_APP_ADMIN, moduleContext)).toBeTruthy();
+
+        moduleContext = {
+            ...TEST_LIMS_STARTER_MODULE_CONTEXT,
+            api: { applicationMenuDisplayMode: 'ALWAYS', moduleNames: ['premium'] },
+        };
+        expect(shouldShowProductNavigation(TEST_USER_APP_ADMIN, moduleContext)).toBeTruthy();
+    });
+
+    test('LKB premium non-admin user', () => {
+        let moduleContext = {
+            ...TEST_LIMS_STARTER_MODULE_CONTEXT,
+            api: { applicationMenuDisplayMode: 'ADMIN', moduleNames: ['premium'] },
+        };
+        expect(shouldShowProductNavigation(TEST_USER_EDITOR, moduleContext)).toBeFalsy();
+
+        moduleContext = {
+            ...TEST_LIMS_STARTER_MODULE_CONTEXT,
+            api: { applicationMenuDisplayMode: 'ALWAYS', moduleNames: ['premium'] },
+        };
+        expect(shouldShowProductNavigation(TEST_USER_EDITOR, moduleContext)).toBeTruthy();
+    });
+
+    test('LKSM non-premium admin user', () => {
+        let moduleContext = { ...TEST_LKSM_STARTER_MODULE_CONTEXT, api: { applicationMenuDisplayMode: 'ADMIN' } };
+        expect(shouldShowProductNavigation(TEST_USER_APP_ADMIN, moduleContext)).toBeFalsy();
+
+        moduleContext = { ...TEST_LKSM_STARTER_MODULE_CONTEXT, api: { applicationMenuDisplayMode: 'ALWAYS' } };
+        expect(shouldShowProductNavigation(TEST_USER_APP_ADMIN, moduleContext)).toBeFalsy();
+    });
+
+    test('LKSM non-premium non-admin user', () => {
+        let moduleContext = { ...TEST_LKSM_STARTER_MODULE_CONTEXT, api: { applicationMenuDisplayMode: 'ADMIN' } };
+        expect(shouldShowProductNavigation(TEST_USER_EDITOR, moduleContext)).toBeFalsy();
+
+        moduleContext = { ...TEST_LKSM_STARTER_MODULE_CONTEXT, api: { applicationMenuDisplayMode: 'ALWAYS' } };
+        expect(shouldShowProductNavigation(TEST_USER_EDITOR, moduleContext)).toBeFalsy();
+    });
+
+    test('LKSM premium admin user', () => {
+        let moduleContext = {
+            ...TEST_LKSM_STARTER_MODULE_CONTEXT,
+            api: { applicationMenuDisplayMode: 'ADMIN', moduleNames: ['premium'] },
+        };
+        expect(shouldShowProductNavigation(TEST_USER_APP_ADMIN, moduleContext)).toBeTruthy();
+
+        moduleContext = {
+            ...TEST_LKSM_STARTER_MODULE_CONTEXT,
+            api: { applicationMenuDisplayMode: 'ALWAYS', moduleNames: ['premium'] },
+        };
+        expect(shouldShowProductNavigation(TEST_USER_APP_ADMIN, moduleContext)).toBeTruthy();
+    });
+
+    test('LKSM premium non-admin user', () => {
+        let moduleContext = {
+            ...TEST_LKSM_STARTER_MODULE_CONTEXT,
+            api: { applicationMenuDisplayMode: 'ADMIN', moduleNames: ['premium'] },
+        };
+        expect(shouldShowProductNavigation(TEST_USER_EDITOR, moduleContext)).toBeFalsy();
+
+        moduleContext = {
+            ...TEST_LKSM_STARTER_MODULE_CONTEXT,
+            api: { applicationMenuDisplayMode: 'ALWAYS', moduleNames: ['premium'] },
+        };
+        expect(shouldShowProductNavigation(TEST_USER_EDITOR, moduleContext)).toBeTruthy();
+    });
+});
--- packages/components/src/internal/components/productnavigation/utils.ts
+++ packages/components/src/internal/components/productnavigation/utils.ts
@@ -1,10 +1,14 @@
 import { User } from '../base/models/User';
-import { hasPremiumModule, resolveModuleContext } from '../../app/utils';
+import { hasPremiumModule, isBiologicsEnabled, resolveModuleContext } from '../../app/utils';
 import { ModuleContext } from '../base/ServerContext';
 
+/**
+ * Returns true for the LKB app or the LKSM app w/ premium module when the user isAdmin or the Look and Feel Setting
+ * for applicationMenuDisplayMode is set to ALWAYS.
+ */
 export function shouldShowProductNavigation(user?: User, moduleContext?: ModuleContext): boolean {
     return (
-        hasPremiumModule(moduleContext) &&
+        (isBiologicsEnabled(moduleContext) || hasPremiumModule(moduleContext)) &&
         (user?.isAdmin ||
             resolveModuleContext(moduleContext)?.api?.applicationMenuDisplayMode?.toLowerCase() === 'always')
     );

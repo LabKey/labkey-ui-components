23bf2a562c5ca92144650ec726394b2ec66ff8fb
XingY <xyang@labkey.com>
2023-05-17 14:15:42 -0700

    add jest

---
+++ packages/components/src/internal/components/project/ProjectDataTypeSelections.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, Col } from 'react-bootstrap';
+
+import { mountWithAppServerContext } from '../../testHelpers';
+
+import { AssayRunDataType, SampleTypeDataType } from '../entities/constants';
+
+import { ProjectDataTypeSelections } from './ProjectDataTypeSelections';
+
+describe('ProjectDataTypeSelections', () => {
+    test('without project Id - new project', () => {
+        const wrapper = mountWithAppServerContext(<ProjectDataTypeSelections entityDataTypes={[SampleTypeDataType]} />);
+        expect(wrapper.find(Button)).toHaveLength(0);
+    });
+
+    test('with project Id', () => {
+        const wrapper = mountWithAppServerContext(
+            <ProjectDataTypeSelections entityDataTypes={[SampleTypeDataType]} projectId="123" />
+        );
+        expect(wrapper.find(Button)).toHaveLength(1);
+        expect(wrapper.find(Col)).toHaveLength(1);
+    });
+
+    test('with 2 entityDataTypes', () => {
+        const wrapper = mountWithAppServerContext(
+            <ProjectDataTypeSelections entityDataTypes={[SampleTypeDataType, AssayRunDataType]} projectId="123" />
+        );
+        expect(wrapper.find(Button)).toHaveLength(1);
+        expect(wrapper.find(Col)).toHaveLength(2);
+    });
+});

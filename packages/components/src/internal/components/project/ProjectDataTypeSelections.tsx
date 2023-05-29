7495523ba9f6bc0dbf86fb6481726dbc3aee7215
cnathe <cnathe@labkey.com>
2023-05-29 09:29:50 -0500

    Project data type panel save button to show Saving during save action

--- packages/components/src/internal/components/project/ProjectDataTypeSelections.tsx
+++ packages/components/src/internal/components/project/ProjectDataTypeSelections.tsx
@@ -96,7 +96,7 @@ export const ProjectDataTypeSelections: FC<Props> = memo(props => {
                                 disabled={isSaving || !dirty}
                                 onClick={onSave}
                             >
-                                Save
+                                {isSaving ? 'Saving' : 'Save'}
                             </Button>
                         </div>
                     )}

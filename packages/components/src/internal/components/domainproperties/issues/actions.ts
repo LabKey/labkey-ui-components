ac5d63f93c9b25782acd957c99f4ba2b4b99c85e
labkey-nicka <nickk@labkey.com>
2023-06-07 10:13:28 -0700

    IssuesAPIWrapper

--- packages/components/src/internal/components/domainproperties/issues/actions.ts
+++ packages/components/src/internal/components/domainproperties/issues/actions.ts
@@ -26,71 +26,102 @@ export function fetchIssuesListDefDesign(issueDefName?: string): Promise<IssuesL
     });
 }
 
-export function getUsersForGroup(groupId: number): Promise<List<Principal>> {
-    return new Promise((resolve, reject) => {
-        Ajax.request({
-            url: ActionURL.buildURL('issues', 'getUsersForGroup.api'),
-            params: { groupId },
-            success: Utils.getCallbackWrapper(coreUsersData => {
-                let users = List<Principal>();
-                coreUsersData.forEach(user => {
-                    const usr = Principal.create(user);
-                    users = users.push(usr);
-                });
-
-                resolve(users);
-            }),
-            failure: handleRequestFailure(reject, 'Failed to fetch users for group'),
-        });
-    });
+export interface IssuesAPIWrapper {
+    getProjectGroups: () => Promise<List<Principal>>;
+    getRelatedFolders: (issueDefName?: string) => Promise<List<IssuesRelatedFolder>>;
+    getUsersForGroup: (groupId: number) => Promise<List<Principal>>;
+    saveIssueListDefOptions: (options: IssuesListDefOptionsConfig) => Promise<DuplicateFilesResponse>;
 }
 
-export function getProjectGroups(): Promise<List<Principal>> {
-    return new Promise((resolve, reject) => {
-        Ajax.request({
-            url: ActionURL.buildURL('issues', 'getProjectGroups.api'),
-            success: Utils.getCallbackWrapper(coreGroupsData => {
-                let groups = List<Principal>();
-                coreGroupsData.forEach(principal => {
-                    const grp = Principal.create(principal);
-                    groups = groups.push(grp);
-                });
+export class IssuesServerAPIWrapper implements IssuesAPIWrapper {
+    getProjectGroups(): Promise<List<Principal>> {
+        return new Promise((resolve, reject) => {
+            Ajax.request({
+                url: ActionURL.buildURL('issues', 'getProjectGroups.api'),
+                success: Utils.getCallbackWrapper(coreGroupsData => {
+                    let groups = List<Principal>();
+                    coreGroupsData.forEach(principal => {
+                        const grp = Principal.create(principal);
+                        groups = groups.push(grp);
+                    });
 
-                resolve(groups);
-            }),
-            failure: handleRequestFailure(reject, 'Failed to fetch project groups'),
+                    resolve(groups);
+                }),
+                failure: handleRequestFailure(reject, 'Failed to fetch project groups'),
+            });
         });
-    });
+    }
+
+    getRelatedFolders(issueDefName?: string): Promise<List<IssuesRelatedFolder>> {
+        return new Promise((resolve, reject) => {
+            Ajax.request({
+                url: ActionURL.buildURL('issues', 'getRelatedFolder.api'),
+                params: { issueDefName },
+                success: Utils.getCallbackWrapper(res => {
+                    let folders = List<IssuesRelatedFolder>();
+                    res.containers.forEach(container => {
+                        const folder = IssuesRelatedFolder.create(container);
+                        folders = folders.push(folder);
+                    });
+                    resolve(folders);
+                }),
+                failure: handleRequestFailure(reject, 'Failed to fetch related folders'),
+            });
+        });
+    }
+
+    getUsersForGroup(groupId: number): Promise<List<Principal>> {
+        return new Promise((resolve, reject) => {
+            Ajax.request({
+                url: ActionURL.buildURL('issues', 'getUsersForGroup.api'),
+                params: { groupId },
+                success: Utils.getCallbackWrapper(coreUsersData => {
+                    let users = List<Principal>();
+                    coreUsersData.forEach(user => {
+                        const usr = Principal.create(user);
+                        users = users.push(usr);
+                    });
+
+                    resolve(users);
+                }),
+                failure: handleRequestFailure(reject, 'Failed to fetch users for group'),
+            });
+        });
+    }
+
+    saveIssueListDefOptions(options: IssuesListDefOptionsConfig): Promise<DuplicateFilesResponse> {
+        return new Promise((resolve, reject) => {
+            Ajax.request({
+                url: buildURL('issues', 'admin.api'),
+                method: 'POST',
+                params: options,
+                success: Utils.getCallbackWrapper(res => {
+                    resolve(res);
+                }),
+                failure: handleRequestFailure(reject, 'Failed to save issue list definition options'),
+            });
+        });
+    }
 }
 
-export function saveIssueListDefOptions(options: IssuesListDefOptionsConfig): Promise<DuplicateFilesResponse> {
-    return new Promise((resolve, reject) => {
-        Ajax.request({
-            url: buildURL('issues', 'admin.api'),
-            method: 'POST',
-            params: options,
-            success: Utils.getCallbackWrapper(res => {
-                resolve(res);
-            }),
-            failure: handleRequestFailure(reject, 'Failed to save issue list definition options'),
-        });
-    });
+let DEFAULT_WRAPPER: IssuesAPIWrapper;
+
+export function getDefaultIssuesAPIWrapper(): IssuesAPIWrapper {
+    if (!DEFAULT_WRAPPER) {
+        DEFAULT_WRAPPER = new IssuesServerAPIWrapper();
+    }
+
+    return DEFAULT_WRAPPER;
 }
 
-export function getRelatedFolders(issueDefName?: string): Promise<List<IssuesRelatedFolder>> {
-    return new Promise((resolve, reject) => {
-        Ajax.request({
-            url: ActionURL.buildURL('issues', 'getRelatedFolder.api'),
-            params: { issueDefName },
-            success: Utils.getCallbackWrapper(res => {
-                let folders = List<IssuesRelatedFolder>();
-                res.containers.forEach(container => {
-                    const folder = IssuesRelatedFolder.create(container);
-                    folders = folders.push(folder);
-                });
-                resolve(folders);
-            }),
-            failure: handleRequestFailure(reject, 'Failed to fetch related folders'),
-        });
-    });
+export function getIssuesTestAPIWrapper(
+    mockFn = (): any => () => {},
+    overrides: Partial<IssuesAPIWrapper> = {}
+): IssuesAPIWrapper {
+    return {
+        getProjectGroups: mockFn(),
+        getRelatedFolders: mockFn(),
+        getUsersForGroup: mockFn(),
+        saveIssueListDefOptions: mockFn(),
+    };
 }

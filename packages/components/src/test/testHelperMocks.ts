c519a4a6d2bfb349055b7b61fbc3b1b15c48ed25
Alan Vezina <alanv@labkey.com>
2023-08-10 13:41:47 -0500

    testHelperMocks.ts: Don't call initQueryGridMocksh, make arg required

--- packages/components/src/test/testHelperMocks.ts
+++ packages/components/src/test/testHelperMocks.ts
@@ -1,15 +1,17 @@
 import mock, { proxy } from 'xhr-mock';
 
-import { initQueryGridMocks } from './mock';
-
 /**
  * Use this method in beforeAll() for your jest tests and you'll have full access
  * to all of the same mock API responses we use in storybook.
  */
-export function initUnitTestMocks(extraMocks?: Array<() => void>): void {
+export function initUnitTestMocks(mockInitializers: Array<() => void>): void {
+    if (mockInitializers === undefined || mockInitializers.length === 0) {
+        console.error(
+            'You must pass an array of mock initializers to initUnitTestMocks. If you do not have a relevant mockInitializer you do not need to use this method'
+        );
+    }
     window['__react-beautiful-dnd-disable-dev-warnings'] = true;
     mock.setup();
-    initQueryGridMocks();
-    extraMocks?.forEach(extraMock => extraMock());
+    mockInitializers?.forEach(extraMock => extraMock());
     mock.use(proxy);
 }

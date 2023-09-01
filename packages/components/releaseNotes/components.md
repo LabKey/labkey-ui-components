359fdd2976dee9c03a15dc357671766226cb2ec6
cnathe <cnathe@labkey.com>
2023-09-01 09:22:11 -0500

    Add SamplesTabbedGridPanelComponent to SampleTypeAppContext

--- packages/components/releaseNotes/components.md
+++ packages/components/releaseNotes/components.md
@@ -1,6 +1,10 @@
 # @labkey/components
 Components, models, actions, and utility functions for LabKey applications and pages.
 
+### version TBD
+*Released*: TBD
+- Add SamplesTabbedGridPanelComponent to SampleTypeAppContext
+
 ### version 2.364.0
 *Released*: 31 August 2023
 - Issue 47376: User Detail Modal - add className to be able to distinguish modal from others
--- packages/components/src/internal/sampleModels.ts
+++ packages/components/src/internal/sampleModels.ts
@@ -1,14 +1,45 @@
 import { List, Map, OrderedMap } from 'immutable';
 import { ComponentType } from 'react';
+import { Query } from '@labkey/api';
 
 import { QueryModel } from '../public/QueryModel/QueryModel';
 import { RequiresModelAndActions } from '../public/QueryModel/withQueryModels';
 import { SchemaQuery } from '../public/SchemaQuery';
 
+import { TabbedGridPanelProps } from '../public/QueryModel/TabbedGridPanel';
+
 import { ComponentsAPIWrapper } from './APIWrapper';
 import { User } from './components/base/models/User';
 import { EntityDataType } from './components/entities/models';
 import { SampleGridButtonProps } from './components/samples/models';
+import { ALIQUOT_FILTER_MODE } from './components/samples/constants';
+
+export interface SamplesTabbedGridPanelComponentProps {
+    afterSampleActionComplete?: (requiresModelReload?: boolean) => void;
+    asPanel?: boolean;
+    autoLoad?: boolean;
+    containerFilter?: Query.ContainerFilter;
+    createBtnParentKey?: string;
+    createBtnParentType?: string;
+    getIsDirty?: () => boolean;
+    gridButtonProps?: any;
+    gridButtons?: ComponentType<SampleGridButtonProps & RequiresModelAndActions>;
+    initialTabId?: string; // use if you have multiple tabs but want to start on something other than the first one
+    isAllSamplesTab?: (QuerySchema) => boolean;
+    modelId?: string; // if a usage wants to just show a single GridPanel, they should provide a modelId prop
+    onSampleTabSelect?: (modelId: string) => void;
+    sampleAliquotType?: ALIQUOT_FILTER_MODE; // the init sampleAliquotType, requires all query models to have completed loading queryInfo prior to rendering of the component
+    samplesEditableGridProps?: Partial<SamplesEditableGridProps>;
+    setIsDirty?: (isDirty: boolean) => void;
+    showLabelOption?: boolean;
+    tabRowCounts?: Record<string, number>;
+    tabbedGridPanelProps?: Partial<TabbedGridPanelProps>;
+    title?: string;
+    user: User;
+    withTitle?: boolean;
+}
+
+export type SamplesTabbedGridPanel = ComponentType<SamplesTabbedGridPanelComponentProps>;
 
 export interface SampleStorageLocationComponentProps {
     actionChangeCount?: number;

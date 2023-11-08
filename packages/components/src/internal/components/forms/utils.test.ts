72a2d74e7adb7b5f4a6e8b1feba43f1376ba38d3
Cory Nathe <cnathe@labkey.com>
2023-11-08 10:43:14 -0600

    Rename spec.ts files to test.ts (#1342)

--- packages/components/src/internal/components/forms/utils.test.ts
+++ packages/components/src/internal/components/forms/utils.test.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
 import { fromJS } from 'immutable';
 
 import { resolveDetailFieldValue } from './utils';

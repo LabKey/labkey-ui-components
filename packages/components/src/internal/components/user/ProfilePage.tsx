2a2c9cd8bef9f56433d59859d2399bd22160e84e
Alan Vezina <alanv@labkey.com>
2023-11-15 15:27:02 -0600

    ProfilePage: don't use withRouter

--- packages/components/src/internal/components/user/ProfilePage.tsx
+++ packages/components/src/internal/components/user/ProfilePage.tsx
@@ -4,7 +4,6 @@
  */
 import React, { FC, useCallback, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { WithRouterProps } from 'react-router';
 
 import { isLoginAutoRedirectEnabled } from '../administration/utils';
 import { InsufficientPermissionsPage } from '../permissions/InsufficientPermissionsPage';
@@ -31,15 +30,15 @@ import { ChangePasswordModal } from './ChangePasswordModal';
 
 import { useUserProperties } from './hooks';
 
-interface Props extends WithRouterProps {
+interface Props {
     updateUserDisplayName: (displayName: string) => void;
 }
 
 const TITLE = 'User Profile';
 
-const ProfilePageImpl: FC<Props> = props => {
-    const { router, routes, updateUserDisplayName } = props;
-    const [_, setIsDirty] = useRouteLeave(router, routes);
+export const ProfilePage: FC<Props> = props => {
+    const { updateUserDisplayName } = props;
+    const [_, setIsDirty] = useRouteLeave();
     const [showChangePassword, setShowChangePassword] = useState<boolean>(false);
     const { moduleContext, user } = useServerContext();
     const userProperties = useUserProperties(user);
@@ -108,5 +107,3 @@ const ProfilePageImpl: FC<Props> = props => {
         </Page>
     );
 };
-
-export const ProfilePage = ProfilePageImpl;

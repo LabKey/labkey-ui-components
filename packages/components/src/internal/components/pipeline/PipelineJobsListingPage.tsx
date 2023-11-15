d11f82430cb6bef064d345ad14a7018e4ffb1ccc
Alan Vezina <alanv@labkey.com>
2023-11-15 16:13:19 -0600

    PipelineJobsListingPage: Don't use RR types

--- packages/components/src/internal/components/pipeline/PipelineJobsListingPage.tsx
+++ packages/components/src/internal/components/pipeline/PipelineJobsListingPage.tsx
@@ -1,13 +1,11 @@
 import React, { FC } from 'react';
 
-import { WithRouterProps } from 'react-router';
-
 import { biologicsIsPrimaryApp, sampleManagerIsPrimaryApp } from '../../app/utils';
 
 import { PipelineJobsPage } from './PipelineJobsPage';
 import { PIPELINE_PROVIDER_FILTER_LKB, PIPELINE_PROVIDER_FILTER_LKSM } from './constants';
 
-export const PipelineJobsListingPage: FC<WithRouterProps> = props => {
+export const PipelineJobsListingPage: FC = props => {
     const baseFilters = sampleManagerIsPrimaryApp()
         ? [PIPELINE_PROVIDER_FILTER_LKSM]
         : biologicsIsPrimaryApp()

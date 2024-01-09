01c00762108d7f7504c495d15e66c4b1ec0f1110
alanv <alanv@labkey.com>
2024-01-09 10:35:26 -0600

    Popover: fix compiler error

--- packages/components/src/internal/Popover.tsx
+++ packages/components/src/internal/Popover.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, LegacyRef } from 'react';
 
 import { useOverlayPositioning } from './useOverlayPositioning';
 import { TooltipProps } from './Tooltip';
@@ -14,9 +14,10 @@ interface PopoverProps extends TooltipProps {
  */
 export const Popover: FC<PopoverProps> = ({ children, targetRef, id, placement, title }) => {
     const { overlayRef, style } = useOverlayPositioning(placement, targetRef);
+    const className = `lk-popover popover ${placement}`;
 
     return (
-        <div id={id} className={`lk-popover popover ${placement}`} style={style} ref={overlayRef}>
+        <div id={id} className={className} style={style} ref={overlayRef as LegacyRef<HTMLDivElement>}>
             <div className="arrow" />
             {title && <h3 className="popover-title">{title}</h3>}
             <div className="popover-content">{children}</div>

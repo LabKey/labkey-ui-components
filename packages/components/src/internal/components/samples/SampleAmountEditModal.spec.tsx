75d72c2a3503ed4c7eaec744130bd950857a31dd
labkey-susanh <susanh@labkey.com>
2024-01-23 14:14:26 -0800

    Update test

--- packages/components/src/internal/components/samples/SampleAmountEditModal.spec.tsx
+++ packages/components/src/internal/components/samples/SampleAmountEditModal.spec.tsx
@@ -4,11 +4,13 @@ import { ReactWrapper } from 'enzyme';
 import { SchemaQuery } from '../../../public/SchemaQuery';
 
 import { Alert } from '../base/Alert';
-import { mountWithAppServerContext } from '../../test/enzymeTestHelpers';
+import { mountWithAppServerContext, waitForLifecycle } from '../../test/enzymeTestHelpers';
 import { TEST_USER_EDITOR } from '../../userFixtures';
 
 import { SampleAmountEditModal } from './SampleAmountEditModal';
 import { act } from 'react-dom/test-utils';
+import { getTestAPIWrapper } from '../../APIWrapper';
+import { getFolderTestAPIWrapper } from '../container/FolderAPIWrapper';
 
 describe('SampleAmountEditModal', () => {
     const testSchemaQuery = new SchemaQuery('schema', 'query', 'view');
@@ -164,73 +166,7 @@ describe('SampleAmountEditModal', () => {
         wrapper.unmount();
     });
 
-    test("Set only comment, can't save", () => {
-        const row = {
-            Name: { value: 'abcd' },
-            StoredAmount: { value: '500' },
-            Units: { value: 'uL' },
-            FreezeThawCount: { value: 1 },
-        } as any;
-
-        const wrapper = mountWithAppServerContext(
-            <SampleAmountEditModal
-                schemaQuery={testSchemaQuery}
-                row={row}
-                noun={noun}
-                updateListener={jest.fn()}
-                onClose={jest.fn()}
-            />,
-            undefined,
-            { user: TEST_USER_EDITOR }
-        );
-
-        validate(wrapper, row.StoredAmount.value, row.Units.value, true, undefined, noun, false);
-
-        const userComment = 'Additional text for the audit log';
-        act(() => {
-            wrapper.find('#actionComments').simulate('change', { target: { name: 'body', value: userComment } });
-        });
-        validate(wrapper, row.StoredAmount.value, row.Units.value, true, userComment, noun, false);
-
-        wrapper.unmount();
-    });
-
-    test('Set comment and StoredAmount, can save', () => {
-        const row = {
-            Name: { value: 'abcd' },
-            StoredAmount: { value: '500' },
-            Units: { value: 'uL' },
-            FreezeThawCount: { value: 1 },
-        } as any;
-
-        const wrapper = mountWithAppServerContext(
-            <SampleAmountEditModal
-                schemaQuery={testSchemaQuery}
-                row={row}
-                noun={noun}
-                updateListener={jest.fn()}
-                onClose={jest.fn()}
-            />,
-            undefined,
-            { user: TEST_USER_EDITOR }
-        );
-
-        validate(wrapper, row.StoredAmount.value, row.Units.value, true, undefined, noun, false);
-
-        const userComment = 'Additional text for the audit log';
-        const newStoredAmount = 5;
-        act(() => {
-            wrapper.find('#actionComments').simulate('change', { target: { name: 'body', value: userComment } });
-            wrapper
-                .find('input.storage-amount-input')
-                .simulate('change', { target: { name: 'amountDelta', value: newStoredAmount } });
-        });
-        validate(wrapper, newStoredAmount, row.Units.value, true, userComment, noun, true);
-
-        wrapper.unmount();
-    });
-
-    test('Set units, can save', () => {
+    test('Set units, can save', async () => {
         const row = {
             Name: { value: 'abcd' },
             StoredAmount: { value: '500' },
@@ -246,9 +182,16 @@ describe('SampleAmountEditModal', () => {
                 updateListener={jest.fn()}
                 onClose={jest.fn()}
             />,
-            undefined,
+            {
+                api: getTestAPIWrapper(jest.fn, {
+                    folder: getFolderTestAPIWrapper(jest.fn, {
+                        getAuditSettings: jest.fn().mockResolvedValue({ requireUserComments: false }),
+                    }),
+                }),
+            },
             { user: TEST_USER_EDITOR }
         );
+        await waitForLifecycle(wrapper);
 
         validate(wrapper, row.StoredAmount.value, row.Units.value, false, undefined, noun, false);
 
@@ -256,6 +199,7 @@ describe('SampleAmountEditModal', () => {
         act(() => {
             wrapper.find('input.checkin-unit-input').simulate('change', { target: { value: newUnits } });
         });
+        await waitForLifecycle(wrapper);
         validate(wrapper, row.StoredAmount.value, newUnits, false, undefined, noun, true, false, false);
 
         wrapper.unmount();

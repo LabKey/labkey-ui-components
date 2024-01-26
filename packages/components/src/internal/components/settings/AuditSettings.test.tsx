24eba5bd853a225359db7196d4662d5e194ce7e7
labkey-susanh <susanh@labkey.com>
2024-01-25 16:49:34 -0800

    package and test updates

--- packages/components/src/internal/components/settings/AuditSettings.test.tsx
+++ packages/components/src/internal/components/settings/AuditSettings.test.tsx
@@ -7,7 +7,7 @@ import { getFolderTestAPIWrapper } from '../container/FolderAPIWrapper';
 import { AuditSettings } from './AuditSettings';
 import { renderWithAppContext } from '../../test/reactTestLibraryHelpers';
 import { TEST_LKSM_PROFESSIONAL_MODULE_CONTEXT, TEST_LKSM_STARTER_MODULE_CONTEXT } from '../../productFixtures';
-import { TEST_USER_APP_ADMIN } from '../../userFixtures';
+import { TEST_USER_APP_ADMIN, TEST_USER_EDITOR } from '../../userFixtures';
 
 describe('AuditSettings', () => {
     const APP_CONTEXT: Partial<AppContext> = {
@@ -54,5 +54,26 @@ describe('AuditSettings', () => {
         const radioInputs = document.querySelectorAll('input[name="requireComments"]');
         expect(radioInputs.item(0).getAttribute('checked')).toBe('');
         expect(radioInputs.item(1).getAttribute('checked')).toBe(null);
+    });
+
+    test('feature is enabled, user not admin', async () => {
+        await act(async () => {
+            const { container } = renderWithAppContext(<AuditSettings />, {
+                serverContext: {
+                    user: TEST_USER_EDITOR,
+                    project: TEST_PROJECT,
+                    container: TEST_FOLDER_CONTAINER,
+                    moduleContext: TEST_LKSM_PROFESSIONAL_MODULE_CONTEXT,
+                },
+                appContext: {
+                    api: getTestAPIWrapper(jest.fn, {
+                        folder: getFolderTestAPIWrapper(jest.fn, {
+                            getAuditSettings: jest.fn().mockResolvedValue({ requireUserComments: false }),
+                        }),
+                    }),
+                },
+            });
+            expect(container.firstChild).toBeNull();
+        });
     });
 });

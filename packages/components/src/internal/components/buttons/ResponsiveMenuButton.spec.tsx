bd562277a798ca9340583ce6cabf928560a65161
alanv <alanv@labkey.com>
2024-02-06 10:40:25 -0600

    Remove usages of DropdownButton and MenuItem from tests

--- packages/components/src/internal/components/buttons/ResponsiveMenuButton.spec.tsx
+++ packages/components/src/internal/components/buttons/ResponsiveMenuButton.spec.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { DropdownButton } from 'react-bootstrap';
 
 import { PicklistButton } from '../picklist/PicklistButton';
 import { TEST_USER_READER } from '../../userFixtures';
@@ -8,8 +7,6 @@ import { makeTestQueryModel } from '../../../public/QueryModel/testUtils';
 import { SchemaQuery } from '../../../public/SchemaQuery';
 
 import { mountWithServerContext } from '../../test/enzymeTestHelpers';
-
-import { SubMenuItem } from '../menus/SubMenuItem';
 
 import { ResponsiveMenuButton } from './ResponsiveMenuButton';
 
@@ -25,8 +22,8 @@ describe('ResponsiveMenuButton', () => {
         const wrapper = mountWithServerContext(<ResponsiveMenuButton {...DEFAULT_PROPS} />, {
             user: TEST_USER_READER,
         });
-        expect(wrapper.find(DropdownButton)).toHaveLength(1);
-        expect(wrapper.find(SubMenuItem)).toHaveLength(0);
+        expect(wrapper.find('DropdownButton')).toHaveLength(1);
+        expect(wrapper.find('MenuItem')).toHaveLength(0);
         expect(wrapper.find(PicklistButton)).toHaveLength(1);
         wrapper.unmount();
     });
@@ -35,8 +32,8 @@ describe('ResponsiveMenuButton', () => {
         const wrapper = mountWithServerContext(<ResponsiveMenuButton {...DEFAULT_PROPS} asSubMenu />, {
             user: TEST_USER_READER,
         });
-        expect(wrapper.find(DropdownButton)).toHaveLength(0);
-        expect(wrapper.find(SubMenuItem)).toHaveLength(1);
+        expect(wrapper.find('DropdownButton')).toHaveLength(0);
+        expect(wrapper.find('MenuItem')).toHaveLength(1);
         expect(wrapper.find(PicklistButton)).toHaveLength(1);
         wrapper.unmount();
     });
--- packages/components/src/internal/components/picklist/PicklistButton.spec.tsx
+++ packages/components/src/internal/components/picklist/PicklistButton.spec.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { DropdownButton } from 'react-bootstrap';
 
 import { makeTestQueryModel } from '../../../public/QueryModel/testUtils';
 import { SchemaQuery } from '../../../public/SchemaQuery';
 import { TEST_USER_EDITOR } from '../../userFixtures';
 
 import { mountWithServerContext } from '../../test/enzymeTestHelpers';
-
-import { SubMenuItem } from '../menus/SubMenuItem';
 
 import { PicklistButton } from './PicklistButton';
 import { PicklistCreationMenuItem } from './PicklistCreationMenuItem';
@@ -21,8 +18,8 @@ describe('PicklistButton', () => {
             <PicklistButton model={queryModel} user={TEST_USER_EDITOR} metricFeatureArea={featureArea} />,
             { user: TEST_USER_EDITOR }
         );
-        expect(wrapper.find(DropdownButton)).toHaveLength(1);
-        expect(wrapper.find(SubMenuItem)).toHaveLength(0);
+        expect(wrapper.find('DropdownButton')).toHaveLength(1);
+        expect(wrapper.find('MenuItem')).toHaveLength(0);
         const menuItem = wrapper.find(PicklistCreationMenuItem);
         expect(menuItem).toHaveLength(1);
         expect(menuItem.prop('metricFeatureArea')).toBe(featureArea);
@@ -38,8 +35,8 @@ describe('PicklistButton', () => {
             <PicklistButton model={queryModel} user={TEST_USER_EDITOR} metricFeatureArea={featureArea} asSubMenu />,
             { user: TEST_USER_EDITOR }
         );
-        expect(wrapper.find(DropdownButton)).toHaveLength(0);
-        expect(wrapper.find(SubMenuItem)).toHaveLength(1);
+        expect(wrapper.find('DropdownButton')).toHaveLength(0);
+        expect(wrapper.find('MenuItem')).toHaveLength(1);
     });
 
     test('with model and selections', () => {

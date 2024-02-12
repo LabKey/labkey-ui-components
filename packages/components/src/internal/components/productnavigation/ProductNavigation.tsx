f9e0d0ed9ec3997476de20a62ffc7ed96c15abbd
alanv <alanv@labkey.com>
2024-02-12 16:33:31 -0600

    ProductNavigation: Don't use DropdownButton

--- packages/components/src/internal/components/productnavigation/ProductNavigation.tsx
+++ packages/components/src/internal/components/productnavigation/ProductNavigation.tsx
@@ -1,30 +1,21 @@
-import React, { FC, memo, useCallback, useState } from 'react';
-import { DropdownButton } from 'react-bootstrap';
+import React, { FC, memo, useCallback } from 'react';
 
 import { ProductNavigationMenu } from './ProductNavigationMenu';
+import { useNavMenuState } from '../../useNavMenuState';
 
 export const ProductNavigation: FC = memo(() => {
-    const [show, setShow] = useState<boolean>(false);
+    const { show, setShow, menuRef, toggleRef } = useNavMenuState();
     const onCloseMenu = useCallback(() => setShow(false), []);
-    const toggleMenu = useCallback(() => {
-        setShow(current => !current);
-    }, []);
+    const toggleMenu = useCallback(() => setShow(s => !s), []);
+    const buttonClassName = 'btn btn-default navbar-icon-button-right navbar-menu-button';
 
-    // TODO: This should not be a DropdownButton, it is not rendering any MenuItems. We should consider Popover or
-    //  something similar.
     return (
-        <div className="navbar-item pull-right product-navigation-menu navbar-item__dropdown hidden-xs">
-            <DropdownButton
-                id="product-navigation-button"
-                className="navbar-icon-button-right"
-                title={<i className="fa fa-th-large navbar-header-icon" />}
-                onToggle={toggleMenu}
-                open={show}
-                noCaret
-                pullRight
-            >
-                {show && <ProductNavigationMenu onCloseMenu={onCloseMenu} />}
-            </DropdownButton>
+        <div className="navbar-item pull-right product-navigation-menu hidden-xs navbar-menu">
+            <button type="button" className={buttonClassName} onClick={toggleMenu} ref={toggleRef}>
+                <span className="fa fa-th-large navbar-header-icon" />
+            </button>
+
+            {show && <ProductNavigationMenu onCloseMenu={onCloseMenu} menuRef={menuRef} />}
         </div>
     );
 });

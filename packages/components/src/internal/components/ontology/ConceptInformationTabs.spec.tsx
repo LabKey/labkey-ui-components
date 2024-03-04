02ed372b711594752f0aba462c439b52158e8997
alanv <alanv@labkey.com>
2024-03-04 17:30:34 -0600

    ConceptInformationTabs: Fix tests

--- packages/components/src/internal/components/ontology/ConceptInformationTabs.spec.tsx
+++ packages/components/src/internal/components/ontology/ConceptInformationTabs.spec.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavItem, Tab } from 'react-bootstrap';
 import { mount, ReactWrapper } from 'enzyme';
 
 import { ConceptInformationTabs, ConceptInfoTabs } from './ConceptInformationTabs';
@@ -13,8 +12,7 @@ const DEFAULT_PROPS = {
 
 describe('ConceptInformationTabs', () => {
     function validate(wrapper: ReactWrapper) {
-        expect(wrapper.find(Tab.Container)).toHaveLength(1);
-        expect(wrapper.find(NavItem)).toHaveLength(2);
+        expect(wrapper.find('li[role="presentation"]')).toHaveLength(2);
         expect(wrapper.find('.ontology-concept-overview-container')).toHaveLength(2);
         expect(wrapper.find(ConceptOverviewPanelImpl)).toHaveLength(1);
         expect(wrapper.find(ConceptPathInfo)).toHaveLength(1);
@@ -34,14 +32,6 @@ describe('ConceptInformationTabs', () => {
         );
         validate(wrapper);
         expect(wrapper.find(ConceptOverviewPanelImpl).prop('concept')).toBe(concept);
-        wrapper.unmount();
-    });
-
-    test('activeTab and defaultActiveKey', () => {
-        const wrapper = mount(<ConceptInformationTabs {...DEFAULT_PROPS} alternatePathClickHandler={jest.fn} />);
-        validate(wrapper);
-        expect(wrapper.find(Tab.Container).prop('defaultActiveKey')).toBe(ConceptInfoTabs.CONCEPT_OVERVIEW_TAB);
-        expect(wrapper.find(Tab.Container).prop('activeKey')).toBe(ConceptInfoTabs.CONCEPT_OVERVIEW_TAB);
         wrapper.unmount();
     });
 });

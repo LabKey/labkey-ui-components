c74bcfed6779419b7ca92725a92236251fde2fad
RosalineP <rosalinep@labkey.com>
2024-06-28 16:29:14 -0700

    AbDisc: Worklist & Instrument Instructions, Improvements  (#1524)

--- packages/components/src/internal/DropdownSection.tsx
+++ packages/components/src/internal/DropdownSection.tsx
@@ -1,14 +1,17 @@
-import React, { FC, useCallback, useMemo, useState } from 'react';
+import React, { FC, ReactNode, useCallback, useMemo, useState } from 'react';
+
+import classNames from 'classnames';
+
+import { Utils } from '@labkey/api';
 
 import { MenuDivider, MenuItem, preventDocumentHandler } from './dropdowns';
 import { DisableableMenuItem, DisableableMenuItemProps } from './components/samples/DisableableMenuItem';
-import classNames from 'classnames';
 
 const SHOW_FILTER_CUTOFF = 10;
 
 export interface MenuSectionItem extends Omit<DisableableMenuItemProps, 'children'> {
     disabledMessage?: string;
-    text: string;
+    label: ReactNode;
 }
 
 interface MenuSectionProps {
@@ -40,7 +43,9 @@ export const DropdownSection: FC<MenuSectionProps> = ({ items, showDivider = fal
     const menuItems = useMemo(() => {
         if (filterValue.trim()) {
             const filterValueLC = filterValue.toLowerCase();
-            return items.filter(item => item.text.toLowerCase().indexOf(filterValueLC) > -1)
+            return items.filter(
+                item => Utils.isString(item.label) && item.label.toLowerCase().indexOf(filterValueLC) > -1
+            );
         }
         return items;
     }, [filterValue, items]);
@@ -67,9 +72,13 @@ export const DropdownSection: FC<MenuSectionProps> = ({ items, showDivider = fal
             )}
 
             {expanded &&
-                menuItems.map(({ text: menuItemText, ...props }) => (
-                    <DisableableMenuItem key={menuItemText} {...props} className={classNames('dropdown-section__menu-item', props.className)}>
-                        {menuItemText}
+                menuItems.map(({ label: menuItemLabel, ...props }, index) => (
+                    <DisableableMenuItem
+                        key={index}
+                        {...props}
+                        className={classNames('dropdown-section__menu-item', props.className)}
+                    >
+                        {menuItemLabel}
                     </DisableableMenuItem>
                 ))}
 

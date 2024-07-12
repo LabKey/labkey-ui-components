09d38ddb64fc50a3e928308928ec245af13c9b0b
labkey-susanh <susanh@labkey.com>
2024-07-12 06:25:39 -0700

    Update message for consistency

--- packages/components/src/internal/components/picklist/constants.ts
+++ packages/components/src/internal/components/picklist/constants.ts
@@ -2,4 +2,4 @@ export const PRIVATE_PICKLIST_CATEGORY = 'PrivatePicklist';
 export const PUBLIC_PICKLIST_CATEGORY = 'PublicPicklist';
 export const MAX_SELECTIONS_PER_ADD = 1000;
 export const MAX_SELECTIONS_MESSAGE =
-    'At most ' + MAX_SELECTIONS_PER_ADD.toLocaleString() + ' samples can be selected at once';
+    'At most ' + MAX_SELECTIONS_PER_ADD.toLocaleString() + ' samples can be selected.';

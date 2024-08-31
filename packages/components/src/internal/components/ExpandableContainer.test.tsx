31df864ccdd6332a95b8d383658514baf61d4de5
alanv <alanv@labkey.com>
2024-08-31 00:09:32 -0500

    Fix userEvent import

--- packages/components/src/internal/components/ExpandableContainer.test.tsx
+++ packages/components/src/internal/components/ExpandableContainer.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import userEvent from "@testing-library/user-event";
+import { userEvent } from "@testing-library/user-event";
 
 import { ExpandableContainer } from './ExpandableContainer';
 

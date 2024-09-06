64f23ab325357c810b07ad7aba5a13a1b9faa1cf
alanv <alanv@labkey.com>
2024-09-06 00:22:28 -0500

    Add process.env.NODE_ENV to main types

--- packages/components/src/typings/main.d.ts
+++ packages/components/src/typings/main.d.ts
@@ -17,3 +17,13 @@
  * @deprecated Use getServerContext() from @labkey/api instead
  */
 declare const LABKEY: import('@labkey/api').LabKey;
+
+/**
+ * Needed so we can use process.env.NODE_ENV, which is injected by webpack, but not included in the types declared in
+ * the browser environments.
+ */
+declare const process: {
+    env: {
+        NODE_ENV: string;
+    };
+};

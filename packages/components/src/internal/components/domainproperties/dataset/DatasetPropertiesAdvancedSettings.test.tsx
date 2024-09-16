ab7bdb2945f2ff362cc0036ceb25b866f0cc1b2b
cnathe <cnathe@labkey.com>
2024-09-16 09:19:04 -0500

    replace snapshot tests in DatasetPropertiesAdvancedSettings.test.tsx

--- packages/components/src/internal/components/domainproperties/dataset/DatasetPropertiesAdvancedSettings.test.tsx
+++ packages/components/src/internal/components/domainproperties/dataset/DatasetPropertiesAdvancedSettings.test.tsx
@@ -1,59 +1,28 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import getDatasetDesign from '../../../../test/data/dataset-getDatasetDesign.json';
-import getDatasetDesignSharedStudy from '../../../../test/data/dataset-getDatasetDesignSharedStudy.json';
+import { waitFor } from '@testing-library/dom';
+
 import { NEW_DATASET_MODEL_WITHOUT_DATASPACE } from '../../../../test/data/constants';
 
 import { AdvancedSettings, DatasetSettingsInput, DatasetSettingsSelect } from './DatasetPropertiesAdvancedSettings';
 import { DatasetModel } from './models';
 
 const newDatasetModel = DatasetModel.create(NEW_DATASET_MODEL_WITHOUT_DATASPACE, undefined);
-const datasetModel = DatasetModel.create(null, getDatasetDesign);
-const sharedDatasetModel = DatasetModel.create(null, getDatasetDesignSharedStudy);
 
 describe('Dataset Advanced Settings', () => {
-    test('New Dataset, without dataspace options', () => {
-        const component = (
+    test('button render', async () => {
+        render(
             <AdvancedSettings title="Advanced Settings" model={newDatasetModel} applyAdvancedProperties={jest.fn()} />
         );
 
-        const { container } = render(component);
-        expect(container).toMatchSnapshot();
+        await waitFor(() => {
+            expect(document.querySelectorAll('button').length).toBe(1);
+            expect(document.querySelector('button').textContent).toBe('Advanced Settings');
+        });
     });
 
-    test('New Dataset, with dataspace options', () => {
-        const component = (
-            <AdvancedSettings title="Advanced Settings" model={newDatasetModel} applyAdvancedProperties={jest.fn()} />
-        );
-
-        const { container } = render(component);
-        expect(container).toMatchSnapshot();
-    });
-
-    test('Edit Dataset, without dataspace options', () => {
-        const component = (
-            <AdvancedSettings title="Advanced Settings" model={datasetModel} applyAdvancedProperties={jest.fn()} />
-        );
-
-        const { container } = render(component);
-        expect(container).toMatchSnapshot();
-    });
-
-    test('Edit Dataset, with dataspace options', () => {
-        const component = (
-            <AdvancedSettings
-                title="Advanced Settings"
-                model={sharedDatasetModel}
-                applyAdvancedProperties={jest.fn()}
-            />
-        );
-
-        const { container } = render(component);
-        expect(container).toMatchSnapshot();
-    });
-
-    test('DatasetSettingsInput', () => {
+    test('DatasetSettingsInput', async () => {
         render(
             <DatasetSettingsInput
                 required={true}
@@ -68,11 +37,15 @@ describe('Dataset Advanced Settings', () => {
             />
         );
 
-        expect(document.querySelector('.domain-no-wrap').textContent).toEqual('Name *');
-        expect(document.querySelector('input').getAttribute('placeholder')).toEqual('Enter a name for this dataset');
+        await waitFor(() => {
+            expect(document.querySelector('.domain-no-wrap').textContent).toEqual('Name *');
+            expect(document.querySelector('input').getAttribute('placeholder')).toEqual(
+                'Enter a name for this dataset'
+            );
+        });
     });
 
-    test('DatasetSettingsSelect', () => {
+    test('DatasetSettingsSelect', async () => {
         render(
             <DatasetSettingsSelect
                 name="visitDateColumn"
@@ -84,7 +57,9 @@ describe('Dataset Advanced Settings', () => {
             />
         );
 
-        expect(document.querySelectorAll('input')).toHaveLength(2);
-        expect(document.querySelectorAll('input')[1].getAttribute('name')).toEqual('visitDateColumn');
+        await waitFor(() => {
+            expect(document.querySelectorAll('input')).toHaveLength(2);
+            expect(document.querySelectorAll('input')[1].getAttribute('name')).toEqual('visitDateColumn');
+        });
     });
 });

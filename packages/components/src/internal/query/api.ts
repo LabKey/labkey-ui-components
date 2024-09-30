60153a4c847419e559629438eabb438e3abbe221
cnathe <cnathe@labkey.com>
2024-09-30 08:51:30 -0500

    "Mock" response for getQueryDetails and selectRows for env.NODE_ENV "test"

--- packages/components/src/internal/query/api.ts
+++ packages/components/src/internal/query/api.ts
@@ -111,6 +111,10 @@ interface GetQueryDetailsSQ extends GetQueryDetailsBasic {
 export type GetQueryDetailsOptions = GetQueryDetailsName | GetQueryDetailsSQ;
 
 export function getQueryDetails(options: GetQueryDetailsOptions): Promise<QueryInfo> {
+    if (process.env.NODE_ENV === 'test') {
+        return Promise.resolve(new QueryInfo({}));
+    }
+
     const { containerPath, fields, fk } = options;
     const schemaQuery = options.schemaQuery ?? new SchemaQuery(options.schemaName, options.queryName);
     const key = getQueryDetailsCacheKey(schemaQuery, containerPath, fk, fields);
@@ -455,7 +459,7 @@ export interface ISelectRowsResult {
     key: string;
     messages?: List<Map<string, string>>;
     models: any;
-    orderedModels: List<any>;
+    orderedModels: any; //List<any>;
     queries: {
         [key: string]: QueryInfo;
     };
@@ -469,6 +473,17 @@ export interface ISelectRowsResult {
  */
 export function selectRowsDeprecated(userConfig, caller?): Promise<ISelectRowsResult> {
     return new Promise((resolve, reject) => {
+        if (process.env.NODE_ENV === 'test') {
+            resolve({
+                key: 'test',
+                models: { test: {} },
+                orderedModels: { test: List() },
+                queries: { test: new QueryInfo({}) },
+                rowCount: 0,
+            });
+            return;
+        }
+
         let schemaQuery, key;
         if (userConfig.queryName) {
             schemaQuery = new SchemaQuery(userConfig.schemaName, userConfig.queryName, userConfig.viewName);

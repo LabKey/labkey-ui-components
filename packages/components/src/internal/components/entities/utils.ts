e08fe6989fd1c2f46390a805ae4fe917e80d3a40
cnathe <cnathe@labkey.com>
2025-01-31 14:51:50 -0600

    npm run lint-branch-fix

--- packages/components/src/internal/components/entities/utils.ts
+++ packages/components/src/internal/components/entities/utils.ts
@@ -18,11 +18,11 @@ import { ViewInfo } from '../../ViewInfo';
 
 import { QueryColumn } from '../../../public/QueryColumn';
 
+import { SELECTION_KEY_TYPE } from '../samples/constants';
+
 import { EntityChoice, EntityDataType, IEntityTypeOption } from './models';
 
 import { ParentIdData } from './actions';
-import { SELECTION_KEY_TYPE } from '../samples/constants';
-import { QueryColumn } from '../../../public/QueryColumn';
 
 export function sampleDeleteDependencyText(): string {
     let deleteMsg = '';
@@ -176,7 +176,7 @@ export function getIdentifyingColumns(queryInfo: QueryInfo): QueryColumn[] {
     if (!idView) {
         return [];
     }
-    return queryInfo.getIdentifyingFieldsEditableGridColumns(true)
+    return queryInfo.getIdentifyingFieldsEditableGridColumns(true);
 }
 
 export const SAMPLE_ID_FIELD_KEY = 'sampleid';

cda396ceb35a7c8434443d1d760b83b9cc09fd02
alanv <alanv@labkey.com>
2025-02-24 16:32:10 -0600

    SelectionStatus: Convert to FC

--- packages/components/src/public/QueryModel/SelectionStatus.tsx
+++ packages/components/src/public/QueryModel/SelectionStatus.tsx
@@ -1,77 +1,74 @@
-import React, { PureComponent, ReactNode } from 'react';
+import React, { FC, memo, useCallback, useMemo } from 'react';
 
 import { LoadingSpinner } from '../../internal/components/base/LoadingSpinner';
 
 import { RequiresModelAndActions } from './withQueryModels';
 
-export class SelectionStatus extends PureComponent<RequiresModelAndActions> {
-    clearSelections = (): void => {
-        this.props.actions.clearSelections(this.props.model.id);
-    };
+export const SelectionStatus: FC<RequiresModelAndActions> = memo(({ actions, model }) => {
+    const { isLoading, isLoadingSelections, isLoadingTotalCount, maxRows, rowCount, selections } = model;
+    const selectionSize = selections?.size;
 
-    selectAll = (): void => {
-        this.props.actions.selectAllRows(this.props.model.id);
-    };
+    const clearSelections = useCallback((): void => {
+        actions.clearSelections(model.id);
+    }, [actions, model.id]);
 
-    clearText = (): string => {
-        const { selections } = this.props.model;
-        let clearText = 'Clear';
+    const selectAll = useCallback((): void => {
+        actions.selectAllRows(model.id);
+    }, [actions, model.id]);
 
-        if (selections.size === 2) {
-            clearText = clearText + ' both';
-        } else if (selections.size > 2) {
-            clearText = clearText + ' all';
+    const clearText = useMemo((): string => {
+        let text = 'Clear';
+
+        if (selectionSize === 2) {
+            text = text + ' both';
+        } else if (selectionSize > 2) {
+            text = text + ' all';
         }
 
-        return clearText;
-    };
+        return text;
+    }, [selectionSize]);
 
-    render(): ReactNode {
-        const { model } = this.props;
-        const { isLoading, isLoadingSelections, isLoadingTotalCount, maxRows, rowCount, selections } = model;
+    if (isLoading || isLoadingSelections || !selections) {
+        return null;
+    }
 
-        if (isLoading || isLoadingSelections || !selections) {
-            return null;
-        }
+    let selectionCount;
+    let clearAllButton;
+    let selectAllButton;
 
-        const selectionSize = selections.size;
-        let selectionCount;
-        let clearAllButton;
-        let selectAllButton;
+    if (selectionSize > 0) {
+        selectionCount = (
+            <span className="selection-status__count">
+                {selectionSize.toLocaleString()} of{' '}
+                {isLoadingTotalCount ? <LoadingSpinner msg="" /> : rowCount.toLocaleString()} selected
+            </span>
+        );
 
-        if (selectionSize > 0) {
-            selectionCount = (
-                <span className="selection-status__count">
-                    {selectionSize.toLocaleString()} of{' '}
-                    {isLoadingTotalCount ? <LoadingSpinner msg="" /> : rowCount.toLocaleString()} selected
-                </span>
-            );
-
-            clearAllButton = (
-                <span className="selection-status__clear-all">
-                    <button className="btn btn-default btn-xs" onClick={this.clearSelections} type="button">
-                        {this.clearText()}
-                    </button>
-                </span>
-            );
-        }
-
-        if (rowCount > maxRows && selectionSize !== rowCount && rowCount > 0) {
-            selectAllButton = (
-                <span className="selection-status__select-all">
-                    <button className="btn btn-default btn-xs" onClick={this.selectAll} type="button">
-                        Select all {!isLoadingTotalCount ? rowCount.toLocaleString() : ''}
-                    </button>
-                </span>
-            );
-        }
-
-        return (
-            <div className="selection-status">
-                {selectionCount}
-                {selectAllButton}
-                {clearAllButton}
-            </div>
+        clearAllButton = (
+            <span className="selection-status__clear-all">
+                <button className="btn btn-default btn-xs" onClick={clearSelections} type="button">
+                    {clearText}
+                </button>
+            </span>
         );
     }
-}
+
+    if (rowCount > maxRows && selectionSize !== rowCount && rowCount > 0) {
+        selectAllButton = (
+            <span className="selection-status__select-all">
+                <button className="btn btn-default btn-xs" onClick={selectAll} type="button">
+                    Select all {!isLoadingTotalCount ? rowCount.toLocaleString() : ''}
+                </button>
+            </span>
+        );
+    }
+
+    return (
+        <div className="selection-status">
+            {selectionCount}
+            {selectAllButton}
+            {clearAllButton}
+        </div>
+    );
+});
+SelectionStatus.displayName = 'SelectionStatus';

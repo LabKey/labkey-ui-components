ece8e8a0c6dfd2e5754cc503e7e3b42081090e1f
alanv <alanv@labkey.com>
2025-05-19 15:53:19 -0500

    FindAndSearchDropdown.test.tsx - Convert from spec file

---
+++ packages/components/src/internal/components/search/FindAndSearchDropdown.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { FindAndSearchDropdown } from './FindAndSearchDropdown';
+
+describe('FindAndSearchDropdown', () => {
+    test('search but no find', () => {
+        render(<FindAndSearchDropdown title="Test title" onSearch={jest.fn} />);
+        expect(document.querySelector('.dropdown-toggle')).toHaveTextContent('Test title');
+        const items = document.querySelectorAll('.lk-menu-item');
+        expect(items).toHaveLength(2);
+        expect(items[0]).toHaveTextContent('Sample Finder');
+        expect(items[1]).toHaveTextContent('Search');
+        expect(document.querySelector('.modal')).not.toBeInTheDocument();
+    });
+
+    test('find but no search', () => {
+        render(<FindAndSearchDropdown title="Test title" findNounPlural="tests" onFindByIds={jest.fn} />);
+        const items = document.querySelectorAll('.lk-menu-item');
+        expect(items).toHaveLength(3);
+        expect(items[0]).toHaveTextContent('Find Tests by Barcode');
+        expect(items[1]).toHaveTextContent('Find Tests by ID');
+        expect(items[2]).toHaveTextContent('Sample Finder');
+    });
+});

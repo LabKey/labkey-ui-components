3d889234215cc2a7d52947fbe19fffbd4ba2a646
alanv <alanv@labkey.com>
2025-06-16 14:22:44 -0500

    sampleModels: remove unused imports

--- packages/components/src/internal/sampleModels.ts
+++ packages/components/src/internal/sampleModels.ts
@@ -1,18 +1,11 @@
-import { ComponentType } from 'react';
-import { Query } from '@labkey/api';
-
 import { SchemaQuery } from '../public/SchemaQuery';
 
-import { TabbedGridPanelProps } from '../public/QueryModel/TabbedGridPanel';
-
-import { User } from './components/base/models/User';
 import { EntityDataType } from './components/entities/models';
-import { ALIQUOT_FILTER_MODE } from './components/samples/constants';
 
 // This interface stores app-wide settings passed to the LineageEditableGrid
 export interface LineageEditableGridProps {
-    parentDataTypes: EntityDataType[];
     omitParentAliases?: (schemaQuery: SchemaQuery) => boolean;
+    parentDataTypes: EntityDataType[];
 }
 
 // This interface stores app-wide settings that get passed to our SamplesEditableGrid. It extends

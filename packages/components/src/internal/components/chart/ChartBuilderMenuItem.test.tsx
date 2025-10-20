75d7885714efbb1047c4a30e07e80632a66e3abc
alanv <alanv@labkey.com>
2025-10-20 15:37:21 -0500

    Fix ChartBuilderMenuItem tests

--- packages/components/src/internal/components/chart/ChartBuilderMenuItem.test.tsx
+++ packages/components/src/internal/components/chart/ChartBuilderMenuItem.test.tsx
@@ -46,11 +46,20 @@ describe('ChartBuilderMenuItem', () => {
     };
 
     test('default props', async () => {
-        renderWithAppContext(<ChartBuilderMenuItem actions={actions} model={model} />, {
-            serverContext: {
-                user: TEST_USER_EDITOR,
-            },
-        });
+        renderWithAppContext(
+            <ChartBuilderMenuItem
+                actions={actions}
+                disabledMessage=""
+                maxCharts={5}
+                model={model}
+                selectedReportIds={[]}
+            />,
+            {
+                serverContext: {
+                    user: TEST_USER_EDITOR,
+                },
+            }
+        );
         const menuItems = document.querySelectorAll('.lk-menu-item a');
         expect(menuItems).toHaveLength(1);
         expect(document.querySelector('.chart-menu-label').textContent).toBe('Create Chart');
@@ -58,5 +67,29 @@ describe('ChartBuilderMenuItem', () => {
 
         await userEvent.click(menuItems[0]);
         expect(document.querySelectorAll('.chart-builder-modal')).toHaveLength(1);
+    });
+
+    test('max charts', async () => {
+        renderWithAppContext(
+            <ChartBuilderMenuItem
+                actions={actions}
+                disabledMessage="too many charts"
+                maxCharts={5}
+                model={model}
+                selectedReportIds={['db:1', 'db:2', 'db:3', 'db:4', 'db:5', 'db:6']}
+            />,
+            {
+                serverContext: {
+                    user: TEST_USER_EDITOR,
+                },
+            }
+        );
+        const menuItems = document.querySelectorAll('.lk-menu-item.disabled');
+        expect(menuItems).toHaveLength(1);
+        expect(document.querySelector('.chart-menu-label').textContent).toBe('Create Chart');
+        expect(document.querySelectorAll('.chart-builder-modal')).toHaveLength(0);
+
+        await userEvent.click(menuItems[0]);
+        expect(document.querySelectorAll('.chart-builder-modal')).toHaveLength(0);
     });
 });

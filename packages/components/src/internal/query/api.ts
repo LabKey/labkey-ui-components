57da0254e0621aa14027c1b6932462c284d66de5
labkey-nicka <nickk@labkey.com>
2025-11-20 16:07:24 -0800

    lint

--- packages/components/src/internal/query/api.ts
+++ packages/components/src/internal/query/api.ts
@@ -374,8 +374,7 @@ function applyViewColumns(
 export class Renderers {
     static _check(columnMetadata, rawColumn, field, metadata) {
         const columnValue = columnMetadata[field];
-        if (columnValue)
-            return columnValue;
+        if (columnValue) return columnValue;
 
         if (columnMetadata.conceptURI || rawColumn.conceptURI) {
             const concept = metadata.getIn([

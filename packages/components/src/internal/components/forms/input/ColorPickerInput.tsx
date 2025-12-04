e87c8644bec17ca5948ecd9f7f873daa5608c982
cnathe <cnathe@labkey.com>
2025-12-04 08:17:34 -0600

    - ColorPickerInput update to support fixed position

--- packages/components/src/internal/components/forms/input/ColorPickerInput.tsx
+++ packages/components/src/internal/components/forms/input/ColorPickerInput.tsx
@@ -9,14 +9,26 @@ interface Props {
     allowRemove?: boolean;
     colors?: string[];
     disabled?: boolean;
+    fixedPosition?: boolean;
     name?: string;
+    noValueText?: string;
     onChange: (name: string, value: string) => void;
     text?: string;
     value: string;
 }
 
 export const ColorPickerInput: FC<Props> = memo(props => {
-    const { allowRemove, colors, disabled, name, onChange, text, value } = props;
+    const {
+        allowRemove,
+        colors,
+        disabled,
+        name,
+        onChange,
+        text,
+        value,
+        noValueText = 'None',
+        fixedPosition = false,
+    } = props;
     const [showPicker, setShowPicker] = useState<boolean>(false);
     const onChangeComplete = useCallback(
         (color?: ColorResult) => {
@@ -31,6 +43,21 @@ export const ColorPickerInput: FC<Props> = memo(props => {
         setShowPicker(s => !s);
     }, []);
 
+    // if value doesn't start with '#', add it
+    const value_ = value && !value.startsWith('#') ? `#${value}` : value;
+
+    // get the position of the button to position the picker
+    const [fixedTop, fixedLeft] = (() => {
+        const button = document.querySelector('.color-picker__button');
+        if (!fixedPosition || !button) {
+            return [0, 0];
+        }
+        const rect = button.getBoundingClientRect();
+        return [rect.bottom + 5, rect.left];
+    })();
+
+    const compactPicker = <CompactPicker onChangeComplete={onChangeComplete} color={value_} colors={colors} />;
+
     return (
         <div className="color-picker">
             <button
@@ -39,13 +66,13 @@ export const ColorPickerInput: FC<Props> = memo(props => {
                 onClick={togglePicker}
                 disabled={disabled}
             >
-                {text ? text : value ? <ColorIcon cls="color-picker__chip-small" asSquare value={value} /> : 'None'}
+                {text ? text : value ? <ColorIcon cls="color-picker__chip-small" asSquare value={value_} /> : noValueText}
                 <i className={classNames('fa', { 'fa-caret-up': showPicker, 'fa-caret-down': !showPicker })} />
             </button>
 
-            {text !== undefined && <ColorIcon cls="color-picker__chip" asSquare value={value} />}
+            {text !== undefined && <ColorIcon cls="color-picker__chip" asSquare value={value_} />}
 
-            {allowRemove && value && !disabled && (
+            {allowRemove && value_ && !disabled && (
                 <RemoveEntityButton onClick={onRemove} labelClass="color-picker__remove" />
             )}
 
@@ -53,7 +80,10 @@ export const ColorPickerInput: FC<Props> = memo(props => {
                 {showPicker && (
                     <>
                         <div className="color-picker__mask" onClick={togglePicker} />
-                        <CompactPicker onChangeComplete={onChangeComplete} color={value} colors={colors} />
+                        {fixedPosition && (
+                            <div style={{position: 'fixed', top: fixedTop, left: fixedLeft}}>{compactPicker}</div>
+                        )}
+                        {!fixedPosition && compactPicker}
                     </>
                 )}
             </div>

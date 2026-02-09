758e13cf98abb536921109a65fc793c7df8db56a
XingY <xyang@labkey.com>
2026-02-09 10:23:18 -0800

    GitHub Issue 840: Increase text choice options

--- packages/components/src/internal/components/domainproperties/constants.ts
+++ packages/components/src/internal/components/domainproperties/constants.ts
@@ -232,7 +232,7 @@ export const DERIVATION_DATA_SCOPES = {
     ALL: 'All',
 };
 
-export const MAX_VALID_TEXT_CHOICES = 200;
+export const MAX_VALID_TEXT_CHOICES = 400;
 
 export const LOOKUP_VALIDATOR_VALUES = { type: 'Lookup', name: 'Lookup Validator' };
 

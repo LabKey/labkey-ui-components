56312bbd2b649af026e1b8ad14449e271a7132a7
Xing Yang <5168106+XingY@users.noreply.github.com>
2026-02-24 18:11:38 -0800

    Expose Grid View Audit event in apps (#1941)

--- packages/components/releaseNotes/components.md
+++ packages/components/releaseNotes/components.md
@@ -1,6 +1,11 @@
 # @labkey/components
 Components, models, actions, and utility functions for LabKey applications and pages
 
+### version 7.20.3
+*Released*: 24 February 2026
+- GitHub Issue 465: Add auditing capabilities for grid views
+- GitHub Issue 840: Increase text choice options from 200 to 500
+
 ### version 7.20.2
 *Released*: 24 February 2026
 - Bump @labkey/api dependency
--- packages/components/src/internal/components/auditlog/utils.test.ts
+++ packages/components/src/internal/components/auditlog/utils.test.ts
@@ -30,18 +30,18 @@ import {
 describe('getAuditQueries', () => {
     test('LKS starter', () => {
         const auditQueries = getAuditQueries(TEST_LKS_STARTER_MODULE_CONTEXT);
-        expect(auditQueries.length).toBe(14);
+        expect(auditQueries.length).toBe(15);
         expect(auditQueries.findIndex(entry => entry === ASSAY_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);
         expect(auditQueries.findIndex(entry => entry === ASSAY_RESULT_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);
-        expect(auditQueries.findIndex(entry => entry === INVENTORY_AUDIT_QUERY)).toBe(12);
+        expect(auditQueries.findIndex(entry => entry === INVENTORY_AUDIT_QUERY)).toBe(13);
         expect(auditQueries.findIndex(entry => entry === WORKFLOW_AUDIT_QUERY)).toBe(-1);
         expect(auditQueries.findIndex(entry => entry === SOURCE_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);
     });
 
     test('LKSM starter', () => {
         const auditQueries = getAuditQueries(TEST_LKSM_STARTER_MODULE_CONTEXT);
-        expect(auditQueries.length).toBe(12);
-        expect(auditQueries.findIndex(entry => entry === INVENTORY_AUDIT_QUERY)).toBe(10);
+        expect(auditQueries.length).toBe(13);
+        expect(auditQueries.findIndex(entry => entry === INVENTORY_AUDIT_QUERY)).toBe(11);
         expect(auditQueries.findIndex(entry => entry === ASSAY_AUDIT_QUERY)).toBe(-1);
         expect(auditQueries.findIndex(entry => entry === ASSAY_RESULT_AUDIT_QUERY)).toBe(-1);
         expect(auditQueries.findIndex(entry => entry === WORKFLOW_AUDIT_QUERY)).toBe(-1);
@@ -50,8 +50,8 @@ describe('getAuditQueries', () => {
 
     test('LKSM professional', () => {
         const auditQueries = getAuditQueries(TEST_LKSM_PROFESSIONAL_MODULE_CONTEXT);
-        expect(auditQueries.length).toBe(17);
-        expect(auditQueries.findIndex(entry => entry === INVENTORY_AUDIT_QUERY)).toBe(15);
+        expect(auditQueries.length).toBe(18);
+        expect(auditQueries.findIndex(entry => entry === INVENTORY_AUDIT_QUERY)).toBe(16);
         expect(auditQueries.findIndex(entry => entry === ASSAY_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);
         expect(auditQueries.findIndex(entry => entry === WORKFLOW_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);
         expect(auditQueries.findIndex(entry => entry === SOURCE_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);
@@ -78,8 +78,8 @@ describe('getAuditQueries', () => {
             },
         };
         const auditQueries = getAuditQueries(moduleContext);
-        expect(auditQueries.length).toBe(18);
-        expect(auditQueries.findIndex(entry => entry === INVENTORY_AUDIT_QUERY)).toBe(16);
+        expect(auditQueries.length).toBe(19);
+        expect(auditQueries.findIndex(entry => entry === INVENTORY_AUDIT_QUERY)).toBe(17);
         expect(auditQueries.findIndex(entry => entry === ASSAY_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);
         expect(auditQueries.findIndex(entry => entry === ASSAY_RESULT_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);
         expect(auditQueries.findIndex(entry => entry === WORKFLOW_AUDIT_QUERY)).toBeGreaterThanOrEqual(0);

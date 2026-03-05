2b60158fe6b4764be0b9ca5d5aec779569532a78
labkey-nicka <nickk@labkey.com>
2026-03-05 14:21:26 -0800

    Lineage: declare props

--- packages/components/src/internal/components/lineage/models.ts
+++ packages/components/src/internal/components/lineage/models.ts
@@ -335,11 +335,11 @@ export class LineageNode
     declare url: string;
 
     // computed properties
-    distance: number;
-    iconProps: LineageIconMetadata;
-    links: LineageLinkMetadata;
-    listURL: string;
-    meta: LineageNodeMetadata;
+    declare distance: number;
+    declare iconProps: LineageIconMetadata;
+    declare links: LineageLinkMetadata;
+    declare listURL: string;
+    declare meta: LineageNodeMetadata;
 
     static create(lsid: string, values?: Partial<LineageNodeConfig>): LineageNode {
         let config;
@@ -350,7 +350,7 @@ export class LineageNode
             config = {
                 ...values,
                 ...LineageIO.applyConfig(values),
-                ...{
+                ...({
                     children: LineageLink.createList(values.children),
                     lsid,
                     name: values.restricted
@@ -358,7 +358,7 @@ export class LineageNode
                         : values.name,
                     parents: LineageLink.createList(values.parents),
                     steps: List(values.steps?.map(stepProps => new LineageRunStep(stepProps))),
-                },
+                } as Partial<LineageNodeConfig>),
             };
         }
 

22c938ec2faf5a31d96816174154734c69a07b98
alanv <alanv@labkey.com>
2026-03-11 13:11:55 -0500

    UnidentifiedPill: render different messages for NS and PS

--- packages/components/src/internal/UnidentifiedPill.tsx
+++ packages/components/src/internal/UnidentifiedPill.tsx
@@ -3,18 +3,28 @@ import { createPortal } from 'react-dom';
 import { generateId } from './util/utils';
 import { useOverlayTriggerState } from './OverlayTrigger';
 import { Popover } from './Popover';
-import { EMPTY_SEQUENCE_WARNING } from './constants';
+import { EMPTY_PS_SEQUENCE_WARNING, EMPTY_NS_SEQUENCE_WARNING } from './constants';
+import { SchemaQuery } from '../public/SchemaQuery';
+import { SCHEMAS } from './schemas';
 
-export const UnidentifiedPill: FC = () => {
+interface Props {
+    schemaQuery: SchemaQuery;
+}
+
+// TODO: take schemaQuery property and use that to determine the appropriate popover content
+export const UnidentifiedPill: FC<Props> = ({ schemaQuery }) => {
     const id = useMemo(() => generateId('unidentified-sequence-overlay-trigger'), []);
     // Note: we use useOverlayTriggerState instead of OverlayTrigger because the wrapping div from OverlayTrigger
     // causes layout problems.
     const { onMouseEnter, onMouseLeave, portalEl, show, targetRef } = useOverlayTriggerState(id, true, false);
+    const message = schemaQuery.isEqual(SCHEMAS.DATA_CLASSES.PROTEIN_SEQUENCE, false)
+        ? EMPTY_PS_SEQUENCE_WARNING
+        : EMPTY_NS_SEQUENCE_WARNING;
 
     const popover = useMemo(
         () => (
             <Popover id="unidentified-sequence-popover" placement="top" targetRef={targetRef}>
-                <div className="unidentified-sequence-popover">{EMPTY_SEQUENCE_WARNING}</div>
+                <div className="unidentified-sequence-popover">{message}</div>
             </Popover>
         ),
         [targetRef]

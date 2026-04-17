361ac6b2089d3495ae70b26a86de224c04dc958e
Xing Yang <5168106+XingY@users.noreply.github.com>
2026-04-17 09:54:33 -0700

    GitHub Issue 848: Add the ability to reset TOTP settings in the apps (#1981)

--- packages/components/releaseNotes/components.md
+++ packages/components/releaseNotes/components.md
@@ -1,6 +1,10 @@
 # @labkey/components
 Components, models, actions, and utility functions for LabKey applications and pages
 
+### version 7.30.0
+*Released*: 17 April 2026
+- GitHub Issue 848: Add the ability to reset TOTP settings in the apps
+
 ### version 7.29.4
 *Released*: 9 April 2026
 - GitHub Issue 954: Add error for duplicate values for parent inputs
---
+++ packages/components/src/internal/components/user/UserResetTotpSettingsConfirmModal.tsx
@@ -0,0 +1,53 @@
+import React, { FC, memo, useCallback, useState } from 'react';
+
+import { resolveErrorMessage } from '../../util/messaging';
+import { Modal } from '../../Modal';
+
+import { ResetTotpResponse, resetTotpSettings } from './actions';
+import { useNotificationsContext } from '../notifications/NotificationsContext';
+
+export interface UserResetTotpSettingsConfirmModalProps {
+    displayName: string;
+    email: string;
+    onCancel: () => void;
+    onComplete: (response: ResetTotpResponse) => void;
+    resetTotpSettingsApi?: (userId: number) => Promise<ResetTotpResponse>;
+    userId: number;
+}
+
+export const UserResetTotpSettingsConfirmModal: FC<UserResetTotpSettingsConfirmModalProps> = memo(props => {
+    const { email, displayName, userId, onCancel, onComplete, resetTotpSettingsApi = resetTotpSettings } = props;
+    const [submitting, setSubmitting] = useState<boolean>(false);
+    const { createNotification } = useNotificationsContext();
+
+    const onConfirm = useCallback(async () => {
+        setSubmitting(true);
+
+        try {
+            const resp = await resetTotpSettingsApi(userId);
+            onComplete({ email, ...resp });
+        } catch (e) {
+            const error = resolveErrorMessage(e, 'user', 'users', 'update') ?? 'Failed to reset TOTP settings';
+            onCancel();
+            createNotification({ alertClass: 'danger', message: error }, true);
+        } finally {
+            setSubmitting(false);
+        }
+    }, [email, userId, onComplete, resetTotpSettingsApi]);
+
+    return (
+        <Modal
+            confirmText="Yes, Reset TOTP Settings"
+            isConfirming={submitting}
+            onCancel={onCancel}
+            onConfirm={onConfirm}
+            title="Reset TOTP Settings?"
+        >
+            <p>
+                Are you sure you want to reset the TOTP settings for <b>{displayName}</b>?
+            </p>
+        </Modal>
+    );
+});
+
+UserResetTotpSettingsConfirmModal.displayName = 'UserResetTotpSettingsConfirmModal';

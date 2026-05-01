09b674248e40c9123a0ab3c0599dda6843d60361
labkey-susanh <susanh@labkey.com>
2026-04-30 17:30:25 -0700

    Remove falsy check for srText in Icon component

--- packages/components/src/internal/Icon.tsx
+++ packages/components/src/internal/Icon.tsx
@@ -2,13 +2,13 @@ import React, { FC } from 'react';
 
 interface IconProps {
     iconClass: string;
-    srText: string | undefined; // text for screen readers
+    srText: string; // text for screen readers
 }
 export const Icon: FC<IconProps> = ({ iconClass, srText }) => {
     return (
         <>
             <span aria-hidden="true" className={iconClass} />
-            {srText && <span className="sr-only">{srText}</span>}
+            <span className="sr-only">{srText}</span>
         </>
     );
 };

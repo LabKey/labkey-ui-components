6c1287383c36b60f4bf16604e8d3f940c5561f94
labkey-susanh <susanh@labkey.com>
2026-05-08 12:07:50 -0700

    Make user link a button styled as clickable-text

--- packages/components/src/internal/components/user/UserLink.tsx
+++ packages/components/src/internal/components/user/UserLink.tsx
@@ -72,9 +72,9 @@ export const UserLink: FC<UserLinkProps> = props => {
 
     return (
         <>
-            <a className="clickable user-link" onClick={toggleDetailsModal}>
+            <button className="clickable-text user-link" onClick={toggleDetailsModal}>
                 {targetUserDisplayValue}
-            </a>
+            </button>
             {showDetails && (
                 <UserDetailsPanel
                     container={container}

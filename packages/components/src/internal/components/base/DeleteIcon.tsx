4011a16c5c3814fb48656dd286a243ff7eb46832
labkey-susanh <susanh@labkey.com>
2026-05-12 06:21:51 -0700

    Add onKeyDown handler for DeleteIcon

--- packages/components/src/internal/components/base/DeleteIcon.tsx
+++ packages/components/src/internal/components/base/DeleteIcon.tsx
@@ -1,4 +1,5 @@
 import React, { FC, memo } from 'react';
+import { useEnterEscape } from '../../../public/useEnterEscape';
 
 interface Props {
     className?: string;
@@ -8,9 +9,12 @@ interface Props {
     title: string;
 }
 
-export const DeleteIcon: FC<Props> = memo(({ id, title, className = 'field-icon', onDelete, iconCls }) => (
-    <span id={id} title={title} className={className} onClick={onDelete}>
-        <span className={`fa fa-times-circle ${iconCls}`} />
-    </span>
-));
+export const DeleteIcon: FC<Props> = memo(({ id, title, className = 'field-icon', onDelete, iconCls }) => {
+    const onKeyDown = useEnterEscape(onDelete);
+    return (
+        <span className={className} id={id} onClick={onDelete} onKeyDown={onKeyDown} tabIndex={0} title={title}>
+            <span className={`fa fa-times-circle ${iconCls}`} />
+        </span>
+   );
+});
 DeleteIcon.displayName = 'DeleteIcon';

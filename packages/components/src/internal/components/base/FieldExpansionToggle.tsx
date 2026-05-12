dea216de9da96f54eda374c676e9f7e657797734
labkey-susanh <susanh@labkey.com>
2026-05-12 06:22:24 -0700

    Convert FieldExpressionToggle to FC to easily add onKeyDown handler

--- packages/components/src/internal/components/base/FieldExpansionToggle.tsx
+++ packages/components/src/internal/components/base/FieldExpansionToggle.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { FC } from 'react';
 import classNames from 'classnames';
+import { useEnterEscape } from '../../../public/useEnterEscape';
 
 interface Props {
     cls?: string;
@@ -8,27 +9,29 @@ interface Props {
     expandedTitle: string;
     highlighted?: boolean;
     id: string;
-    onClick: (event) => void;
+    onClick: (event: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-export class FieldExpansionToggle extends React.Component<Props, any> {
-    render() {
-        const { expanded, expandedTitle, collapsedTitle, cls, highlighted, id, onClick } = this.props;
-        const className = classNames('fa fa-lg', {
-            'field-highlighted': highlighted && !expanded,
-            'fa-chevron-down': expanded,
-            'fa-chevron-right': !expanded,
-        });
+export const FieldExpansionToggle: FC<Props> = props => {
+    const { expanded, expandedTitle, collapsedTitle, cls, highlighted, id, onClick } = props;
+    const className = classNames('fa fa-lg', {
+        'field-highlighted': highlighted && !expanded,
+        'fa-chevron-down': expanded,
+        'fa-chevron-right': !expanded,
+    });
+    const onKeyDown = useEnterEscape(onClick);
 
-        return (
-            <div
-                title={expanded ? expandedTitle : collapsedTitle}
-                className={'field-icon ' + (cls ? cls : '')}
-                id={id}
-                onClick={onClick}
-            >
-                <span className={className} />
-            </div>
-        );
-    }
-}
+    return (
+        <div
+            className={'field-icon ' + (cls ? cls : '')}
+            id={id}
+            onClick={onClick}
+            onKeyDown={onKeyDown}
+            tabIndex={0}
+            title={expanded ? expandedTitle : collapsedTitle}
+        >
+            <span className={className} />
+        </div>
+    );
+};
+FieldExpansionToggle.displayName = 'FieldExpansionToggle';

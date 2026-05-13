71445f20f2136e6a866994e7702085b6b19338c7
cnathe <cnathe@labkey.com>
2026-05-13 10:52:47 -0500

    jest updates to match changes

--- packages/components/src/internal/components/files/FileAttachmentEntry.test.tsx
+++ packages/components/src/internal/components/files/FileAttachmentEntry.test.tsx
@@ -7,12 +7,12 @@ import { FileAttachmentEntry } from './FileAttachmentEntry';
 describe('<FileAttachmentEntry>', () => {
     test('with onDelete', () => {
         const { container } = render(<FileAttachmentEntry onDelete={jest.fn()} name="Test files" />);
-        expect(document.querySelectorAll('span.fa-times-circle')).toHaveLength(1);
+        expect(document.querySelectorAll('.fa-times-circle')).toHaveLength(1);
         expect(container.textContent).toBe('Test files');
     });
     test('no deletion', () => {
         render(<FileAttachmentEntry allowDelete={false} name="Test files" />);
-        expect(document.querySelectorAll('span.fa-times-circle')).toHaveLength(0);
+        expect(document.querySelectorAll('.fa-times-circle')).toHaveLength(0);
     });
     test('with downloadUrl', () => {
         render(<FileAttachmentEntry downloadUrl="http://get/me/my/file" name="Test files" />);

0238f2ecb622c97b30fe5551dd0841352df4ae33
labkey-nicka <nickk@labkey.com>
2026-05-13 13:51:42 -0700

    AppContexts.includeGlobalState

--- packages/components/src/internal/AppContexts.tsx
+++ packages/components/src/internal/AppContexts.tsx
@@ -1,5 +1,5 @@
+import React, { FC, PropsWithChildren, useMemo } from 'react';
 import { getServerContext } from '@labkey/api';
-import React, { FC, ReactNode, useMemo } from 'react';
 
 import { AppContextProvider, ExtendableAppContext } from './AppContext';
 import { GlobalStateContextProvider } from './GlobalStateContext';
@@ -7,27 +7,30 @@ import { ServerContextProvider, withAppUser } from './components/base/ServerCont
 import { NotificationsContextProvider } from './components/notifications/NotificationsContext';
 import { LabelPrintingContextProvider } from './components/labelPrinting/LabelPrintingContextProvider';
 
-interface Props<T = {}> {
+interface Props<T = {}> extends PropsWithChildren {
+    includeGlobalState?: boolean;
     initialAppContext?: ExtendableAppContext<T>;
-    children?: ReactNode | undefined
 }
 
 /**
  * AppContexts is where you should add any additional contexts needed by our applications. At the moment all of our
- * apps share the same basic context configurations, and this component makes it easy for us to update all of our Apps
- * at once, and reduce the level of nesting needed in our Route configurations.
+ * apps share the same basic context configurations. This component makes it easy for us to update all of our Apps
+ * at once and reduce the level of nesting needed in our Route configurations.
  */
 export const AppContexts: FC<Props> = props => {
-    const { children, initialAppContext } = props;
+    const { children, includeGlobalState = true, initialAppContext } = props;
     const initialServerContext = useMemo(() => withAppUser(getServerContext()), []);
     return (
         <ServerContextProvider initialContext={initialServerContext}>
             <AppContextProvider initialContext={initialAppContext}>
-                <GlobalStateContextProvider>
-                    <NotificationsContextProvider>
-                        <LabelPrintingContextProvider>{children}</LabelPrintingContextProvider>
-                    </NotificationsContextProvider>
-                </GlobalStateContextProvider>
+                {includeGlobalState && (
+                    <GlobalStateContextProvider>
+                        <NotificationsContextProvider>
+                            <LabelPrintingContextProvider>{children}</LabelPrintingContextProvider>
+                        </NotificationsContextProvider>
+                    </GlobalStateContextProvider>
+                )}
+                {!includeGlobalState && <>{children}</>}
             </AppContextProvider>
         </ServerContextProvider>
     );

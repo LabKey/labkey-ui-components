2786dc64efc3382533b5b6cc63e279f0db4b829e
Nick Kerr <nickk@labkey.com>
2026-08-14 15:04:35 -0700

    Formsy: validation messages update on prop changes (#2051)

--- packages/components/src/internal/components/forms/input/useDataChangeCommentsRequired.ts
+++ packages/components/src/internal/components/forms/input/useDataChangeCommentsRequired.ts
@@ -2,28 +2,29 @@
  * Copyright (c) 2024-2026 LabKey Corporation. All rights reserved. No portion of this work may be reproduced
  * in any form or by any electronic or mechanical means without written permission from LabKey Corporation.
  */
-import { useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { useServerContext } from '../../base/ServerContext';
 import { getAppHomeFolderPath } from '../../../app/utils';
 import { useAppContext } from '../../../AppContext';
+import { Loader, useLoadableState } from '../../../useLoadableState';
+import { LoadingState } from '../../../../public/LoadingState';
 
-export const useDataChangeCommentsRequired = (): { requiresUserComment: boolean } => {
+export type DataChangeCommentsRequired = {
+    loadingState: LoadingState;
+    requiresUserComment: boolean;
+};
+
+export const useDataChangeCommentsRequired = (): DataChangeCommentsRequired => {
     const { container, moduleContext } = useServerContext();
     const { api } = useAppContext();
-    const [requiresUserComment, setRequiresUserComment] = useState<boolean>(false);
 
-    useEffect(
-        () => {
-            (async () => {
-                const path = getAppHomeFolderPath(container, moduleContext);
-                try {
-                    const response = await api.folder.getAuditSettings(path);
-                    setRequiresUserComment(!!response?.requireUserComments);
-                } catch (error) {
-                    console.error('Unable to retrieve audit log settings for ' + path, error);
-                }
-            })();
-        }, [ /** on load only */ ]);
+    const loader = useCallback<Loader<boolean>>(async () => {
+        const path = getAppHomeFolderPath(container, moduleContext);
+        const response = await api.folder.getAuditSettings(path);
+        return !!response?.requireUserComments;
+    }, [api.folder, container, moduleContext]);
 
-    return { requiresUserComment };
+    const { loadingState, value: requiresUserComment } = useLoadableState(loader);
+
+    return { loadingState, requiresUserComment };
 };

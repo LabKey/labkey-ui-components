005a2456b3f8c845bc1931b465ec6c06746ea5c7
labkey-nicka <nickk@labkey.com>
2026-08-30 12:58:18 -0700

    TextAreaInput: convert to FC, useDisableableInput

--- packages/components/src/internal/components/forms/input/TextAreaInput.tsx
+++ packages/components/src/internal/components/forms/input/TextAreaInput.tsx
@@ -2,52 +2,50 @@
  * Copyright (c) 2019-2026 LabKey Corporation. All rights reserved. No portion of this work may be reproduced
  * in any form or by any electronic or mechanical means without written permission from LabKey Corporation.
  */
-import React, { ReactNode } from 'react';
-
-import { FieldLabel } from '../FieldLabel';
+import React, { FC, ReactNode, useCallback, useMemo } from 'react';
 
 import { QueryColumn } from '../../../../public/QueryColumn';
-
+import { FieldLabel } from '../FieldLabel';
 import { INPUT_LABEL_CLASS_NAME, INPUT_WRAPPER_CLASS_NAME, MIXED_VALUE_DISPLAY } from '../constants';
-
-import { DisableableInput, DisableableInputProps, DisableableInputState } from './DisableableInput';
 import { FormsyTextArea, FormsyTextAreaProps } from './FormsyReactComponents';
+import { DisableableInputProps, useDisableableInput } from './DisableableInput';
 import { stringToHtmlId } from '../../../util/utils';
 
-interface TextAreaInputProps extends DisableableInputProps, Omit<FormsyTextAreaProps, 'onChange'> {
+export interface TextAreaInputProps extends DisableableInputProps, Omit<FormsyTextAreaProps, 'onChange'> {
     addLabelAsterisk?: boolean;
-    onChange?: (value: any) => void;
+    onChange?: (name: string, value: any) => void;
     queryColumn: QueryColumn;
     renderFieldLabel?: (queryColumn: QueryColumn, label?: string, description?: string) => ReactNode;
     showLabel?: boolean;
 }
 
-export class TextAreaInput extends DisableableInput<TextAreaInputProps, DisableableInputState> {
-    static defaultProps = {
-        ...DisableableInput.defaultProps,
-        ...{
-            cols: 50,
-            elementWrapperClassName: INPUT_WRAPPER_CLASS_NAME,
-            labelClassName: `${INPUT_LABEL_CLASS_NAME} textarea-control-label`,
-            rows: 5,
-            showLabel: true,
-        },
-    };
+export const TextAreaInput: FC<TextAreaInputProps> = props => {
+    // Extract DisableableInputProps, TextAreaInputProps
+    const {
+        addLabelAsterisk,
+        allowDisable,
+        disabled,
+        elementWrapperClassName = INPUT_WRAPPER_CLASS_NAME,
+        hasMixedValue,
+        id,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        initiallyDisabled,
+        label,
+        labelClassName = `${INPUT_LABEL_CLASS_NAME} textarea-control-label`,
+        onChange,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        onToggleDisable,
+        renderFieldLabel,
+        queryColumn,
+        showLabel = true,
+        ...formsyTextAreaProps
+    } = props;
+    const { inputValue, isDisabled, setInputValue, toggleDisabled } = useDisableableInput<string>(props);
+    const isDisabled_ = isDisabled || disabled;
 
-    constructor(props: TextAreaInputProps) {
-        super(props);
+    const id_ = useMemo(() => id ?? stringToHtmlId(queryColumn.fieldKey), [id, queryColumn]);
 
-        this.toggleDisabled = this.toggleDisabled.bind(this);
-
-        this.state = {
-            isDisabled: props.initiallyDisabled,
-        };
-    }
-
-    renderLabel() {
-        const { label, queryColumn, showLabel, allowDisable, addLabelAsterisk, renderFieldLabel } = this.props;
-        const { isDisabled } = this.state;
-
+    const label_ = useMemo(() => {
         if (renderFieldLabel) {
             return renderFieldLabel(queryColumn);
         }
@@ -57,6 +55,7 @@ export class TextAreaInput extends DisableableInput<TextAreaInputProps, Disablea
         if (showLabel === false) {
             return null;
         }
+
         return (
             <FieldLabel
                 column={queryColumn}
@@ -65,48 +64,38 @@ export class TextAreaInput extends DisableableInput<TextAreaInputProps, Disablea
                 labelOverlayProps={{ isFormsy: true, addLabelAsterisk }}
                 showLabel={showLabel}
                 showToggle={allowDisable}
-                toggleProps={{
-                    onClick: this.toggleDisabled,
-                }}
+                toggleProps={{ onClick: toggleDisabled }}
             />
         );
-    }
+    }, [renderFieldLabel, showLabel, queryColumn, isDisabled, label, addLabelAsterisk, allowDisable, toggleDisabled]);
 
-    onChange = (name, value): void => {
-        if (this.props.allowDisable) {
-            this.setState({ inputValue: value });
-        }
+    const onChange_ = useCallback(
+        (name, value) => {
+            setInputValue(value);
+            onChange?.(name, value);
+        },
+        [onChange, setInputValue]
+    );
 
-        this.props.onChange?.(value);
-    };
-
-    render() {
-        // Extract DisableableInputProps
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { allowDisable, initiallyDisabled, onToggleDisable, hasMixedValue, ...rest } = this.props;
-        // Extract TextAreaInputProps
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { addLabelAsterisk, onChange, queryColumn, renderFieldLabel, showLabel, ...textAreaProps } = rest;
-        const { labelClassName } = textAreaProps;
-
-        return (
-            <FormsyTextArea
-                aria-label={showLabel ? undefined : queryColumn.caption}
-                id={stringToHtmlId(queryColumn.fieldKey)}
-                name={queryColumn.fieldKey}
-                placeholder={
-                    hasMixedValue && this.state.isDisabled
-                        ? MIXED_VALUE_DISPLAY
-                        : `Enter ${queryColumn.caption.toLowerCase()}`
-                }
-                required={queryColumn.required}
-                {...textAreaProps}
-                disabled={this.state.isDisabled}
-                label={this.renderLabel()}
-                labelClassName={showLabel ? labelClassName : 'hide-label'}
-                onChange={this.onChange}
-                value={this.getInputValue()}
-            />
-        );
-    }
-}
+    return (
+        <FormsyTextArea
+            aria-label={showLabel ? undefined : queryColumn.caption}
+            cols={50}
+            name={queryColumn.fieldKey}
+            placeholder={
+                hasMixedValue && isDisabled_ ? MIXED_VALUE_DISPLAY : `Enter ${queryColumn.caption.toLowerCase()}`
+            }
+            required={queryColumn.required}
+            rows={5}
+            {...formsyTextAreaProps}
+            disabled={isDisabled_}
+            elementWrapperClassName={elementWrapperClassName}
+            id={id_}
+            label={label_}
+            labelClassName={showLabel ? labelClassName : 'hide-label'}
+            onChange={onChange_}
+            value={inputValue}
+        />
+    );
+};
+TextAreaInput.displayName = 'TextAreaInput';
